bf78791dfea56d36e8bb173b6cc16b242146fc8d
Jose Perez Rodriguez <joperezr@microsoft.com>
2018-10-11 10:48:48 -0700

    Adding comments to sample pwm project

--- samples/pwm-led/Program.cs
+++ samples/pwm-led/Program.cs
@@ -9,23 +9,24 @@ namespace pwm_led
     {
         static void Main(string[] args)
         {
+            // GPIO pin 18 maps to chip 0 channel 0 on the Raspberry pi
             int pwmChannel = 0;
             int pwmChip = 0;
-            int pwmPeriod = (int)((1f/800f)*100000000);
-            int pwm80PercentFrequency = (int)(pwmPeriod*0.8f);
-            int pwm40PercentFrequency = (int)(pwmPeriod*0.4f);
+            int pwmPeriod = (int)((1f/800f)*100000000); // Setting the period to 800 Mhgz
+            int pwm80PercentFrequency = (int)(pwmPeriod*0.8f); // Frequency at 80%
+            int pwm40PercentFrequency = (int)(pwmPeriod*0.4f); // Frequency at 40%
 
             using (GpioController controller = new GpioController())
             {
-                GpioPWMPin pin = controller.OpenPWMPin(pwmChip, pwmChannel);
-                pin.Period = pwmPeriod;
+                GpioPWMPin led = controller.OpenPWMPin(pwmChip, pwmChannel);
+                led.Period = pwmPeriod;
                 for (int i = 0; i < 10; i++)
                 {
-                    pin.DutyCycle = pwm80PercentFrequency;
-                    pin.PWMWrite();
+                    led.DutyCycle = pwm80PercentFrequency;
+                    led.PWMWrite(); // Setting frequency to 80%. Should make the LED look brighter.
                     Thread.Sleep(TimeSpan.FromSeconds(2));
-                    pin.DutyCycle = pwm40PercentFrequency;
-                    pin.PWMWrite();
+                    led.DutyCycle = pwm40PercentFrequency;
+                    led.PWMWrite(); // Setting frequency to 40%. Should make the LED look less bright.
                     Thread.Sleep(TimeSpan.FromSeconds(2));
                 }
             }
