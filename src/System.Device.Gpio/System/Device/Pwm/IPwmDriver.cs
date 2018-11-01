ba45d9f26a5c84b1ab43082d17aa43f2f2ae2c87
Jose Perez Rodriguez <joperezr@microsoft.com>
2018-11-01 15:37:08 -0700

    Adding Pwm classes

---
+++ src/System.Device.Gpio/System/Device/Pwm/IPwmDriver.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Device.Pwm
+{
+    public interface IPwmDriver : IDisposable
+    {
+        void OpenChannel(int pwmChannel);
+        void CloseChannel(int pwmChannel);
+        void Start(int pwmChannel, double frequency, double dutyCycle);
+        void Stop(int pwmChannel);
+    }
+}
---
+++ src/System.Device.Gpio/System/Device/Pwm/UnixPwmDriver.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Device.Pwm
+{
+    internal class UnixPwmDriver : IPwmDriver
+    {
+        public void CloseChannel(int pwmChannel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenChannel(int pwmChannel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start(int pwmChannel, double frequency, double dutyCycle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop(int pwmChannel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
