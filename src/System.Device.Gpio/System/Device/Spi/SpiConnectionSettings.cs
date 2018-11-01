b74d8e77a69dc171ca5b2382de397677e392b949
Jose Perez Rodriguez <joperezr@microsoft.com>
2018-11-01 14:47:21 -0700

    Adding Spi classes

---
+++ src/System.Device.Gpio/System/Device/Spi/SpiConnectionSettings.cs
@@ -0,0 +1,53 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Device.Spi
+{
+    /// <summary>
+    /// Class that holds the connection settings of a device on a Spi bus.
+    /// </summary>
+    public sealed class SpiConnectionSettings
+    {
+        private const SpiMode _defaultSpiMode = SpiMode.Mode0;
+        private const uint _defaultDataBitLenght = 8; // 1 byte.
+        private const uint _defaultClockFrequency = 500_000; // 500 KHz
+
+        private SpiConnectionSettings() { }
+
+        /// <summary>
+        /// Default constructor. Takes the bus id and the chip select line for that bus.
+        /// </summary>
+        /// <param name="busId">The bus id the device is connected to.</param>
+        /// <param name="chipSelectLine">The chip select line used on that bus id.</param>
+        public SpiConnectionSettings(uint busId, uint chipSelectLine)
+        {
+            BusId = busId;
+            ChipSelectLine = chipSelectLine;
+            Mode = _defaultSpiMode;
+            DataBitLength = _defaultDataBitLenght;
+            ClockFrequency = _defaultClockFrequency;
+        }
+
+        /// <summary>
+        /// The Spi mode being used.
+        /// </summary>
+        public SpiMode Mode { get; set; }
+        /// <summary>
+        /// The bus id the device is connected to.
+        /// </summary>
+        public uint BusId { get; set; }
+        /// <summary>
+        /// The chip select line used on that bus id.
+        /// </summary>
+        public uint ChipSelectLine { get; set; }
+        /// <summary>
+        /// The length of the data to be transfered.
+        /// </summary>
+        public uint DataBitLength { get; set; }
+        /// <summary>
+        /// The frequency in which the data will be transfered.
+        /// </summary>
+        public uint ClockFrequency { get; set; }
+    }
+}
---
+++ src/System.Device.Gpio/System/Device/Spi/SpiMode.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Device.Spi
+{
+    /// <summary>
+    /// Enum with the different modes supported by SPI.
+    /// CPOL - Clock polarity: defines if each cycle consists of a pulse of 1, or 0.
+    /// CPHA - Clock phase: timing of the data bits relative to the clock pulses.
+    /// </summary>
+    public enum SpiMode
+    {
+        /// <summary>
+        /// CPOL 0, CPHA 0
+        /// </summary>
+        Mode0,
+        /// <summary>
+        /// CPOL 0, CPHA 1
+        /// </summary>
+        Mode1,
+        /// <summary>
+        /// CPOL 1, CPHA 0
+        /// </summary>
+        Mode2,
+        /// <summary>
+        /// CPOL 1, CPHA 1
+        /// </summary>
+        Mode3
+    }
+}
