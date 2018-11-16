898569c79e0ceb592cc87d9f96d8c4172b1d3d25
Jose Perez Rodriguez <joperezr@microsoft.com>
2018-11-16 14:37:24 -0800

    Adding -ci parameter to the ci build so that we get the right build parameters set

--- eng/common/cibuild.sh
+++ eng/common/cibuild.sh
@@ -13,4 +13,4 @@ while [[ -h $source ]]; do
 done
 scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
 
-. "$scriptroot/build.sh" --restore --build --test --pack --publish $@
+. "$scriptroot/build.sh" --restore --build --test --pack --publish --ci $@
