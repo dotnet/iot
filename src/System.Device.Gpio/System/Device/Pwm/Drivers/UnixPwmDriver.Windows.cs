a457e611aaf930f9816fb78df13f49c70b1cdb09
Jose Perez Rodriguez <joperezr@microsoft.com>
2018-11-30 20:47:35 -0800

    Adding implementation for the PwmController (#57)
    Adding implementation for the PwmController

---
+++ src/System.Device.Gpio/System/Device/Pwm/Drivers/UnixPwmDriver.Windows.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.IO;
+
+namespace System.Device.Pwm.Drivers
+{
+    public class UnixPwmDriver : PwmDriver
+    {
+        public UnixPwmDriver() => throw new PlatformNotSupportedException($"The {GetType().Name} class is not available on Windows.");
+
+        protected internal override void ChangeDutyCycle(int pwmChip, int pwmChannel, double dutyCycleInNanoSeconds) => throw new PlatformNotSupportedException();
+
+        protected internal override void CloseChannel(int pwmChip, int pwmChannel) => throw new PlatformNotSupportedException();
+
+        protected internal override void OpenChannel(int pwmChip, int pwmChannel) => throw new PlatformNotSupportedException();
+
+        protected internal override void StartWriting(int pwmChip, int pwmChannel, double frequencyInHertz, double dutyCycleInNanoSeconds) => throw new PlatformNotSupportedException();
+
+        protected internal override void StopWriting(int pwmChip, int pwmChannel) => throw new PlatformNotSupportedException();
+    }
+}
--- src/System.Device.Gpio/System/Device/Pwm/PwmController.cs
+++ src/System.Device.Gpio/System/Device/Pwm/PwmController.cs
@@ -2,53 +2,93 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
+using System.Linq;
+
 namespace System.Device.Pwm
 {
-    public sealed class PwmController : IDisposable
+    public sealed partial class PwmController : IDisposable
     {
-        public PwmController()
-        {
-            throw new NotImplementedException();
-        }
+        private PwmDriver _driver;
+        private HashSet<(int, int)> _openChannels;
 
         public PwmController(PwmDriver driver)
         {
-            throw new NotImplementedException();
+            _driver = driver;
+            _openChannels = new HashSet<(int, int)>();
         }
 
-        public void OpenChannel(int pwmChannel)
+        public void OpenChannel(int pwmChip, int pwmChannel)
         {
-            throw new NotImplementedException();
+            if (_openChannels.Contains((pwmChip, pwmChannel)))
+            {
+                throw new InvalidOperationException("The selected pwm channel is already open.");
+            }
+
+            _driver.OpenChannel(pwmChip, pwmChannel);
+            _openChannels.Add((pwmChip, pwmChannel));
         }
 
-        public void CloseChannel(int pwmChannel)
+        public void CloseChannel(int pwmChip, int pwmChannel)
         {
-            throw new NotImplementedException();
+            if (!_openChannels.Contains((pwmChip, pwmChannel)))
+            {
+                throw new InvalidOperationException("Can not close a pwm channel that is not yet opened.");
+            }
+
+            _driver.CloseChannel(pwmChip, pwmChannel);
+            _openChannels.Remove((pwmChip, pwmChannel));
         }
 
-        public void ChangeDutyCycle(int pwmChannel, double dutyCycle)
+        public void ChangeDutyCycle(int pwmChip, int pwmChannel, double dutyCyclePercentage)
         {
-            throw new NotImplementedException();
+            if (!_openChannels.Contains((pwmChip, pwmChannel)))
+            {
+                throw new InvalidOperationException("Can not change dutycycle to a pwm channel that is not yet opened.");
+            }
+            if (dutyCyclePercentage < 0.0 || dutyCyclePercentage > 100.0)
+            {
+                throw new ArgumentException("Duty cycle must be a percentage in the range of 0.0 - 100.0", nameof(dutyCyclePercentage));
+            }
+            _driver.ChangeDutyCycle(pwmChip, pwmChannel, dutyCyclePercentage);
         }
 
-        public void StartWriting(int pwmChannel, double frequency, double dutyCycle)
+        public void StartWriting(int pwmChip, int pwmChannel, double frequencyInHertz, double dutyCyclePercentage)
         {
-            throw new NotImplementedException();
+            if (!_openChannels.Contains((pwmChip, pwmChannel)))
+            {
+                throw new InvalidOperationException("Can not start writing to a pwm channel that is not yet opened.");
+            }
+            if (dutyCyclePercentage < 0.0 || dutyCyclePercentage > 100.0)
+            {
+                throw new ArgumentException("Duty cycle must be a percentage in the range of 0.0 - 100.0", nameof(dutyCyclePercentage));
+            }
+            _driver.StartWriting(pwmChip, pwmChannel, frequencyInHertz, dutyCyclePercentage);
         }
 
-        public void StopWriting(int pwmChannel)
+        public void StopWriting(int pwmChip, int pwmChannel)
         {
-            throw new NotImplementedException();
+            if (!_openChannels.Contains((pwmChip, pwmChannel)))
+            {
+                throw new InvalidOperationException("Can not stop writing to a pwm channel that is not yet opened.");
+            }
+            _driver.StopWriting(pwmChip, pwmChannel);
         }
 
         public void Dispose()
         {
-
+            Dispose(true);
         }
 
         private void Dispose(bool disposing)
         {
-
+            while (_openChannels.Count > 0)
+            {
+                (int, int) channel = _openChannels.FirstOrDefault();
+                _driver.CloseChannel(channel.Item1, channel.Item2);
+                _openChannels.Remove(channel);
+            }
+            _driver.Dispose();
         }
     }
 }
