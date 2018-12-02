2b8f6835d345ed8b2fe2cb1b3cd7f33d6d4f0a42
Jose Perez Rodriguez <joperezr@microsoft.com>
2018-12-01 19:20:18 -0800

    Adding Raspberry Pi Driver

--- src/System.Device.Gpio/System/Device/Gpio/Drivers/RaspberryPi3Driver.Linux.cs
+++ src/System.Device.Gpio/System/Device/Gpio/Drivers/RaspberryPi3Driver.Linux.cs
@@ -2,68 +2,289 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace System.Device.Gpio.Drivers
 {
-    public partial class RaspberryPi3Driver : GpioDriver
+    public unsafe partial class RaspberryPi3Driver : GpioDriver
     {
-        protected internal override int PinCount => throw new NotImplementedException();
+        private RegisterView* _registerViewPointer = null;
+        private const int GpioRegisterOffset = 0x00;
+        private static readonly object s_InitializationLock = new object();
+        private static readonly object s_SysFsInitializationLock = new object();
+        private const string GpioMemoryFilePath = "/dev/gpiomem";
+        private UnixDriver _sysFSDriver = null;
+        private IDictionary<int, PinMode> _sysFSModes = new Dictionary<int, PinMode>();
 
         protected override void Dispose(bool disposing)
         {
-            throw new NotImplementedException();
+            if (_registerViewPointer != null)
+            {
+                Interop.munmap((IntPtr)_registerViewPointer, 0);
+                _registerViewPointer = null;
+            }
+            if (_sysFSDriver != null)
+            {
+                _sysFSDriver.Dispose();
+                _sysFSDriver = null;
+            }
         }
 
         protected internal override void AddCallbackForPinValueChangedEvent(int pinNumber, PinEventTypes eventType, PinChangeEventHandler callback)
         {
-            throw new NotImplementedException();
+            ValidatePinNumber(pinNumber);
+            InitializeSysFS();
+
+            _sysFSDriver.OpenPin(pinNumber);
+            _sysFSDriver.SetPinMode(pinNumber, _sysFSModes[pinNumber]);
+
+            _sysFSDriver.AddCallbackForPinValueChangedEvent(pinNumber, eventType, callback);
         }
 
         protected internal override void ClosePin(int pinNumber)
         {
-            throw new NotImplementedException();
+            ValidatePinNumber(pinNumber);
         }
 
         protected internal override bool IsPinModeSupported(int pinNumber, PinMode mode)
         {
-            throw new NotImplementedException();
+            switch(mode)
+            {
+                case PinMode.Input:
+                case PinMode.InputPullDown:
+                case PinMode.InputPullUp:
+                case PinMode.Output:
+                    return true;
+                default:
+                    return false;
+            }
         }
 
         protected internal override void OpenPin(int pinNumber)
         {
-            throw new NotImplementedException();
+            ValidatePinNumber(pinNumber);
+            Initialize();
         }
 
-        protected internal override PinValue Read(int pinNumber)
+        protected internal unsafe override PinValue Read(int pinNumber)
         {
-            throw new NotImplementedException();
+            ValidatePinNumber(pinNumber);
+
+            /*
+             * There are two registers that contain the value of a pin. Each hold the value of 32 
+             * different pins. 1 bit represents the value of a pin, 0 is PinValue.Low and 1 is PinValue.High
+             */
+
+            uint register = _registerViewPointer->GPLEV[pinNumber / 32];
+            return Convert.ToBoolean((register >> (pinNumber % 32)) & 1) ? PinValue.High : PinValue.Low;
         }
 
         protected internal override void RemoveCallbackForPinValueChangedEvent(int pinNumber, PinChangeEventHandler callback)
         {
-            throw new NotImplementedException();
+            ValidatePinNumber(pinNumber);
+            InitializeSysFS();
+
+            _sysFSDriver.OpenPin(pinNumber);
+            _sysFSDriver.SetPinMode(pinNumber, _sysFSModes[pinNumber]);
+
+            _sysFSDriver.RemoveCallbackForPinValueChangedEvent(pinNumber, callback);
         }
 
         protected internal override void SetPinMode(int pinNumber, PinMode mode)
         {
-            throw new NotImplementedException();
+            ValidatePinNumber(pinNumber);
+            IsPinModeSupported(pinNumber, mode);
+
+            /*
+             * There are 6 registers(4-byte ints) that control the mode for all pins. Each
+             * register controls the mode for 10 pins. Each pin uses 3 bits in the register
+             * containing the mode.
+             */
+
+            // Define the shift to get the right 3 bits in the register
+            int shift = (pinNumber % 10) * 3;
+            // Gets a pointer to the register that holds the mode for the pin
+            uint* registerPointer = &_registerViewPointer->GPFSEL[pinNumber / 10];
+            uint register = *registerPointer;
+            // Clear the 3 bits to 0 for the pin Number.
+            register &= ~(0b111U << shift);
+            // Set the 3 bits to the desired mode for that pin.
+            register |= (mode == PinMode.Output ? 1u : 0u) << shift;
+            *registerPointer = register;
+            
+            PinMode sysFSPinMode;
+            switch (mode)
+            {
+                case PinMode.Input:
+                case PinMode.InputPullDown:
+                case PinMode.InputPullUp:
+                    sysFSPinMode = PinMode.Input;
+                    break;
+                case PinMode.Output:
+                    sysFSPinMode = PinMode.Output;
+                    break;
+                default:
+                    throw new ArgumentException("Invalid PinMode");
+            }
+            if (_sysFSModes.ContainsKey(pinNumber))
+            {
+                _sysFSModes[pinNumber] = sysFSPinMode;
+            }
+            else
+            {
+                _sysFSModes.Add(pinNumber, sysFSPinMode);
+            }
+
+            if (mode != PinMode.Output)
+            {
+                SetInputPullMode(pinNumber, mode);
+            }
+        }
+
+        private void SetInputPullMode(int pinNumber, PinMode mode)
+        {
+            byte modeToPullMode;
+            switch(mode)
+            {
+                case PinMode.Input:
+                    modeToPullMode = 0;
+                    break;
+                case PinMode.InputPullDown:
+                    modeToPullMode = 1;
+                    break;
+                case PinMode.InputPullUp:
+                    modeToPullMode = 2;
+                    break;
+                default:
+                    throw new ArgumentException($"{mode} is not supported as a pull up/down mode.");
+            }
+
+            /*
+             * This is the process outlined by the BCM2835 datasheet on how to set the pull mode.
+             * The GPIO Pull - up/down Clock Registers control the actuation of internal pull-downs on the respective GPIO pins.
+             * These registers must be used in conjunction with the GPPUD register to effect GPIO Pull-up/down changes.
+             * The following sequence of events is required: 
+             * 
+             * 1. Write to GPPUD to set the required control signal (i.e.Pull-up or Pull-Down or neither to remove the current Pull-up/down)
+             * 2. Wait 150 cycles – this provides the required set-up time for the control signal 
+             * 3. Write to GPPUDCLK0/1 to clock the control signal into the GPIO pads you wish to modify
+             *    – NOTE only the pads which receive a clock will be modified, all others will retain their previous state.
+             * 4. Wait 150 cycles – this provides the required hold time for the control signal 
+             * 5. Write to GPPUD to remove the control signal 
+             * 6. Write to GPPUDCLK0/1 to remove the clock
+             */
+
+            uint* gppudPointer = &_registerViewPointer->GPPUD;
+            uint register = *gppudPointer;
+            register &= ~0b11U;
+            register |= modeToPullMode;
+            *gppudPointer = register;
+
+            // Wait 150 cycles – this provides the required set-up time for the control signal
+            Thread.SpinWait(150);
+
+            int index = pinNumber / 32;
+            int shift = pinNumber % 32;
+            uint* gppudclkPointer = &_registerViewPointer->GPPUDCLK[index];
+            register = *gppudclkPointer;
+            register |= 1U << shift;
+            *gppudclkPointer = register;
+
+            // Wait 150 cycles – this provides the required hold time for the control signal 
+            Thread.SpinWait(150);
+
+            register = *gppudPointer;
+            register &= ~0b11U;
+            *gppudPointer = register;
+            *gppudclkPointer = 0;
         }
 
         protected internal override WaitForEventResult WaitForEvent(int pinNumber, PinEventTypes eventType, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            ValidatePinNumber(pinNumber);
+            InitializeSysFS();
+
+            _sysFSDriver.OpenPin(pinNumber);
+            _sysFSDriver.SetPinMode(pinNumber, _sysFSModes[pinNumber]);
+
+            return _sysFSDriver.WaitForEvent(pinNumber, eventType, cancellationToken);
         }
 
         protected internal override ValueTask<WaitForEventResult> WaitForEventAsync(int pinNumber, PinEventTypes eventType, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            ValidatePinNumber(pinNumber);
+            InitializeSysFS();
+
+            _sysFSDriver.OpenPin(pinNumber);
+            _sysFSDriver.SetPinMode(pinNumber, _sysFSModes[pinNumber]);
+
+            return _sysFSDriver.WaitForEventAsync(pinNumber, eventType, cancellationToken);
         }
 
         protected internal override void Write(int pinNumber, PinValue value)
         {
-            throw new NotImplementedException();
+            ValidatePinNumber(pinNumber);
+
+            /*
+             * If the value is High, GPSET register is used. Otherwise, GPCLR will be used. For
+             * both cases, a 1 is set on the corresponding bit in the register in order to set 
+             * the desired value.
+             */
+
+            uint* registerPointer = (value == PinValue.High) ? &_registerViewPointer->GPSET[pinNumber / 32] : &_registerViewPointer->GPCLR[pinNumber / 32];
+            uint register = *registerPointer;
+            register = 1U << (pinNumber % 32);
+            *registerPointer = register;
+        }
+        
+        private void InitializeSysFS()
+        {
+            if (_sysFSDriver != null)
+            {
+                return;
+            }
+            lock (s_SysFsInitializationLock)
+            {
+                if (_sysFSDriver != null)
+                {
+                    return;
+                }
+                _sysFSDriver = new UnixDriver();
+            }
+        }
+
+        private void Initialize()
+        {
+            if (_registerViewPointer != null)
+            {
+                return;
+            }
+
+            lock (s_InitializationLock)
+            {
+                if (_registerViewPointer != null)
+                {
+                    return;
+                }
+
+                int fileDescriptor = Interop.open(GpioMemoryFilePath, FileOpenFlags.O_RDWR | FileOpenFlags.O_SYNC);
+                if (fileDescriptor < 0)
+                {
+                    throw new IOException("Error initializing the Gpio driver.");
+                }
+
+                IntPtr mapPointer = Interop.mmap(IntPtr.Zero, Environment.SystemPageSize, (MemoryMappedProtections.PROT_READ | MemoryMappedProtections.PROT_WRITE), MemoryMappedFlags.MAP_SHARED, fileDescriptor, GpioRegisterOffset);
+                if (mapPointer.ToInt32() < 0)
+                {
+                    throw new IOException("Error initializing the Gpio driver.");
+                }
+
+                Interop.close(fileDescriptor);
+                _registerViewPointer = (RegisterView*)mapPointer;
+            }
         }
     }
 }
