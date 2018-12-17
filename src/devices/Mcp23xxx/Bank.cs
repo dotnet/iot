66a9eba5fe2c86a1edac02f4feaeded739962a33
Greg Ingram <shaggygi97@gmail.com>
2018-12-17 17:31:10 -0500

    Added summary comments for Bank

--- src/devices/Mcp23xxx/Bank.cs
+++ src/devices/Mcp23xxx/Bank.cs
@@ -4,9 +4,22 @@
 
 namespace Iot.Device.Mcp23xxx
 {
+    /// <summary>
+    /// The MCP28XXX family has an address mapping concept for accessing registers.
+    /// This provides a way to easily address registers by group or type.
+    /// </summary>
     public enum Bank
     {
+        /// <summary>
+        /// This mode is used specifically for 16-bit devices where it causes the
+        /// address pointer to toggle between associated A/B register pairs.
+        /// </summary>
         Bank0 = 0,
+        /// <summary>
+        /// This mode is used to group each port's registers together.
+        /// This mode is the default since 8-bit devices only have one port and
+        /// 16-bit devices are initialized in this state.
+        /// </summary>
         Bank1 = 1
     }
 }
