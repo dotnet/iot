dd450c83109793a0ecf3734f836dad350527c5b0
Greg Ingram <shaggygi97@gmail.com>
2018-12-21 17:37:44 -0500

    Added CreateXxxDevice helpers

--- src/System.Device.Gpio/System/Device/I2c/I2cDevice.cs
+++ src/System.Device.Gpio/System/Device/I2c/I2cDevice.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Runtime.InteropServices;
+
 namespace System.Device.I2c
 {
     public abstract class I2cDevice : IDisposable
@@ -11,6 +13,30 @@ namespace System.Device.I2c
         public abstract void Read(Span<byte> buffer);
         public abstract void WriteByte(byte data);
         public abstract void Write(Span<byte> data);
+
+        /// <summary>
+        /// Initializes new instance of I2cDevice based on OS platform.
+        /// </summary>
+        /// <param name="busId">The bus ID the device is connected to.</param>
+        /// <param name="deviceAddress">The bus address of the device.</param>
+        public static I2cDevice CreateI2cDevice(int busId, int deviceAddress)
+        {
+            I2cConnectionSettings settings = new I2cConnectionSettings(busId, deviceAddress);
+            OSPlatform osPlatform = Interop.OperatingSystem.GetOsPlatform();
+
+            if (osPlatform == OSPlatform.Linux)
+            {
+                return new Drivers.UnixI2cDevice(settings);
+            }
+            else if (osPlatform == OSPlatform.Windows)
+            {
+                return new Drivers.Windows10I2cDevice(settings);
+            }
+            else
+            {
+                throw new PlatformNotSupportedException($"I2C not supported for this platform.");
+            }
+        }
 
         public void Dispose()
         {
--- src/System.Device.Gpio/System/Device/Spi/SpiDevice.cs
+++ src/System.Device.Gpio/System/Device/Spi/SpiDevice.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Runtime.InteropServices;
+
 namespace System.Device.Spi
 {
     public abstract class SpiDevice : IDisposable
@@ -12,6 +14,30 @@ namespace System.Device.Spi
         public abstract void WriteByte(byte data);
         public abstract void Write(Span<byte> data);
         public abstract void TransferFullDuplex(Span<byte> writeBuffer, Span<byte> readBuffer);
+
+        /// <summary>
+        /// Initializes new instance of SpiDevice based on OS platform.
+        /// </summary>
+        /// <param name="busId">The bus ID the device is connected to.</param>
+        /// <param name="chipSelectLine">The chip select line used on the bus.</param>
+        public static SpiDevice CreateSpiDevice(int busId, int chipSelectLine)
+        {
+            SpiConnectionSettings settings = new SpiConnectionSettings(busId, chipSelectLine);
+            OSPlatform osPlatform = Interop.OperatingSystem.GetOsPlatform();
+
+            if (osPlatform == OSPlatform.Linux)
+            {
+                return new Drivers.UnixSpiDevice(settings);
+            }
+            else if (osPlatform == OSPlatform.Windows)
+            {
+                return new Drivers.Windows10SpiDevice(settings);
+            }
+            else
+            {
+                throw new PlatformNotSupportedException($"SPI not supported for this platform.");
+            }
+        }
 
         public void Dispose()
         {
