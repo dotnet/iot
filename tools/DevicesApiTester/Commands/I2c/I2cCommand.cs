f155283e221dd26ba4f70f083e2815419695a609
Greg Ingram <shaggygi97@gmail.com>
2018-12-26 14:52:56 -0500

    Added new I2c commands

--- tools/DevicesApiTester/Commands/I2c/I2cCommand.cs
+++ tools/DevicesApiTester/Commands/I2c/I2cCommand.cs
@@ -13,11 +13,8 @@ namespace DeviceApiTester.Commands.I2c
         [Option('d', "device", HelpText = "The I2cDevice to use: { Windows | Unix }", Required = true)]
         public I2cDriverType Device { get; set; }
 
-        [Option('b', "bus-id", HelpText = "The bus id the I2C device to connect to", Required = true)]
+        [Option('b', "bus-id", HelpText = "The bus ID the device is connected to.", Required = true)]
         public int BusId { get; set; }
-
-        [Option('a', "device-address", HelpText = "The device address for the connection to the I2C device", Required = true)]
-        public int DeviceAddress { get; set; }
 
         protected I2cDevice CreateI2cDevice(I2cConnectionSettings connectionSettings)
         {
