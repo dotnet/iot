b38f4da7574ed8806aa7214b31ffc7af66bc79b8
fredeil <fredrik.eilertsen@gmail.com>
2019-01-20 15:48:33 +0100

    Initial commit

---
+++ src/devices/HCSR04/HCSR04.cs
@@ -0,0 +1,44 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Device.Gpio;
+using System.Device.I2c;
+using System.Device.I2c.Drivers;
+using System.Device.Spi;
+using System.Device.Spi.Drivers;
+
+namespace Iot.Device.HCSR04
+{
+    public class HCSR04 : IDisposable
+    {
+        private readonly int _echo;
+        private readonly int _trigger;
+        private readonly GpioController _controller;
+
+        public HCSR04(int triggerPin, int echoPin)
+        {
+            _echo = echoPin;
+            _trigger = triggerPin;
+            _controller = new GpioController();
+
+            _controller.SetPinMode(_echo, PinMode.Input);
+            _controller.SetPinMode(_trigger, PinMode.Output);
+        }
+
+        public double GetDistance()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            if(_controller != null)
+            {
+                _controller.Dispose();
+                _controller = null;
+            }
+        }
+    }
+}
---
+++ src/devices/HCSR04/README.md
@@ -0,0 +1,15 @@
+﻿# HC-SR04
+
+Device bindings for the HC-SR04 sonar distance sensor. Work in progress.
+
+## Summary
+
+Provide a brief description on what the component is and its functonality.
+
+## Binding Notes
+
+Provide any specifics related to binding API.  This could include how to configure component for particular functions and example code.
+
+## References
+
+Provide any references to other tutorials, blogs and hardware related to the component that could help others get started.
