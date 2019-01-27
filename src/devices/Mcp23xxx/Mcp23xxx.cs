3ab6f6b073ac8cccb304e046dbd5c17375602c57
Greg Ingram <shaggygi97@gmail.com>
2019-01-27 11:00:20 -0500

    Fix input pin bug and added summaries

--- src/devices/Mcp23xxx/Mcp23xxx.cs
+++ src/devices/Mcp23xxx/Mcp23xxx.cs
@@ -19,9 +19,9 @@ namespace Iot.Device.Mcp23xxx
         /// A general purpose parallel I/O expansion for I2C or SPI applications.
         /// </summary>
         /// <param name="deviceAddress">The device address for the connection on the I2C or SPI bus.</param>
-        /// <param name="reset">Output pin number that is connected to the hardware reset.</param>
-        /// <param name="interruptA">Input pin number that is connected to the interrupt for Port A (INTA).</param>
-        /// <param name="interruptB">Input pin number that is connected to the interrupt for Port B (INTB).</param>
+        /// <param name="reset">The output pin number that is connected to the hardware reset.</param>
+        /// <param name="interruptA">The input pin number that is connected to the interrupt for Port A (INTA).</param>
+        /// <param name="interruptB">The input pin number that is connected to the interrupt for Port B (INTB).</param>
         public Mcp23xxx(int deviceAddress, int? reset = null, int? interruptA = null, int? interruptB = null)
         {
             ValidateDeviceAddress(deviceAddress);
@@ -93,9 +93,28 @@ namespace Iot.Device.Mcp23xxx
             }
         }
 
+        /// <summary>
+        /// The I/O pin count of the device.
+        /// </summary>
         public abstract int PinCount { get; }
 
+        /// <summary>
+        /// Reads a number of bytes from registers.
+        /// </summary>
+        /// <param name="startingRegisterAddress">The starting register address to read.</param>
+        /// <param name="byteCount">The number of bytes to read.</param>
+        /// <param name="port">The I/O port used with the register.</param>
+        /// <param name="bank">The bank type that determines how the registers are mapped.</param>
+        /// <returns>The data read from the registers.</returns>
         public abstract byte[] Read(Register.Address startingRegisterAddress, byte byteCount, Port port = Port.PortA, Bank bank = Bank.Bank1);
+
+        /// <summary>
+        /// Writes a number of bytes to registers.
+        /// </summary>
+        /// <param name="startingRegisterAddress">The starting register address to write.</param>
+        /// <param name="data">The data to write to registers.</param>
+        /// <param name="port">The I/O port used with the registers.</param>
+        /// <param name="bank">The bank type that determines how the registers are mapped.</param>
         public abstract void Write(Register.Address startingRegisterAddress, byte[] data, Port port = Port.PortA, Bank bank = Bank.Bank1);
 
         public virtual void Dispose()
@@ -107,6 +126,9 @@ namespace Iot.Device.Mcp23xxx
             }
         }
 
+        /// <summary>
+        /// Disables the device by setting the reset pin LOW.
+        /// </summary>
         public void Disable()
         {
             if (_masterGpioController == null)
@@ -122,6 +144,9 @@ namespace Iot.Device.Mcp23xxx
             _masterGpioController.Write((int)_reset, PinValue.Low);
         }
 
+        /// <summary>
+        /// Enables the device by setting the reset pin HIGH.
+        /// </summary>
         public void Enable()
         {
             if (_masterGpioController == null)
@@ -137,23 +162,37 @@ namespace Iot.Device.Mcp23xxx
             _masterGpioController.Write((int)_reset, PinValue.High);
         }
 
+        /// <summary>
+        /// Reads a byte from a register.
+        /// </summary>
+        /// <param name="registerAddress">The register address to read.</param>
+        /// <param name="port">The I/O port used with the registers.</param>
+        /// <param name="bank">The bank type that determines how the register is mapped.</param>
+        /// <returns>The data read from the register.</returns>
         public byte Read(Register.Address registerAddress, Port port = Port.PortA, Bank bank = Bank.Bank1)
         {
             byte[] data = Read(registerAddress, 1, port, bank);
             return data[0];
         }
 
+        /// <summary>
+        /// Reads a bit from a register.
+        /// </summary>
+        /// <param name="registerAddress">The register address to read.</param>
+        /// <param name="bitNumber">The register bit number to read.</param>
+        /// <param name="port">The I/O port used with the registers.</param>
+        /// <param name="bank">The bank type that determines how the register is mapped.</param>
+        /// <returns>The value of the register bit read.</returns>
         public bool ReadBit(Register.Address registerAddress, int bitNumber, Port port = Port.PortA, Bank bank = Bank.Bank1)
         {
-            ValidateBitNumber(bitNumber);
             byte data = Read(registerAddress, port, bank);
             return GetBit(data, bitNumber);
         }
 
         /// <summary>
-        /// Read the pin value of interrupt for Port A (INTA).
+        /// Reads the pin value of interrupt for Port A (INTA).
         /// </summary>
-        /// <returns>Pin value of interrupt for Port A (INTA).</returns>
+        /// <returns>The pin value of interrupt for Port A (INTA).</returns>
         public PinValue ReadInterruptA()
         {
             if (_masterGpioController == null)
@@ -161,7 +200,7 @@ namespace Iot.Device.Mcp23xxx
                 throw new Exception("Master controller has not been initialized.");
             }
 
-            if (_reset == null)
+            if (_interruptA == null)
             {
                 throw new Exception("INTA pin has not been initialized.");
             }
@@ -170,9 +209,9 @@ namespace Iot.Device.Mcp23xxx
         }
 
         /// <summary>
-        /// Read the pin value of interrupt for Port B (INTB).
+        /// Reads the pin value of interrupt for Port B (INTB).
         /// </summary>
-        /// <returns>Pin value of interrupt for Port B (INTB).</returns>
+        /// <returns>The pin value of interrupt for Port B (INTB).</returns>
         public PinValue ReadInterruptB()
         {
             if (_masterGpioController == null)
@@ -180,7 +219,7 @@ namespace Iot.Device.Mcp23xxx
                 throw new Exception("Master controller has not been initialized.");
             }
 
-            if (_reset == null)
+            if (_interruptB == null)
             {
                 throw new Exception("INTB pin has not been initialized.");
             }
@@ -188,14 +227,28 @@ namespace Iot.Device.Mcp23xxx
             return _masterGpioController.Read((int)_interruptB);
         }
 
+        /// <summary>
+        ///  Writes a byte to a register.
+        /// </summary>
+        /// <param name="registerAddress">The register address to write.</param>
+        /// <param name="data">The data to write to the register.</param>
+        /// <param name="port">The I/O port used with the registers.</param>
+        /// <param name="bank">The bank type that determines how the register is mapped.</param>
         public void Write(Register.Address registerAddress, byte data, Port port = Port.PortA, Bank bank = Bank.Bank1)
         {
             Write(registerAddress, new byte[] { data }, port, bank);
         }
 
+        /// <summary>
+        ///  Writes to a register bit.
+        /// </summary>
+        /// <param name="registerAddress">The register address to write.</param>
+        /// <param name="bitNumber">The register bit number to write.</param>
+        /// <param name="bit">The value to write to register bit.</param>
+        /// <param name="port">The I/O port used with the registers.</param>
+        /// <param name="bank">The bank type that determines how the register is mapped.</param>
         public void WriteBit(Register.Address registerAddress, int bitNumber, bool bit, Port port = Port.PortA, Bank bank = Bank.Bank1)
         {
-            ValidateBitNumber(bitNumber);
             byte data = Read(registerAddress, port, bank);
 
             if (bit)
