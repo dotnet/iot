f67a588945c5982dc2e4e8101e46d4305876e939
Buyaa Namnan <bunamnan@microsoft.com>
2019-02-01 11:48:13 -0800

    FSR binding Version 1 for review

---
+++ src/devices/Fsr408/Fsr408.cs
@@ -0,0 +1,156 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Device.Gpio;
+using System.Device.I2c;
+using System.Device.I2c.Drivers;
+using System.Device.Spi;
+using System.Device.Spi.Drivers;
+
+namespace Iot.Device.Fsr408
+{
+    public class Fsr408 : IDisposable
+    {
+        private GpioController _controller;
+        private int _pinNumber = 18;
+
+        public Mcp3008 AdcConverter { get; set; }
+        public int Resistance { get; set; } = 10000; // 10k ohm
+        public int PowerSupplied { get; set; } = 5000;  // 5 mV
+
+        public Fsr408()
+        {
+            _controller = new GpioController();
+            _controller.OpenPin(_pinNumber);
+        }
+        public Fsr408(int pinNumber)
+        {
+            _pinNumber = pinNumber;
+            _controller = new GpioController();
+            _controller.OpenPin(_pinNumber);
+        }
+
+
+        public int ReadFromMcp3008()
+        {
+            if (AdcConverter != null)
+            {
+                return AdcConverter.Read(0);
+            }
+            else
+            {
+                throw new NotSupportedException("ADC converter not set");
+            }
+        }
+
+        public int ReadCapacitorChargingDuration()
+        {
+            int count = 0;
+            // set pin to low
+            _controller.SetPinMode(_pinNumber, PinMode.Output);
+            _controller.Write(_pinNumber, PinValue.Low);
+
+            // Prepare pin for input and ...
+            _controller.SetPinMode(_pinNumber, PinMode.Input);
+            while (_controller.Read(_pinNumber) == PinValue.Low)
+            { // count until read high
+                count++;
+                if (count == 30000)
+                {   // if count goes too high it means FSR resustance is highest which means no pressure, don't need to count more  
+                    break;
+                }
+            }
+            return count;
+        }
+
+        public int ReadVotlageUsingMcp3008()
+        {
+            if (AdcConverter != null)
+            {
+                int readValue = AdcConverter.Read(_pinNumber);
+                return CalculateVoltage(readValue);
+            }
+            else
+            {
+                throw new NotSupportedException("ADC converter not set");
+            }
+        }
+
+        public int CalculateVoltage(int readValue)
+        {
+            // Mcp3008 analog voltage reading ranges from 0 to 1023 (10 bit) 
+            // mapping it to corresponding milli voltage 
+            // which ranging from 0mV to PowerSupplied mV 
+            return PowerSupplied * readValue / 1023;
+        }
+
+        public int ReadFsrResistanceUsingMcp3008()
+        {
+            if (AdcConverter != null)
+            {
+                int readValue = AdcConverter.Read(_pinNumber);
+                return CalculateFsrResistance(CalculateVoltage(readValue));
+            }
+            else
+            {
+                throw new NotSupportedException("ADC converter not set");
+            }
+        }
+
+        public int CalculateFsrResistance(int fsrVoltage)
+        {
+            // Formula: FSR = ((Vcc - V) * R) / V
+            if (fsrVoltage > 0)
+            {
+                return (PowerSupplied - fsrVoltage) * Resistance / fsrVoltage;
+            }
+            return 0;
+        }
+
+        public int ReadPressureForceUsingMcp3008()
+        {
+            if (AdcConverter != null)
+            {
+                int readValue = AdcConverter.Read(_pinNumber);
+                return CalculateForce(CalculateFsrResistance(CalculateVoltage(readValue)));
+            }
+            else
+            {
+                throw new NotSupportedException("ADC converter not set");
+            }
+        }
+
+        public int CalculateForce(int resistance)
+        {
+            if (resistance > 0)
+            {
+                int force;
+                int fsrConductance = 1000000 / resistance; // in micro ohms
+
+                // Use the two FSR guide graphs to approximate the force
+                if (fsrConductance <= 1000)
+                {
+                    force = fsrConductance / 80;
+                }
+                else
+                {
+                    force = fsrConductance - 1000;
+                    force /= 30;
+                }
+                return force;
+            }
+            return 0;
+        }
+
+        public void Dispose()
+        {
+            if (_controller != null)
+            {
+                _controller.Dispose();
+                _controller = null;
+            }
+        }
+    }
+}
---
+++ src/devices/Fsr408/README.md
@@ -0,0 +1,23 @@
+﻿# Using Fsr408 (Force sensitive resistor)
+
+## Summary
+Force sensitive resistors change its resistivity depening on how much its pressed. This feature allows you to detect physical pressure, squeezing and weight. This sample demonstrates use of FSR Interlink 402 model, other types of FSR sensors usage will be pretty identical.
+
+
+**[FSR]**: [https://cdn-learn.adafruit.com/assets/assets/000/010/126/original/fsrguide.pdf]
+
+## Binding Notes
+
+As FSR generates analog signal depending on pressure, for controllers not having analog input we will need to use ADC converter
+You can use ADC device for reading in your project to access analog devices. [Reading Analog Input from a Potentiometer](samples/README.md) demonstrates a concrete example using this class.
+
+
+
+
+## References 
+The sample is based on following resources:
+
+* [Reading Analog Input from a Potentiometer](https://github.com/dotnet/iot/tree/master/src/devices/Mcp3008/samples) 
+* [Using an FSR](https://learn.adafruit.com/force-sensitive-resistor-fsr/using-an-fsr)
+* [Basic Resistor Sensor Reading on Raspberry Pi](https://learn.adafruit.com/basic-resistor-sensor-reading-on-raspberry-pi)
+
