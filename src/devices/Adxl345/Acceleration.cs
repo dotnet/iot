82836906b84bea259f347c4f16746981ca8a4c17
Zhang Yuexin <zhangyuexin121@live.cn>
2019-02-01 15:25:30 +0800

    add Acceleration.cs notes

--- src/devices/Adxl345/Acceleration.cs
+++ src/devices/Adxl345/Acceleration.cs
@@ -1,15 +1,27 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
 namespace Iot.Device.Adxl345
 {
+    /// <summary>
+    /// ADX1345 Data
+    /// </summary>
     public class Acceleration
     {
+        /// <summary>
+        /// X-axis acceleration (G)
+        /// </summary>
         public double X { get; set; }
-
+        
+        /// <summary>
+        /// Y-axis acceleration (G)
+        /// </summary>
         public double Y { get; set; }
-
+        
+        /// <summary>
+        /// Z-axis acceleration (G)
+        /// </summary>
         public double Z { get; set; }
     }
 }
