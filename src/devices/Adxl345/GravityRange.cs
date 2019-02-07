180f3ad654baa28bb92d6021aa27568833cea5a5
Zhang Yuexin <zhangyuexin121@live.cn>
2019-02-08 05:21:45 +0800

    Added ADXL345 binding (#193)
    * Added ADXL345 binding
    
    * Update README.md
    
    * add Acceleration.cs notes
    
    * Update Adxl345.csproj
    
    * fixed first review
    
    * Update GravityRange.cs
    
    * fixed second review
    
    * update .csproj
    
    * Update Adxl345.cs
    
    * add some notes
    
    * add enum Register and some notes
    
    * update README
    
    * Update Adxl345.cs

---
+++ src/devices/Adxl345/GravityRange.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Iot.Device.Adxl345
+{
+    /// <summary>
+    /// Gravity Measurement Range
+    /// </summary>
+    public enum GravityRange
+    {
+        /// <summary>
+        /// ±2G
+        /// </summary>
+        Range1 = 0x00,
+        /// <summary>
+        /// ±4G
+        /// </summary>
+        Range2 = 0x01,
+        /// <summary>
+        /// ±8G
+        /// </summary>
+        Range3 = 0x02,
+        /// <summary>
+        /// ±16G
+        /// </summary>
+        Range4 = 0x03
+    };
+}
