ad06368b4011801b355f6c7fa3f53f0e1619b4ff
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2019-02-07 23:05:31 -0800

    Change PinValue to struct
    Allows easier interop with raw data and enables us to extend with additional functionality. This will avoid having to add additional APIs for convenience and repeated typing of `PinValue == PinValue.High ? 1 :  0`.
    
    Doesn't impact existing enum usage.
    
    Sample usage:
    
    ``` C#
        class Program
        {
            static void Main(string[] args)
            {
                PinValue value = true;
                Console.WriteLine($"True is {value}");
                TestApi(0);
                Console.WriteLine($"Equality is {PinValue.High == true}");
            }
    
            public static void TestApi(PinValue value)
            {
                Console.WriteLine($"PinValue is {value}");
            }
        }
    ```

--- src/System.Device.Gpio/System/Device/Gpio/PinValue.cs
+++ src/System.Device.Gpio/System/Device/Gpio/PinValue.cs
@@ -7,15 +7,35 @@ namespace System.Device.Gpio
     /// <summary>
     /// Represents a value for a pin.
     /// </summary>
-    public enum PinValue
+    public readonly struct PinValue : IEquatable<PinValue>
     {
-        /// <summary>
-        /// The value of the pin is low.
-        /// </summary>
-        Low = 0,
+        // This isn't bool so the struct will retain blittable. This
+        // allows arrays of PinValue and structs that contain PinValue to
+        // be stack allocated.
+        private readonly byte _value;
+
+        private PinValue(byte value) => _value = value;
+
         /// <summary>
         /// The value of the pin is high.
         /// </summary>
-        High = 1
+        public static PinValue High => new PinValue(1);
+
+        /// <summary>
+        /// The value of the pin is low.
+        /// </summary>
+        public static PinValue Low => new PinValue(0);
+
+        public static implicit operator PinValue(int value) => value == 0 ? Low : High;
+        public static implicit operator PinValue(bool value) => value ? High : Low;
+        public static explicit operator int(PinValue value) => value._value;
+        public static explicit operator bool(PinValue value) => value._value == 0 ? false : true;
+
+        public bool Equals(PinValue other) => other._value == _value;
+        public static bool operator ==(PinValue a, PinValue b) => a.Equals(b);
+        public static bool operator !=(PinValue a, PinValue b) => !a.Equals(b);
+        public override int GetHashCode() => _value.GetHashCode();
+
+        public override string ToString() => _value == 0 ? "Low" : "High";
     }
 }
