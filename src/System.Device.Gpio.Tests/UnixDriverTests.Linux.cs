f662bdf6c2f971c1b880224436fd9a1607fe5bdd
Buyaa Namnan <bunamnan@microsoft.com>
2019-02-12 10:15:38 -0800

    Fixing build error

--- src/System.Device.Gpio.Tests/UnixDriverTests.Linux.cs
+++ src/System.Device.Gpio.Tests/UnixDriverTests.Linux.cs
@@ -8,7 +8,7 @@ namespace System.Device.Gpio.Tests
 {
     public class UnixDriverTests : GpioControllerTestBase
     {
-        protected override GpioDriver GetTestDriver() => UnixDriver.InitUnixDriver();
+        protected override GpioDriver GetTestDriver() => UnixDriver.CreateUnixDriver();
 
         protected override PinNumberingScheme GetTestNumberingScheme() => PinNumberingScheme.Logical;
     }
--- src/System.Device.Gpio/System/Device/Gpio/Drivers/UnixDriver.Linux.cs
+++ src/System.Device.Gpio/System/Device/Gpio/Drivers/UnixDriver.Linux.cs
@@ -10,7 +10,8 @@ namespace System.Device.Gpio.Drivers
     public abstract class UnixDriver : GpioDriver
     {
         // TODO: remove try catch after https://github.com/dotnet/corefx/issues/32015 deployed
-        public static UnixDriver CreateUnixDriver() {
+        public static UnixDriver CreateUnixDriver()
+        {
             UnixDriver driver = null;
             try
             {
