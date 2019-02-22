679ffea88166348a28429c952e33d027101d350d
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2019-02-22 12:38:49 -0800

    Change `data` to `buffer` - `value` (#273)
    Matching existing .NET API conventions.

--- src/System.Device.Gpio/System/Device/Spi/Drivers/UnixSpiDevice.Windows.cs
+++ src/System.Device.Gpio/System/Device/Spi/Drivers/UnixSpiDevice.Windows.cs
@@ -18,8 +18,8 @@ namespace System.Device.Spi.Drivers
 
         public override void TransferFullDuplex(ReadOnlySpan<byte> writeBuffer, Span<byte> readBuffer) => throw new PlatformNotSupportedException();
 
-        public override void Write(ReadOnlySpan<byte> data) => throw new PlatformNotSupportedException();
+        public override void Write(ReadOnlySpan<byte> buffer) => throw new PlatformNotSupportedException();
 
-        public override void WriteByte(byte data) => throw new PlatformNotSupportedException();
+        public override void WriteByte(byte value) => throw new PlatformNotSupportedException();
     }
 }
--- src/System.Device.Gpio/System/Device/Spi/Drivers/Windows10SpiDevice.Windows.cs
+++ src/System.Device.Gpio/System/Device/Spi/Drivers/Windows10SpiDevice.Windows.cs
@@ -76,21 +76,21 @@ namespace System.Device.Spi.Drivers
         /// <summary>
         /// Writes a byte to the SPI device.
         /// </summary>
-        /// <param name="data">The byte to be written to the SPI device.</param>
-        public override void WriteByte(byte data)
+        /// <param name="value">The byte to be written to the SPI device.</param>
+        public override void WriteByte(byte value)
         {
-            _winDevice.Write(new[] { data });
+            _winDevice.Write(new[] { value });
         }
 
         /// <summary>
         /// Writes data to the SPI device.
         /// </summary>
-        /// <param name="data">
+        /// <param name="buffer">
         /// The buffer that contains the data to be written to the SPI device.
         /// </param>
-        public override void Write(ReadOnlySpan<byte> data)
+        public override void Write(ReadOnlySpan<byte> buffer)
         {
-            _winDevice.Write(data.ToArray());
+            _winDevice.Write(buffer.ToArray());
         }
 
         /// <summary>
