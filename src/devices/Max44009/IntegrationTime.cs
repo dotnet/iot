99801aaef60685284f0387b8822cfb88f591a49c
悦欣 张 <zhangyuexin121@live.cn>
2019-02-24 10:21:10 +0800

    added MAX44009 binding

---
+++ src/devices/Max44009/IntegrationTime.cs
@@ -0,0 +1,42 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Iot.Device.Max44009
+{
+    public enum IntegrationTime
+    {
+        /// <summary>
+        /// 800ms
+        /// </summary>
+        TIM_800 = 0b000,
+        /// <summary>
+        /// 400ms
+        /// </summary>
+        TIM_400 = 0b001,
+        /// <summary>
+        /// 200ms
+        /// </summary>
+        TIM_200 = 0b010,
+        /// <summary>
+        /// 100ms
+        /// </summary>
+        TIM_100 = 0b011,
+        /// <summary>
+        /// 50ms
+        /// </summary>
+        TIM_050 = 0b100,
+        /// <summary>
+        /// 25ms
+        /// </summary>
+        TIM_025 = 0b101,
+        /// <summary>
+        /// 12.5ms
+        /// </summary>
+        TIM_012_5 = 0b110,
+        /// <summary>
+        /// 6.25ms
+        /// </summary>
+        TIM_006_25 = 0b111
+    }
+}
---
+++ src/devices/Max44009/samples/Program.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Device.I2c;
+using System.Device.I2c.Drivers;
+using System.Threading;
+
+namespace Iot.Device.Max44009.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            I2cConnectionSettings settings = new I2cConnectionSettings(1, Max44009.DefaultI2cAddress);
+            UnixI2cDevice device = new UnixI2cDevice(settings);
+
+            // integration time is 100ms
+            using (Max44009 sensor = new Max44009(device, IntegrationTime.TIM_100))
+            {
+                while (true)
+                {
+                    // read illuminance
+                    Console.WriteLine($"Illuminance: {sensor.Illuminance}Lux");
+                    Console.WriteLine();
+
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
