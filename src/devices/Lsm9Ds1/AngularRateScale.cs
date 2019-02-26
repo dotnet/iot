f994a9dbf378bcfa8a2de360aab9f07a6432d449
Krzysztof Wicher <mordotymoja@gmail.com>
2019-02-25 21:28:49 -0800

    SenseHAT [LSM9DS1, HTS221, LPS25H included] - LED matrix, joystick, accelerometer, gyroscope, magnetometer, thermometer, humidity, pressure (#242)
    * SenseHAT - LED matrix
    
    * add I2C implementation for SenseHatLedMatrix (I2C does not require installing driver)
    
    * add joystick
    
    * Accelerometer and gyroscope
    
    * magnetometer
    
    * temperature and humidity (HTS221)
    
    * pressure and temperature
    
    * Add SenseHat class combining everything together with example
    
    * add links to datasheets, explicit private
    
    * address feedback
    
    * brace on new line
    
    * access modifiers
    
    * make sample classes internal

---
+++ src/devices/Lsm9Ds1/AngularRateScale.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Iot.Device.Lsm9Ds1
+{
+    public enum AngularRateScale : byte
+    {
+        Scale0245Dps = 0b00,
+        Scale0500Dps = 0b01,
+        Scale2000Dps = 0b11,
+    }
+}
