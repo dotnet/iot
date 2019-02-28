a9c76b98fb6320f71d041d80aec63eb68a97a67e
Mikhail Bolshakov <mikhail.bolshakov@akvelon.com>
2019-02-28 19:31:28 +0300

    Refactoring to samples

--- src/devices/Buzzer/samples/Buzzer.Sample.cs
+++ src/devices/Buzzer/samples/Buzzer.Sample.cs
@@ -11,45 +11,46 @@ namespace Iot.Device.Buzzer.Samples
 {
     class Program
     {
-        static IList<MelodyElement> AbcSong = new List<MelodyElement>()
-            {
-                new NoteElement(Note.C, Octave.Fourth, Duration.Quarter),   // A
-                new NoteElement(Note.C, Octave.Fourth, Duration.Quarter),   // B
-                new NoteElement(Note.G, Octave.Fourth, Duration.Quarter),   // C
-                new NoteElement(Note.G, Octave.Fourth, Duration.Quarter),   // D
+        // Alphabet song: https://en.wikipedia.org/wiki/Alphabet_song#/media/File:Alphabet_song.png
+        static IList<MelodyElement> AlphabetSong = new List<MelodyElement>()
+        {
+            new NoteElement(Note.C, Octave.Fourth, Duration.Quarter),   // A
+            new NoteElement(Note.C, Octave.Fourth, Duration.Quarter),   // B
+            new NoteElement(Note.G, Octave.Fourth, Duration.Quarter),   // C
+            new NoteElement(Note.G, Octave.Fourth, Duration.Quarter),   // D
 
-                new NoteElement(Note.A, Octave.Fourth, Duration.Quarter),   // E
-                new NoteElement(Note.A, Octave.Fourth, Duration.Quarter),   // F
-                new NoteElement(Note.G, Octave.Fourth, Duration.Half),      // G
+            new NoteElement(Note.A, Octave.Fourth, Duration.Quarter),   // E
+            new NoteElement(Note.A, Octave.Fourth, Duration.Quarter),   // F
+            new NoteElement(Note.G, Octave.Fourth, Duration.Half),      // G
 
-                new NoteElement(Note.F, Octave.Fourth, Duration.Quarter),   // H
-                new NoteElement(Note.F, Octave.Fourth, Duration.Quarter),   // I
-                new NoteElement(Note.E, Octave.Fourth, Duration.Quarter),   // J
-                new NoteElement(Note.E, Octave.Fourth, Duration.Quarter),   // K
+            new NoteElement(Note.F, Octave.Fourth, Duration.Quarter),   // H
+            new NoteElement(Note.F, Octave.Fourth, Duration.Quarter),   // I
+            new NoteElement(Note.E, Octave.Fourth, Duration.Quarter),   // J
+            new NoteElement(Note.E, Octave.Fourth, Duration.Quarter),   // K
 
-                new NoteElement(Note.D, Octave.Fourth, Duration.Eighth),    // L
-                new NoteElement(Note.D, Octave.Fourth, Duration.Eighth),    // M
-                new NoteElement(Note.D, Octave.Fourth, Duration.Eighth),    // N
-                new NoteElement(Note.D, Octave.Fourth, Duration.Eighth),    // O
-                new NoteElement(Note.C, Octave.Fourth, Duration.Half),      // P
-                
-                new NoteElement(Note.G, Octave.Fourth, Duration.Quarter),   // Q
-                new NoteElement(Note.G, Octave.Fourth, Duration.Quarter),   // R
-                new NoteElement(Note.F, Octave.Fourth, Duration.Half),      // S
-                
-                new NoteElement(Note.E, Octave.Fourth, Duration.Quarter),   // T
-                new NoteElement(Note.E, Octave.Fourth, Duration.Quarter),   // U
-                new NoteElement(Note.D, Octave.Fourth, Duration.Half),      // V
+            new NoteElement(Note.D, Octave.Fourth, Duration.Eighth),    // L
+            new NoteElement(Note.D, Octave.Fourth, Duration.Eighth),    // M
+            new NoteElement(Note.D, Octave.Fourth, Duration.Eighth),    // N
+            new NoteElement(Note.D, Octave.Fourth, Duration.Eighth),    // O
+            new NoteElement(Note.C, Octave.Fourth, Duration.Half),      // P
+            
+            new NoteElement(Note.G, Octave.Fourth, Duration.Quarter),   // Q
+            new NoteElement(Note.G, Octave.Fourth, Duration.Quarter),   // R
+            new NoteElement(Note.F, Octave.Fourth, Duration.Half),      // S
+            
+            new NoteElement(Note.E, Octave.Fourth, Duration.Quarter),   // T
+            new NoteElement(Note.E, Octave.Fourth, Duration.Quarter),   // U
+            new NoteElement(Note.D, Octave.Fourth, Duration.Half),      // V
 
-                new NoteElement(Note.G, Octave.Fourth, Duration.Eighth),    // Dou-
-                new NoteElement(Note.G, Octave.Fourth, Duration.Eighth),    // ble
-                new NoteElement(Note.G, Octave.Fourth, Duration.Quarter),   // U
-                new NoteElement(Note.F, Octave.Fourth, Duration.Half),      // X
+            new NoteElement(Note.G, Octave.Fourth, Duration.Eighth),    // Dou-
+            new NoteElement(Note.G, Octave.Fourth, Duration.Eighth),    // ble
+            new NoteElement(Note.G, Octave.Fourth, Duration.Quarter),   // U
+            new NoteElement(Note.F, Octave.Fourth, Duration.Half),      // X
 
-                new NoteElement(Note.E, Octave.Fourth, Duration.Quarter),   // Y
-                new NoteElement(Note.E, Octave.Fourth, Duration.Quarter),   // and
-                new NoteElement(Note.D, Octave.Fourth, Duration.Half)       // Z
-            };
+            new NoteElement(Note.E, Octave.Fourth, Duration.Quarter),   // Y
+            new NoteElement(Note.E, Octave.Fourth, Duration.Quarter),   // and
+            new NoteElement(Note.D, Octave.Fourth, Duration.Half)       // Z
+        };
 
         static void Main(string[] args)
         {
@@ -57,8 +58,8 @@ namespace Iot.Device.Buzzer.Samples
             using (var player2 = new MelodyPlayer(new Buzzer(26, -1)))
             {
                 Task.WaitAll(
-                    Task.Run(() => player1.Play(AbcSong, 100, -12)),
-                    Task.Run(() => player2.Play(AbcSong, 100)));
+                    Task.Run(() => player1.Play(AlphabetSong, 100, -12)),
+                    Task.Run(() => player2.Play(AlphabetSong, 100)));
             }
         }
     }
