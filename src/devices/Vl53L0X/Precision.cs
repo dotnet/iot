8bae3cab3f9f936f28d26d3da121e853e265cda8
Laurent Ellerbach <laurelle@microsoft.com>
2019-04-10 22:29:31 +0200

    Adding VL53L0X distance sensor (#312)
    * Adding VL53L0X
    
    * reworked some properties based on PR feedbacks
    
    * Adjusting based on PR feedbacks

---
+++ src/devices/Vl53L0X/Precision.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Iot.Device.Vl53L0X
+{
+    /// <summary>
+    /// Sensor have multiple modes, you can select one of the 
+    /// predefined mode using the SetPrecision method
+    /// </summary>
+    public enum Precision
+    {
+        ShortRange = 0,
+        LongRange,
+        HighPrecision,
+    }
+}
