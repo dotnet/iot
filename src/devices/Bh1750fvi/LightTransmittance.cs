d2aa0bf873173011e888ac5227fd98362f78c5e9
悦欣 张 <zhangyuexin121@live.cn>
2019-04-16 19:59:23 +0800

    added bh1750fvi binding

---
+++ src/devices/Bh1750fvi/LightTransmittance.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Iot.Device.Bh1750fvi
+{
+    /// <summary>
+    /// Light Transmittance
+    /// </summary>
+    public enum LightTransmittance
+    {
+        /// <summary>
+        /// Light Transmittance is 50%
+        /// </summary>
+        Transmittance050,
+        /// <summary>
+        /// Light Transmittance is 50%
+        /// </summary>
+        Transmittance100,
+        /// <summary>
+        /// Light Transmittance is 50%
+        /// </summary>
+        Transmittance120,
+        /// <summary>
+        /// Light Transmittance is 50%
+        /// </summary>
+        Transmittance150,
+        /// <summary>
+        /// Light Transmittance is 50%
+        /// </summary>
+        Transmittance200,
+    }
+}
