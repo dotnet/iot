880b04fc206be509d6532a23c726af9a0487f5d7
Laurent Ellerbach <laurelle@microsoft.com>
2019-04-18 18:28:14 +0200

    updating csproj files and fixing double namespace

--- src/devices/GrovePi/Sensors/DhtSensor.cs
+++ src/devices/GrovePi/Sensors/DhtSensor.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using Iot.Device.GrovePiDevice.Models;
-using Iot.Device.GrovePiDevice.Models;
 using System;
 using System.Collections.Generic;
 using System.Threading;
