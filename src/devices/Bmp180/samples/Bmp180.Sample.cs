c0984ad1ddf735918a4e8335e10d2ff4fcfaf460
Antonio Garcia <garcia.olais@gmail.com>
2019-04-17 19:18:32 -0500

    Add bmp180 device (#184)
    * Add BMP180 device binding
    
    * Create README.md
    
    * Remove StartupObject and OutputType
    
    * Remove nuget.config file, isn't required
    
    * Fix project setup
    
    * Fix ProjectReference
    
    * Changes based in feedback
    
    * added Units.csproj
    
    * Added Iot.Units
    Fix "Sapling" -> "Sampling"
    Fix extra space
    Change Dispose method
    
    * Implement Temperature - Iot.Units
    
    * Remove CommunicationProtocol , BMP180 only have i2c
    
    * Remove CommunicationProtocol
    
    * Remove duplicate
    
    * Fix namespace
    
    * Fix README
    
    * Remove initialized
    Remove "this" keyword
    Add space
    
    * Fix brace style
    
    * add space between get and set
    
    * - Remove LangVersion
    - Remove MSBuildThisFileDirectory
    
    * Removed duplicate logic
    
    * - Changed B7 type
    - Add seaLevelPressure = 103125
    
    * - Add units
    - Fix tempValue.Celsius
    
    * simplify temperature formula
    
    * Remove shift operator
    Simplify pressure formula
    
    * Change return value
    Implement Span
    
    * add comment referencing  datasheet  time constant
    
    * Update Bmp180.cs
    
    Fix space
    
    * Update Register.cs
    
    change access modifier
    
    * Update Bmp180.csproj
    
    convert to -> Version="$(SystemDeviceGpioPackageVersion)"

---
+++ src/devices/Bmp180/samples/Bmp180.Sample.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Device.I2c;
+using System.Device.I2c.Drivers;
+using System.Threading;
+using Iot.Device.Bmp180;
+using Iot.Units;
+
+namespace Iot.Device.Bmp180.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Bmp180!");
+
+            //bus id on the raspberry pi 3
+            const int busId = 1;
+
+            var i2cSettings = new I2cConnectionSettings(busId, Bmp180.DefaultI2cAddress);
+            var i2cDevice = new UnixI2cDevice(i2cSettings);
+            var i2cBmp280 = new Bmp180(i2cDevice); 
+
+            using (i2cBmp280)
+            {
+                //set samplings
+                i2cBmp280.SetSampling(Sampling.Standard);
+
+                //read values
+                Temperature tempValue = i2cBmp280.ReadTemperature();
+                Console.WriteLine($"Temperature {tempValue.Celsius} °C");                
+                double preValue = i2cBmp280.ReadPressure();
+                Console.WriteLine($"Pressure {preValue} Pa");
+                double altValue = i2cBmp280.ReadAltitude();
+                Console.WriteLine($"Altitude {altValue:0.##} m");
+                Thread.Sleep(1000);
+
+                //set higher sampling
+                i2cBmp280.SetSampling(Sampling.UltraLowPower);
+
+                //read values
+                tempValue = i2cBmp280.ReadTemperature();
+                Console.WriteLine($"Temperature {tempValue.Celsius} °C");
+                preValue = i2cBmp280.ReadPressure();
+                Console.WriteLine($"Pressure {preValue} Pa");
+                altValue = i2cBmp280.ReadAltitude();
+                Console.WriteLine($"Altitude {altValue:0.##} m");                
+            }
+        }
+    }
+}
