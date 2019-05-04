4155e58f1fbef0aa53175667aa6d0dc617197393
Greg Ingram <shaggygi97@gmail.com>
2019-05-04 08:50:50 -0400

    Exposed more items in IGpioController

--- src/System.Device.Gpio/System/Device/Gpio/IGpioController.cs
+++ src/System.Device.Gpio/System/Device/Gpio/IGpioController.cs
@@ -6,12 +6,80 @@ namespace System.Device.Gpio
 {
     public interface IGpioController : IDisposable
     {
+        int PinCount { get; }
+
+        /// <summary>
+        /// Opens a pin in order for it to be ready to use.
+        /// </summary>
+        /// <param name="pinNumber">The pin number in the controller's numbering scheme.</param>
+        void OpenPin(int pinNumber);
+
+        /// <summary>
+        /// Opens a pin and sets it to a specific mode.
+        /// </summary>
+        /// <param name="pinNumber">The pin number in the controller's numbering scheme.</param>
+        /// <param name="mode">The mode to be set.</param>
         void OpenPin(int pinNumber, PinMode mode);
+
+        /// <summary>
+        /// Closes an open pin.
+        /// </summary>
+        /// <param name="pinNumber">The pin number in the controller's numbering scheme.</param>
         void ClosePin(int pinNumber);
+
+        /// <summary>
+        /// Checks if a specific pin is open.
+        /// </summary>
+        /// <param name="pinNumber">The pin number in the controller's numbering scheme.</param>
+        /// <returns>The status if the pin is open or closed.</returns>
+        bool IsPinOpen(int pinNumber);
+
+        /// <summary>
+        /// Gets the mode of a pin.
+        /// </summary>
+        /// <param name="pinNumber">The pin number in the controller's numbering scheme.</param>
+        /// <returns>The mode of the pin.</returns>
+        PinMode GetPinMode(int pinNumber);
+
+        /// <summary>
+        /// Sets the mode to a pin.
+        /// </summary>
+        /// <param name="pinNumber">The pin number in the controller's numbering scheme.</param>
+        /// <param name="mode">The mode to be set.</param>
         void SetPinMode(int pinNumber, PinMode mode);
+
+        /// <summary>
+        /// Checks if a pin supports a specific mode.
+        /// </summary>
+        /// <param name="pinNumber">The pin number in the controller's numbering scheme.</param>
+        /// <param name="mode">The mode to check.</param>
+        /// <returns>The status if the pin supports the mode.</returns>
+        bool IsPinModeSupported(int pinNumber, PinMode mode);
+
+        /// <summary>
+        /// Writes a value to a pin.
+        /// </summary>
+        /// <param name="pinNumber">The pin number in the controller's numbering scheme.</param>
+        /// <param name="value">The value to be written to the pin.</param>
         void Write(int pinNumber, PinValue value);
+
+        /// <summary>
+        /// Write the given pins with the given values.
+        /// </summary>
+        /// <param name="pinValuePairs">The pin/value pairs to write.</param>
         void Write(ReadOnlySpan<PinValuePair> pinValuePairs);
+
+        /// <summary>
+        /// Reads the current value of a pin.
+        /// </summary>
+        /// <param name="pinNumber">The pin number in the controller's numbering scheme.</param>
+        /// <returns>The value of the pin.</returns>
         PinValue Read(int pinNumber);
+
+        /// <summary>
+        /// Read the given pins with the given pin numbers.
+        /// </summary>
+        /// <param name="pinValuePairs">The pin/value pairs to read.</param>
         void Read(Span<PinValuePair> pinValuePairs);
     }
 }
