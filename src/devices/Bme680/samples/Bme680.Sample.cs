68e40b0020b2b9c504c985436c19e60d168dd558
George Mathieson <george@mathieson.me>
2019-06-02 21:14:08 +0100

    Create bme680 device binding project from the template

---
+++ src/devices/Bme680/samples/Bme680.Sample.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Device.Gpio;
+using System.Device.I2c;
+using System.Device.I2c.Drivers;
+using System.Device.Spi;
+using System.Device.Spi.Drivers;
+using System.Threading;
+
+namespace Iot.Device.Bme680.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Bme680 Sample!");
+        }
+    }
+}
