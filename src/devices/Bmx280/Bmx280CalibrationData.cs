b8704b56b3cf3c11389f20fecde4bdf75fa142a1
George Mathieson <george@mathieson.me>
2019-06-08 16:34:13 +0100

    Calibration data only needs functionality from the Bmxx80Base class

--- src/devices/Bmx280/Bmx280CalibrationData.cs
+++ src/devices/Bmx280/Bmx280CalibrationData.cs
@@ -6,12 +6,11 @@ using Iot.Device.Bmx280.Register;
 
 namespace Iot.Device.Bmx280
 {
-    internal class Bmx280CalibrationData
+    /// <summary>
+    /// Calibration data for the Bmx280 family.
+    /// </summary>
+    internal class Bmx280CalibrationData : Bmxx80CalibrationData
     {
-        public ushort DigT1 { get; set; }
-        public short DigT2 { get; set; }
-        public short DigT3 { get; set; }
-
         public ushort DigP1 { get; set; }
         public short DigP2 { get; set; }
         public short DigP3 { get; set; }
@@ -22,23 +21,27 @@ namespace Iot.Device.Bmx280
         public short DigP8 { get; set; }
         public short DigP9 { get; set; }
 
-        internal virtual void ReadFromDevice(BmxBase bmxBase)
+        /// <summary>
+        /// Read coefficient data from device.
+        /// </summary>
+        /// <param name="bmxx80Base">The <see cref="Bmxx80Base"/> to read coefficient data from.</param>
+        internal virtual void ReadFromDevice(Bmxx80Base bmxx80Base)
         {
             // Read temperature calibration data
-            DigT1 = bmxBase.Read16BitsFromRegister((byte)Bmx280Register.DIG_T1);
-            DigT2 = (short)bmxBase.Read16BitsFromRegister((byte)Bmx280Register.DIG_T2);
-            DigT3 = (short)bmxBase.Read16BitsFromRegister((byte)Bmx280Register.DIG_T3);
+            DigT1 = bmxx80Base.Read16BitsFromRegister((byte)Bmx280Register.DIG_T1);
+            DigT2 = (short)bmxx80Base.Read16BitsFromRegister((byte)Bmx280Register.DIG_T2);
+            DigT3 = (short)bmxx80Base.Read16BitsFromRegister((byte)Bmx280Register.DIG_T3);
 
             // Read pressure calibration data
-            DigP1 = bmxBase.Read16BitsFromRegister((byte)Bmx280Register.DIG_P1);
-            DigP2 = (short)bmxBase.Read16BitsFromRegister((byte)Bmx280Register.DIG_P2);
-            DigP3 = (short)bmxBase.Read16BitsFromRegister((byte)Bmx280Register.DIG_P3);
-            DigP4 = (short)bmxBase.Read16BitsFromRegister((byte)Bmx280Register.DIG_P4);
-            DigP5 = (short)bmxBase.Read16BitsFromRegister((byte)Bmx280Register.DIG_P5);
-            DigP6 = (short)bmxBase.Read16BitsFromRegister((byte)Bmx280Register.DIG_P6);
-            DigP7 = (short)bmxBase.Read16BitsFromRegister((byte)Bmx280Register.DIG_P7);
-            DigP8 = (short)bmxBase.Read16BitsFromRegister((byte)Bmx280Register.DIG_P8);
-            DigP9 = (short)bmxBase.Read16BitsFromRegister((byte)Bmx280Register.DIG_P9);
+            DigP1 = bmxx80Base.Read16BitsFromRegister((byte)Bmx280Register.DIG_P1);
+            DigP2 = (short)bmxx80Base.Read16BitsFromRegister((byte)Bmx280Register.DIG_P2);
+            DigP3 = (short)bmxx80Base.Read16BitsFromRegister((byte)Bmx280Register.DIG_P3);
+            DigP4 = (short)bmxx80Base.Read16BitsFromRegister((byte)Bmx280Register.DIG_P4);
+            DigP5 = (short)bmxx80Base.Read16BitsFromRegister((byte)Bmx280Register.DIG_P5);
+            DigP6 = (short)bmxx80Base.Read16BitsFromRegister((byte)Bmx280Register.DIG_P6);
+            DigP7 = (short)bmxx80Base.Read16BitsFromRegister((byte)Bmx280Register.DIG_P7);
+            DigP8 = (short)bmxx80Base.Read16BitsFromRegister((byte)Bmx280Register.DIG_P8);
+            DigP9 = (short)bmxx80Base.Read16BitsFromRegister((byte)Bmx280Register.DIG_P9);
         }
     }
 }
