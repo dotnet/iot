82dc53c6590851b4710b6458c3531f1bb8bd2990
George Mathieson <george@mathieson.me>
2019-06-08 23:42:50 +0100

    Add licence

--- src/devices/Bmxx80/CalibrationData/Bmxx80CalibrationData.cs
+++ src/devices/Bmxx80/CalibrationData/Bmxx80CalibrationData.cs
@@ -1,4 +1,8 @@
-﻿namespace Iot.Device.Bmxx80.CalibrationData
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Iot.Device.Bmxx80.CalibrationData
 {
     /// <summary>
     /// Calibration data for the Bmxx80 family.
