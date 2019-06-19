d80aa6643916f0737a712192728e7b413b1aa81b
Robin_tty <muellerobin95@gmail.com>
2019-06-19 19:34:34 +0200

    Add samples

---
+++ src/devices/Bh1745/samples/Bh1745.Sample.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Device.I2c;
+using System.Device.I2c.Drivers;
+using System.Threading.Tasks;
+
+namespace Iot.Device.Bh1745.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // bus id on the raspberry pi 3
+            const int busId = 1;
+
+			// create device
+            var i2cSettings = new I2cConnectionSettings(busId, Bh1745.DefaultI2cAddress);
+            var i2cDevice = new UnixI2cDevice(i2cSettings);
+            var i2CBh1745 = new Bh1745(i2cDevice);
+			
+			// Init device and wait for first measurement
+			i2CBh1745.Init();
+			Task.Delay(i2CBh1745.MeasurementTime.ToMilliseconds()).Wait();		
+			
+
+            while (true)
+            {                
+                var color = i2CBh1745.GetCompensatedColor();
+                Console.WriteLine("RGB color read: #{0:X2}{1:X2}{2:X2}", color.R, color.G, color.B);
+				Console.WriteLine($"Raw illumination value: {i2CBh1745.ClearDataRegister}")
+
+                Task.Delay(i2CBh1745.MeasurementTime.ToMilliseconds()).Wait();
+            }
+        }
+    }
+}
---
+++ src/devices/Bh1745/samples/Bh1745CustomConfiguration.Sample.cs
@@ -0,0 +1,66 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Device.I2c;
+using System.Device.I2c.Drivers;
+using System.Threading.Tasks;
+
+namespace Iot.Device.Bh1745.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // bus id on the raspberry pi 3
+            const int busId = 1;
+
+			// create device
+            var i2cSettings = new I2cConnectionSettings(busId, Bh1745.DefaultI2cAddress);
+            var i2cDevice = new UnixI2cDevice(i2cSettings);
+            var i2CBh1745 = new Bh1745(i2cDevice);
+			
+			// Init device
+			i2CBh1745.Init();
+			
+			// multipliers affect the compensated values
+			i2CBh1745.ChannelCompensationMultipliers.Red = 2.5;
+			i2CBh1745.ChannelCompensationMultipliers.Green = 0.9;
+			i2CBh1745.ChannelCompensationMultipliers.Blue = 1.9;
+			i2CBh1745.ChannelCompensationMultipliers.Clear = 9.5;
+			
+			// set custom  measurement time
+			i2CBh1745.MeasurementTime = MeasurementTime.Ms1280;
+			
+			// interrupt functionality is detailed in the datasheet
+			// Reference: https://www.mouser.co.uk/datasheet/2/348/bh1745nuc-e-519994.pdf (page 13)
+			i2CBh1745.LowerInterruptThreshold = 0xABFF;
+			i2CBh1745.HigherInterruptThreshold = 0x0A10;
+			
+			i2CBh1745.LatchBehavior = LatchBehavior.LatchEachMeasurement;
+			i2CBh1745.InterruptPersistence = InterruptPersistence.UpdateMeasurementEnd;
+			i2CBh1745.InterruptIsEnabled = true;
+			
+			
+			// wait for first measurement
+			Task.Delay(i2CBh1745.MeasurementTime.ToMilliseconds()).Wait();
+
+            while (true)
+            {                
+                var color = i2CBh1745.GetCompensatedColor();
+				
+				if(!i2CBh1745.MeasurementIsValid)
+				{					
+					Console.WriteLine("Measurement was not valid!");
+					continue;
+				}
+				
+                Console.WriteLine("RGB color read: #{0:X2}{1:X2}{2:X2}", color.R, color.G, color.B);
+				Console.WriteLine($"Raw illumination value: {i2CBh1745.ClearDataRegister}")
+
+                Task.Delay(i2CBh1745.MeasurementTime.ToMilliseconds()).Wait();
+            }
+        }
+    }
+}
