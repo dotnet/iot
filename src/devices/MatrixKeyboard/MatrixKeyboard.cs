aabe2c015e1701235f41de2afa0266e55bd74951
HumJ <tlos142857@live.cn>
2019-06-25 14:44:11 +0800

    Change Timer into Thread. Issue fixed

--- src/devices/MatrixKeyboard/MatrixKeyboard.cs
+++ src/devices/MatrixKeyboard/MatrixKeyboard.cs
@@ -18,21 +18,40 @@ namespace Iot.Device.MatrixKeyboard
         /// <summary>
         /// Get row pins
         /// </summary>
-        public IEnumerable<int> RowPins => rowPins;
+        public IEnumerable<int> RowPins => _rowPins;
 
         /// <summary>
         /// Get column pins
         /// </summary>
-        public IEnumerable<int> ColPins => colPins;
+        public IEnumerable<int> ColumnPins => _columnPins;
 
         /// <summary>
         /// Get or set scanning frequency in Hertz
         /// </summary>
-        public double ScanFreq
+        public double ScanFrequency
         {
-            get => 1000 / (scanTimer.Interval / colPins.Length);
-            set => scanTimer.Interval = 1000 / (value * rowPins.Length);
+            get => _scanFrequency;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Scanning frequency must be positive");
+                }
+                _scanFrequency = value;
+                _scanInterval = CalcScanInterval(_scanFrequency);
+            }
         }
+
+        /// <summary>
+        /// Get whether this keyboard is scanning
+        /// </summary>
+        public bool IsScanning { get; private set; }
+
+        private readonly int[] _rowPins;
+        private readonly int[] _columnPins;
+        private int _currentRow;
+        private readonly PinValue[] _buttonValues;
+        private double _scanFrequency;
 
         /// <summary>
         /// Initialize keyboard
@@ -43,14 +62,38 @@ namespace Iot.Device.MatrixKeyboard
         /// <param name="scanFreq">Scanning frequency</param>
         public MatrixKeyboard(IGpioController gpioController, IEnumerable<int> rowPins, IEnumerable<int> colPins, double scanFreq = 50)
         {
-            gpio = gpioController;
+            _gpio = gpioController ?? throw new ArgumentNullException(nameof(gpioController));
 
-            this.rowPins = rowPins.ToArray();
-            this.colPins = colPins.ToArray();
-            buttonValues = new PinValue[this.rowPins.Length * this.colPins.Length];
+            if (rowPins == null)
+            {
+                throw new ArgumentNullException(nameof(rowPins));
+            }
+            if (rowPins.Count() < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rowPins), "The number of rows is at least 1");
+            }
 
-            scanTimer = new Timer(1000 / (scanFreq * this.rowPins.Length));
-            scanTimer.Elapsed += ScanTimerElapsed;
+            if (colPins == null)
+            {
+                throw new ArgumentNullException(nameof(colPins));
+            }
+            if (colPins.Count() < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(colPins), "The number of columns is at least 1");
+            }
+
+            if (scanFreq <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(scanFreq), "Scanning frequency must be positive");
+            }
+
+            _rowPins = rowPins.ToArray();
+            _columnPins = colPins.ToArray();
+            _buttonValues = new PinValue[_rowPins.Length * _columnPins.Length];
+
+            _scanThread = new Thread(ScanThreadStart);
+            IsScanning = false;
+            ScanFrequency = scanFreq;
         }
 
         /// <summary>
@@ -58,15 +101,36 @@ namespace Iot.Device.MatrixKeyboard
         /// </summary>
         public void StartScan()
         {
-            for (var i = 0; i < rowPins.Length; i++)
+            if (!IsScanning)
             {
-                gpio.OpenPin(rowPins[i], PinMode.Output);
+                try
+                {
+                    for (var i = 0; i < _rowPins.Length; i++)
+                    {
+                        _gpio.OpenPin(_rowPins[i], PinMode.Output);
+                    }
+                    for (var i = 0; i < _columnPins.Length; i++)
+                    {
+                        _gpio.OpenPin(_columnPins[i], PinMode.Input);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    IsScanning = false;
+                    throw new Exception("Error while openning GPIO pins.", ex);
+                }
+
+                IsScanning = true;
+                try
+                {
+                    _scanThread.Start();
+                }
+                catch (Exception ex)
+                {
+                    IsScanning = false;
+                    throw new Exception("Error while starting scanning thread.", ex);
+                }
             }
-            for (var i = 0; i < colPins.Length; i++)
-            {
-                gpio.OpenPin(colPins[i], PinMode.Input);
-            }
-            scanTimer.Start();
         }
 
         /// <summary>
@@ -74,27 +138,40 @@ namespace Iot.Device.MatrixKeyboard
         /// </summary>
         public void StopScan()
         {
-            scanTimer.Stop();
-            for (var i = 0; i < rowPins.Length; i++)
+            if (IsScanning)
             {
-                gpio.ClosePin(rowPins[i]);
-            }
-            for (var i = 0; i < colPins.Length; i++)
-            {
-                gpio.ClosePin(colPins[i]);
+                IsScanning = false;
+                Thread.Sleep(_scanInterval); // wait for thread ends
+
+                try
+                {
+                    for (var i = 0; i < _rowPins.Length; i++)
+                    {
+                        _gpio.ClosePin(_rowPins[i]);
+                    }
+                    for (var i = 0; i < _columnPins.Length; i++)
+                    {
+                        _gpio.ClosePin(_columnPins[i]);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    IsScanning = false;
+                    throw new Exception("Error while closing GPIO pins.", ex);
+                }
             }
         }
 
         /// <summary>
         /// Get all buttons' values
         /// </summary>
-        public ReadOnlySpan<PinValue> Values => buttonValues.AsSpan();
+        public ReadOnlySpan<PinValue> Values => _buttonValues.AsSpan();
 
         /// <summary>
         /// Get buttons' values by row
         /// </summary>
         /// <param name="row">Row index</param>
-        public ReadOnlySpan<PinValue> RowValues(int row) => buttonValues.AsSpan(row * rowPins.Length, colPins.Length);
+        public ReadOnlySpan<PinValue> RowValues(int row) => _buttonValues.AsSpan(row * _rowPins.Length, _columnPins.Length);
 
         /// <summary>
         /// Keyboard event
@@ -108,40 +185,47 @@ namespace Iot.Device.MatrixKeyboard
 
         public void Dispose()
         {
-            scanTimer?.Dispose();
-            scanTimer = null;
+            IsScanning = false;
+            _scanThread = null;
 
-            gpio?.Dispose();
-            gpio = null;
+            _gpio?.Dispose();
+            _gpio = null;
         }
 
-        private readonly int[] rowPins;
-        private readonly int[] colPins;
-        private int currentRow;
-        private readonly PinValue[] buttonValues;
-
-        private IGpioController gpio;
-        private Timer scanTimer;
-        private void ScanTimerElapsed(object sender, ElapsedEventArgs e)
+        private IGpioController _gpio;
+        private Thread _scanThread;
+        private TimeSpan _scanInterval;
+        private TimeSpan CalcScanInterval(double scanFrequency)
         {
-            for (var i = 0; i < colPins.Length; i++)
+            // "scanFrequency * _rowPins.Length" is how many rows to scan per second
+            // it divide by 10000000(ticks) so we can get interval of the timer
+            return new TimeSpan((long)(10000000 / (scanFrequency * _rowPins.Length)));
+        }
+        private void ScanThreadStart()
+        {
+            while (IsScanning)
             {
-                var index = currentRow * colPins.Length + i;
-
-                var oldValue = buttonValues[index];
-                var newValue = gpio.Read(colPins[i]);
-
-                buttonValues[index] = newValue;
-                if (newValue != oldValue)
+                for (var i = 0; i < _columnPins.Length; i++)
                 {
-                    var args = new MatrixKeyboardEventArgs(newValue == PinValue.High ? PinEventTypes.Rising : PinEventTypes.Falling, currentRow, i);
-                    PinChangeEvent(this, args);
-                }
-            }
+                    var index = _currentRow * _columnPins.Length + i;
 
-            gpio.Write(rowPins[currentRow], PinValue.Low);
-            currentRow = (currentRow + 1) % rowPins.Length;
-            gpio.Write(rowPins[currentRow], PinValue.High);
+                    var oldValue = _buttonValues[index];
+                    var newValue = _gpio.Read(_columnPins[i]);
+
+                    _buttonValues[index] = newValue;
+                    if (newValue != oldValue)
+                    {
+                        var args = new MatrixKeyboardEventArgs(newValue == PinValue.High ? PinEventTypes.Rising : PinEventTypes.Falling, _currentRow, i);
+                        PinChangeEvent(this, args);
+                    }
+                }
+
+                _gpio.Write(_rowPins[_currentRow], PinValue.Low);
+                _currentRow = (_currentRow + 1) % _rowPins.Length;
+                _gpio.Write(_rowPins[_currentRow], PinValue.High);
+
+                Thread.Sleep(_scanInterval);
+            }
         }
     }
 }
