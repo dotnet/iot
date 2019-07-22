c10d7fc56cfa0265522893bc86d9013ecf997afc
张悦欣 <zhangyuexin121@live.cn>
2019-07-22 17:12:35 +0800

    add mlx90614

---
+++ src/devices/Mlx90614/Mlx90614.cs
@@ -0,0 +1,75 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Buffers.Binary;
+using System.Device.I2c;
+using Iot.Units;
+
+namespace Iot.Device.Mlx90614
+{
+    /// <summary>
+    /// Infra Red thermometer Sensor MLX90614
+    /// </summary>
+    public class Mlx90614 : IDisposable
+    {
+        private I2cDevice _i2cDevice;
+
+        /// <summary>
+        /// MLX90614's Ambient Temperature
+        /// </summary>
+        public Temperature AmbientTemperature => Temperature.FromCelsius(GetAmbientTemperature());
+
+        /// <summary>
+        /// MLX90614's Object Temperature
+        /// </summary>
+        public Temperature ObjectTemperature => Temperature.FromCelsius(GetObjectTemperature());
+
+        /// <summary>
+        /// Creates a new instance of the MLX90614
+        /// </summary>
+        /// <param name="i2cDevice">The I2C device used for communication.</param>
+        public Mlx90614(I2cDevice i2cDevice)
+        {
+            _i2cDevice = i2cDevice;
+        }
+
+        /// <summary>
+        /// Read ambient temperature from MLX90614
+        /// </summary>
+        /// <returns>Temperature in celsius</returns>
+        private double GetAmbientTemperature()
+        {
+            Span<byte> writeBuffer = stackalloc byte[] { (byte)Register.MLX_AMBIENT_TEMP };
+            Span<byte> readBuffer = stackalloc byte[2];
+
+            _i2cDevice.WriteRead(writeBuffer, readBuffer);
+
+            // The formula is on the datasheet P30.
+            return BinaryPrimitives.ReadInt16LittleEndian(readBuffer) * 0.02 - 273.15;
+        }
+
+        /// <summary>
+        /// Read object temperature from MLX90614
+        /// </summary>
+        /// <returns>Temperature in celsius</returns>
+        private double GetObjectTemperature()
+        {
+            Span<byte> writeBuffer = stackalloc byte[] { (byte)Register.MLX_OBJECT1_TEMP };
+            Span<byte> readBuffer = stackalloc byte[2];
+
+            _i2cDevice.WriteRead(writeBuffer, readBuffer);
+
+            return BinaryPrimitives.ReadInt16LittleEndian(readBuffer) * 0.02 - 273.15;
+        }
+
+        /// <summary>
+        /// Cleanup
+        /// </summary>
+        public void Dispose()
+        {
+            _i2cDevice?.Dispose();
+        }
+    }
+}
---
+++ src/devices/Mlx90614/Register.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Iot.Device.Mlx90614
+{
+    internal enum Register
+    {
+        MLX_AMBIENT_TEMP = 0x06,
+        MLX_OBJECT1_TEMP = 0x07,
+        MLX_OBJECT2_TEMP = 0x08
+    }
+}
