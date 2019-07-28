77aa9266da2f2734930893b82c2241eb6aee5fc4
Greg Ingram <shaggygi97@gmail.com>
2019-07-28 15:36:52 -0400

    Simplified and improved logic

--- src/devices/ServoMotor/ServoMotor.cs
+++ src/devices/ServoMotor/ServoMotor.cs
@@ -3,219 +3,132 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Device.Gpio;
 using System.Device.Pwm;
-using System.Device.Pwm.Drivers;
-using System.IO;
 
-namespace Iot.Device.Servo
+namespace Iot.Device.ServoMotor
 {
+    /// <summary>
+    /// Represents a rotary actuator or linear actuator that allows for precise control of angular or linear position.
+    /// </summary>
     public class ServoMotor : IDisposable
     {
-        private double _pulseFrequency = 20.0;
-        private double _angle;
-        double _currentPulseWidth = 0;
-        
-        /// <summary>
-        /// Are we running on hardware or software PWM? True for hardware, false for sofware
-        /// </summary>
-        public bool IsRunningHardwarePwm { get; private set; }
-
-        /// <summary>
-        /// Servo motor definition.
-        /// </summary>
-        private ServoMotorDefinition _definition;
-
-        /// <summary>
-        /// The duration per angle's degree.
-        /// </summary>
-        private double _rangePerDegree;
         private PwmChannel _pwmChannel;
+        private readonly int _maximumAngle;
+        private readonly int _maximumPulseWidthMicroseconds;
+        private readonly int _minimumPulseWidthMicroseconds;
+        private readonly double _servoAngleSlope;
 
         /// <summary>
-        /// Initialize a <see cref="ServoMotor"/> using a specified PwmChannel
+        /// Initializes a new instance of the <see cref="ServoMotor"/> class.
         /// </summary>
-        /// <param name="pwmChannel">The <see cref="PwmChannel"/> to be used to control this servo.</param>
-        /// <param name="definition">The <see cref="ServoMotorDefinition"/> to be used</param>
-        public ServoMotor(PwmChannel pwmChannel, ServoMotorDefinition definition)
+        /// <param name="maximumAngle">The maximum angle the servo motor can move.</param>
+        /// <param name="minimumPulseWidthMicroseconds">The minimum pulse width, in microseconds, that represent an angle for 0 degrees.</param>
+        /// <param name="maximumPulseWidthMicroseconds">The maxnimum pulse width, in microseconds, that represent an angle for maximum angle.</param>
+        private ServoMotor(int maximumAngle, int minimumPulseWidthMicroseconds, int maximumPulseWidthMicroseconds)
         {
-            _definition = definition;
-            _pulseFrequency = definition.PeriodMicroseconds / 1000.0;
+            _maximumAngle = maximumAngle;
+            _minimumPulseWidthMicroseconds = minimumPulseWidthMicroseconds;
+            _maximumPulseWidthMicroseconds = maximumPulseWidthMicroseconds;
+            _servoAngleSlope = CalculateServoAngleSlope(maximumAngle, minimumPulseWidthMicroseconds, maximumPulseWidthMicroseconds);
+        }
 
-            UpdateRange();
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="ServoMotor"/> class.
+        /// </summary>
+        /// <param name="pwmChannel">The PWM channel used to control the servo motor.</param>
+        /// <param name="maximumAngle">The maximum angle the servo motor can move.</param>
+        /// <param name="minimumPulseWidthMicroseconds">The minimum pulse width, in microseconds, that represent an angle for 0 degrees.</param>
+        /// <param name="maximumPulseWidthMicroseconds">The maxnimum pulse width, in microseconds, that represent an angle for maximum angle.</param>
+        public ServoMotor(
+            PwmChannel pwmChannel,
+            int maximumAngle = 180,
+            int minimumPulseWidthMicroseconds = 1_000,
+            int maximumPulseWidthMicroseconds = 2_000)
+            : this(maximumAngle, minimumPulseWidthMicroseconds, maximumPulseWidthMicroseconds)
+        {
             _pwmChannel = pwmChannel;
-
-            _pwmChannel.DutyCyclePercentage = (1 - (_pulseFrequency - _currentPulseWidth) / _pulseFrequency) * 100;
-            _pwmChannel.Start();
-
         }
 
         /// <summary>
-        /// Initialize a <see cref="ServoMotor"/> using <see cref="SoftwarePwmChannel"/> on <paramref name="pinNumber"/>
+        ///  Initializes a new instance of the <see cref="ServoMotor"/> class.
         /// </summary>
-        /// <param name="pinNumber">The pin that will be used for the software pwm</param>
-        /// <param name="definition">The <see cref="ServoMotorDefinition"/> to be used</param>
-        public ServoMotor(int pinNumber, ServoMotorDefinition definition)
-            : this(CreatePwmChannel(pinNumber, 0, 0, definition, false), definition)
+        /// <param name="chip">The PWM chip number used to control the servo motor.</param>
+        /// <param name="channel">The PWM channel number used to control the servo motor.</param>
+        /// <param name="maximumAngle">The maximum angle the servo motor can move.</param>
+        /// <param name="minimumPulseWidthMicroseconds">The minimum pulse width, in microseconds, that represent an angle for 0 degrees.</param>
+        /// <param name="maximumPulseWidthMicroseconds">The maxnimum pulse width, in microseconds, that represent an angle for maximum angle.</param>
+        public ServoMotor(
+            int chip,
+            int channel,
+            int maximumAngle = 180,
+            int minimumPulseWidthMicroseconds = 1_000,
+            int maximumPulseWidthMicroseconds = 2_000)
+            : this(maximumAngle, minimumPulseWidthMicroseconds, maximumPulseWidthMicroseconds)
         {
-            IsRunningHardwarePwm = false;
+            _pwmChannel = PwmChannel.Create(chip, channel, 50);  // Default 50Hz
         }
 
-        /// <summary>
-        /// Initialize a <see cref="ServoMotor"/> using Hardware Pwm on <paramref name="chip"/> and <paramref name="channel"/>
-        /// </summary>
-        /// <param name="chip">The chip number for Hardware Pwm</param>
-        /// <param name="channel">The channel number for Hardware Pwm</param>
-        /// <param name="definition">The <see cref="ServoMotorDefinition"/> to be used</param>
-        public ServoMotor(int chip, int channel, ServoMotorDefinition definition)
-            : this(CreatePwmChannel(-1, chip, channel, definition, true), definition)
+        private static double CalculateServoAngleSlope(int maximumAngle, int minimumPulseWidthMicroseconds, int maximumPulseWidthMicroseconds)
         {
-            IsRunningHardwarePwm = true;
-        }
-
-        private static PwmChannel CreatePwmChannel(int pinNumber, int chip, int channel, ServoMotorDefinition definition, bool useHardwarePwm)
-        {
-            if (useHardwarePwm)
+            if (maximumAngle < 0 || maximumAngle > 360)
             {
-                return PwmChannel.Create(chip, channel, (int)(1000000 / definition.PeriodMicroseconds));
+                throw new ArgumentOutOfRangeException(nameof(maximumAngle), maximumAngle, "Value must be between 0 and 360.");
             }
-            else
+
+            if (minimumPulseWidthMicroseconds < 0)
             {
-                return new SoftwarePwmChannel(pinNumber, (int)(1000000 / definition.PeriodMicroseconds));
+                throw new ArgumentOutOfRangeException(nameof(minimumPulseWidthMicroseconds), minimumPulseWidthMicroseconds, "Value must not be negative.");
             }
-        }
 
-        /// <summary>
-        /// Rotate the servomotor with a specific pulse in microseconds
-        /// </summary>
-        /// <param name="durationMicroSec">Pulse duration in microseconds</param>
-        public void SetPulse(uint durationMicroSec)
-        {
-            _currentPulseWidth = durationMicroSec / 1000.0;
-            _pwmChannel.DutyCyclePercentage = (1 - (_pulseFrequency - _currentPulseWidth) / _pulseFrequency) * 100;
-        }
-
-        /// <summary>
-        /// Rotate the servomotor with a specific pulse in microseconds
-        /// </summary>
-        /// <param name="periodMicroSec">Period duration in microseconds</param>
-        /// <param name="durationMicroSec">Pulse duration in microseconds</param>
-        /// <remarks>Changing the period will override the period definition for the motor. It is not recommended to do so.</remarks>
-        public void SetPulse(uint periodMicroSec, uint durationMicroSec)
-        {
-            _definition.PeriodMicroseconds = periodMicroSec;
-            _pulseFrequency = periodMicroSec / 1000.0;
-            _currentPulseWidth = durationMicroSec / 1000.0;
-            _pwmChannel.DutyCyclePercentage = (1 - (_pulseFrequency - _currentPulseWidth) / _pulseFrequency) * 100;
-        }
-
-        /// <summary>
-        /// Is it moving? Return true if the servomotor is currently moving.
-        /// </summary>
-        public bool IsMoving => _currentPulseWidth != 0;
-
-        /// <summary>
-        /// Get the current angle or set to move the motor.
-        /// The minimum angle is always 0, maximum is user defined and by default is 360°.
-        /// The maximum angle is passed in the constructor thru the ServoMotorDefinition.
-        /// When passing 100 as the maximum angle, you will use angle as a percentage 
-        /// from 0 = minimum rotation of the servomotor to 100 = maximum rotation
-        /// </summary>
-        /// <remarks>Motor won't move until Angle is set explicitly.</remarks>
-        public double Angle
-        {
-            get
+            if (maximumPulseWidthMicroseconds < minimumPulseWidthMicroseconds)
             {
-                return _angle;
+                throw new ArgumentOutOfRangeException(nameof(maximumPulseWidthMicroseconds), maximumPulseWidthMicroseconds, $"Value must be greater than or equal to {minimumPulseWidthMicroseconds}.");
             }
-            set
+
+            // slope = (y2 - y1) / (x2 - x1).  x1 is 0 in this case, so it is not subtracted.
+            double servoAngleSlope = (double)(maximumPulseWidthMicroseconds - minimumPulseWidthMicroseconds) / maximumAngle;
+            return servoAngleSlope;
+        }
+
+        /// <summary>
+        /// Starts the servo motor.
+        /// </summary>
+        public void Start() => _pwmChannel.Start();
+
+        /// <summary>
+        /// Stops the servo motor.
+        /// </summary>
+        public void Stop() => _pwmChannel.Stop();
+
+        /// <summary>
+        /// Writes an angle to the servo motor.
+        /// </summary>
+        /// <param name="angle">The angle to write to the servo motor.</param>
+        public void WriteAngle(int angle)
+        {
+            if (angle < 0 || angle > _maximumAngle)
             {
-                double newAngle = Math.Clamp(value, 0, _definition.MaximumAngle);
-                if (newAngle != Angle)
-                {
-                    _angle = newAngle;
-                    Rotate();
-                }
+                throw new ArgumentOutOfRangeException(nameof(angle), angle, $"Value must be between 0 and {_maximumAngle}.");
             }
+
+            WritePulseWidth((int)(_servoAngleSlope * angle + _minimumPulseWidthMicroseconds));
         }
 
         /// <summary>
-        /// Rotate the motor to current <see cref="Angle"/>.
+        /// Writes a pulse width to the servo motor.
         /// </summary>
-        private void Rotate()
+        /// <param name="microseconds">The pulse width, in microseconds, to write to the servo motor.</param>
+        public void WritePulseWidth(int microseconds)
         {
-            double duration = (_definition.MinimumDurationMicroseconds + _rangePerDegree * Angle) / 1000.0;
-            _currentPulseWidth = duration;
-            _pwmChannel.DutyCyclePercentage = (1 - (_pulseFrequency - _currentPulseWidth) / _pulseFrequency) * 100;
-        }
-
-        /// <summary>
-        /// Updates the temporary variables when definition changes.
-        /// </summary>
-        private void UpdateRange()
-        {
-            if (_definition.MaximumAngle != 0)
-                _rangePerDegree = (_definition.MaximumDurationMicroseconds - _definition.MinimumDurationMicroseconds) / _definition.MaximumAngle;
+            double dutyCyclePercentage = (double)microseconds / 1_000_000 * _pwmChannel.Frequency; // Convert to seconds 1st.
+            _pwmChannel.DutyCyclePercentage = dutyCyclePercentage;
         }
 
         public void Dispose()
         {
+            _pwmChannel?.Stop();
             _pwmChannel?.Dispose();
             _pwmChannel = null;
         }
-
-        /// <summary>
-        /// Minimum duration for shortest pulse expressed in microseconds that servo supports.
-        /// </summary>
-        public uint MinimumDurationMicroseconds
-        {
-            get { return _definition.MinimumDurationMicroseconds; }
-            set
-            {
-                if (_definition.MinimumDurationMicroseconds != value)
-                {
-                    _definition.MinimumDurationMicroseconds = value;
-                    UpdateRange();
-                }
-            }
-        }
-
-        /// <summary>
-        /// Maximum duration for longest pulse expressed in microseconds that servo supports.
-        /// </summary>
-        public uint MaximumDurationMicroseconds
-        {
-            get { return _definition.MaximumDurationMicroseconds; }
-            set
-            {
-                if (_definition.MaximumDurationMicroseconds != value)
-                {
-                    _definition.MaximumDurationMicroseconds = value;
-                    UpdateRange();
-                }
-            }
-        }
-
-        /// <summary>
-        /// Period length expressed in microseconds.
-        /// </summary>
-        public uint PeriodMicroseconds
-        {
-            get { return _definition.PeriodMicroseconds; }
-            set
-            {
-                _definition.PeriodMicroseconds = value;
-                if (!IsMoving)
-                    Rotate();
-            }
-        }
-
-
     }
 }
