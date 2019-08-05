fc6e4fb30ec7d6473504a80cf6082e4b3ba6b33b
Greg Ingram <shaggygi97@gmail.com>
2019-08-05 16:26:24 -0400

    Update Servo Motor Binding (#635)
    * Updated folder and files naming
    
    * Cleaned up proj file
    
    * Updated project reference
    
    * Removed unneeded file
    
    * Temp rename
    
    * Rename back from temp
    
    * Simplified and improved logic
    
    * Added test project
    
    * Updated docs
    
    * Removed unneeded Stop method
    
    * Updated angle naming
    
    * Updated markdown spacing
    
    * Removed constructor overload
    
    * Feedback updates
    
    * Made sealed

---
+++ src/devices/ServoMotor/README.md
@@ -0,0 +1,71 @@
+﻿# Servo Motor
+
+## Summary
+
+This is a generic binding to control many servo motors using a hardware or software `PwmChannel`.  Servo motors are usually based on a signal frequency of 50Hz.  They also require a minimum/maximum pulse width to determine the position.  The pulse width is generally between 1 and 2 milliseconds, where 1ms is approximately 0 degrees, 1.5ms is the rest position, and 2ms is 180 degrees.  This information can be found in each servo motor's datasheet.  
+
+One thing to be aware of is the wiring as the servo motor connector is usually a 3-pin connector.  The pinout is shown below where colors can vary.
+
+| Pin Number | Signal         | Color                     |
+|------------|----------------|---------------------------|
+| 1          | Ground         | Black or Brown            |
+| 2          | Power Supply   | Brown or Red              |
+| 3          | Control Signal | Orange or White or Yellow |
+
+## Device Family
+
+There are many servo motor sizes available that offer both standard and continuous rotation.  Below are a few links where to purchase servo motors.
+
+[Adafruit Servo Motor Accessories](https://www.adafruit.com/?q=servo)  
+[Sparkfun Servo Motor Accessories](https://www.sparkfun.com/categories/245)  
+
+## Binding Notes
+
+The `ServoMotor` binding offers an easy way to begin controlling a servo motor.  The quickest approach is to provide the `ServoMotor` object a `PwmChannel` using the default values for other optional arguments.
+
+```csharp
+// Example of hardware PWM using chip 0 and channel 0 on a dev board.
+ServoMotor servoMotor = new ServoMotor(PwmChannel.Create(0, 0, 50));
+servoMotor.Start();  // Enable control signal.
+
+// Move position.  Pulse width argument is in microseconds.
+servoMotor.WritePulseWidth(1000); // 1ms; Approximately 0 degrees.
+servoMotor.WritePulseWidth(1500); // 1.5ms; Approximately 90 degrees.
+servoMotor.WritePulseWidth(2000); // 2ms; Approximately 180 degrees.
+
+servoMotor.Stop(); // Disable control signal.
+```
+
+The position of servo motor can also be adjusted by the angle.  The `ServoMotor` constructor's optional arguments must be set according to device's specs.  NOTE: These are usually an approximation, so you may need to manually tweak to determine exact values.
+
+For example, the [Hitec HS-300BB](https://servodatabase.com/servo/hitec/hs-300bb) servo has the following specifications:
+- MaximumAngle = 180
+- MinimumPulseWidthMicroseconds = 900
+- MaximumPulseWidthMicroseconds = 2100
+- Frequency 50Hz; Period 20000uS
+
+```csharp
+// Example of hardware PWM using chip 0 and channel 0 on a dev board.
+ServoMotor servoMotor = new ServoMotor(
+    PwmChannel.Create(0, 0, 50),
+    180,
+    900,
+    2100);
+
+servoMotor.Start();  // Enable control signal.
+
+// Move position.
+servoMotor.WriteAngle(0); // ~0.9ms; Approximately 0 degrees.
+servoMotor.WritePulseWidth(90); // ~1.5ms; Approximately 90 degrees.
+servoMotor.WritePulseWidth(180); // ~2.1ms; Approximately 180 degrees.
+
+servoMotor.Stop(); // Disable control signal.
+```
+
+## References
+
+[Wikipedia Servo Motor](https://en.wikipedia.org/wiki/Servomotor)  
+[Hobby Servo Tutorial](https://learn.sparkfun.com/tutorials/hobby-servo-tutorial/all)  
+[How Servo Motors Work & How To Control Servos Using Arduino](https://howtomechatronics.com/how-it-works/how-servo-motors-work-how-to-control-servos-using-arduino/)  
+[Arduino Servo Library](https://www.arduino.cc/en/Reference/Servo)  
+[Raspberry Pi Lesson 28: Controlling a Servo on Raspberry Pi with Python](http://www.toptechboy.com/raspberry-pi/raspberry-pi-lesson-28-controlling-a-servo-on-raspberry-pi-with-python/)  
---
+++ src/devices/ServoMotor/ServoMotor.cs
@@ -0,0 +1,103 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Device.Pwm;
+
+namespace Iot.Device.ServoMotor
+{
+    /// <summary>
+    /// Represents a rotary actuator or linear actuator that allows for precise control of angular or linear position.
+    /// </summary>
+    public sealed class ServoMotor : IDisposable
+    {
+        private PwmChannel _pwmChannel;
+        private readonly int _maximumAngle;
+        private readonly int _minimumPulseWidthMicroseconds;
+        private readonly double _angleToMicroseconds;
+
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="ServoMotor"/> class.
+        /// </summary>
+        /// <param name="pwmChannel">The PWM channel used to control the servo motor.</param>
+        /// <param name="maximumAngle">The maximum angle the servo motor can move represented as a value between 0 and 360.</param>
+        /// <param name="minimumPulseWidthMicroseconds">The minimum pulse width, in microseconds, that represent an angle for 0 degrees.</param>
+        /// <param name="maximumPulseWidthMicroseconds">The maxnimum pulse width, in microseconds, that represent an angle for maximum angle.</param>
+        public ServoMotor(
+            PwmChannel pwmChannel,
+            int maximumAngle = 180,
+            int minimumPulseWidthMicroseconds = 1_000,
+            int maximumPulseWidthMicroseconds = 2_000)
+        {
+            _pwmChannel = pwmChannel;
+            _maximumAngle = maximumAngle;
+            _minimumPulseWidthMicroseconds = minimumPulseWidthMicroseconds;
+
+            if (((uint)maximumAngle) > 360)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maximumAngle), maximumAngle, "Value must be between 0 and 360.");
+            }
+
+            if (minimumPulseWidthMicroseconds < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(minimumPulseWidthMicroseconds), minimumPulseWidthMicroseconds, "Value must not be negative.");
+            }
+
+            if (maximumPulseWidthMicroseconds < minimumPulseWidthMicroseconds)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maximumPulseWidthMicroseconds), maximumPulseWidthMicroseconds, $"Value must be greater than or equal to {minimumPulseWidthMicroseconds}.");
+            }
+
+            if (maximumAngle > 0)
+            {
+                // slope = (y2 - y1) / (x2 - x1).  x1 is 0 in this case, so it is not subtracted.
+                _angleToMicroseconds = (double)(maximumPulseWidthMicroseconds - minimumPulseWidthMicroseconds) / maximumAngle;
+            }
+            else
+            {
+                _angleToMicroseconds = 0;
+            }
+        }
+
+        /// <summary>
+        /// Starts the servo motor.
+        /// </summary>
+        public void Start() => _pwmChannel.Start();
+
+        /// <summary>
+        /// Stops the servo motor.
+        /// </summary>
+        public void Stop() => _pwmChannel.Stop();
+
+        /// <summary>
+        /// Writes an angle to the servo motor.
+        /// </summary>
+        /// <param name="angle">The angle to write to the servo motor.</param>
+        public void WriteAngle(int angle)
+        {
+            if (((uint)angle) > _maximumAngle)
+            {
+                throw new ArgumentOutOfRangeException(nameof(angle), angle, $"Value must be between 0 and {_maximumAngle}.");
+            }
+
+            WritePulseWidth((int)(_angleToMicroseconds * angle + _minimumPulseWidthMicroseconds));
+        }
+
+        /// <summary>
+        /// Writes a pulse width to the servo motor.
+        /// </summary>
+        /// <param name="microseconds">The pulse width, in microseconds, to write to the servo motor.</param>
+        public void WritePulseWidth(int microseconds)
+        {
+            double dutyCyclePercentage = (double)microseconds / 1_000_000 * _pwmChannel.Frequency; // Convert to seconds 1st.
+            _pwmChannel.DutyCyclePercentage = dutyCyclePercentage;
+        }
+
+        public void Dispose()
+        {
+            _pwmChannel?.Dispose();
+            _pwmChannel = null;
+        }
+    }
+}
