ad9a4ad5caaff4aeaf355011b509a1e752d528f7
Robin_tty <muellerobin95@gmail.com>
2019-08-06 20:18:40 +0200

    Add license notice

--- src/devices/Bmxx80/FilteringMode/Bme680FilteringMode.cs
+++ src/devices/Bmxx80/FilteringMode/Bme680FilteringMode.cs
@@ -1,4 +1,8 @@
-﻿namespace Iot.Device.Bmxx80.FilteringMode
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Iot.Device.Bmxx80.FilteringMode
 {
     /// <summary>
     /// IIR filter coefficient. The higher the coefficient, the slower the sensors
