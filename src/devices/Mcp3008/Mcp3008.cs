78a22eb43ea6fe5323435c399fb3ace9211db732
Krzysztof Wicher <mordotymoja@gmail.com>
2019-08-07 18:33:16 -0700

    Simplify Mcp3008 implementation

--- src/devices/Mcp3008/Mcp3008.cs
+++ src/devices/Mcp3008/Mcp3008.cs
@@ -12,12 +12,6 @@ namespace Iot.Device.Mcp3008
     {
         private SpiDevice _spiDevice;
 
-        internal enum InputConfiguration
-        {
-            Differential = 0,
-            SingleEnded = 1
-        }
-
         public Mcp3008(SpiDevice spiDevice)
         {
             _spiDevice = spiDevice;
@@ -36,34 +30,29 @@ namespace Iot.Device.Mcp3008
                 throw new ArgumentException("ADC channel must be within 0-7 range.");
             }
 
-            return ReadSpi(channel, InputConfiguration.SingleEnded);
-        }
+            Span<byte> writeBuffer = stackalloc byte[3];
+            Span<byte> readBuffer = stackalloc byte[3];
 
-        private static byte GetConfigurationBits(int channel, InputConfiguration inputConfiguration)
-        {
-            int configurationBits = (0b0001_1000 | channel) << 3;
-
-            if (inputConfiguration == InputConfiguration.Differential)
-            {
-                configurationBits &= 0b1011_1111; // Clear mode bit.
-            }
-
-            return (byte)configurationBits;
-        }
-
-        // Ported: https://github.com/adafruit/Adafruit_Python_MCP3008/blob/master/Adafruit_MCP3008/MCP3008.py
-        private int ReadSpi(int channel, InputConfiguration inputConfiguration)
-        {
-            byte configurationBits = GetConfigurationBits(channel, inputConfiguration);
-            byte[] writeBuffer = new byte[] { configurationBits, 0, 0 };
-            byte[] readBuffer = new byte[3];
-
+            // first we send 5 bits, rest is ignored
+            // start bit = 1
+            // conversion bit = 1 (single ended)
+            // next 3 bits are channel
+            writeBuffer[0] = (byte)((0b1_1000 | channel) << 3);
             _spiDevice.TransferFullDuplex(writeBuffer, readBuffer);
 
-            int result = (readBuffer[0] & 0b0000_0001) << 9;
-            result |= (readBuffer[1] & 0b1111_1111) << 1;
-            result |= (readBuffer[2] & 0b1000_0000) >> 7;
-            result = result & 0b0011_1111_1111;
+            // we ignore first 7 bits then read following 10
+            // 7 bits:
+            // - 5 are used for writing start bit, conversion bit and channel
+            // - next bit is ignored so device has time to sample
+            // - following bit should be 0
+            if ((readBuffer[0] & 0b10) != 0)
+            {
+                throw new InvalidOperationException("Invalid data was read from the sensor");
+            }
+
+            int result = (readBuffer[0] & 1) << 9;
+            result |= readBuffer[1] << 1;
+            result |= readBuffer[2] >> 7;
             return result;
         }
     }
