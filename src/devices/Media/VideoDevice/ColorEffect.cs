dfef5de348eae59b09950f0e22fc6997475efe15
张悦欣 <zhangyuexin121@live.cn>
2019-08-13 20:24:05 +0800

    add license

--- src/devices/Media/VideoDevice/ColorEffect.cs
+++ src/devices/Media/VideoDevice/ColorEffect.cs
@@ -1,4 +1,8 @@
-﻿namespace Iot.Device.Media
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Iot.Device.Media
 {
     /// <summary>
     /// The color effect of a video device.
