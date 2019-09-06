2f68b695327ecd97d120eb47c2c72cdbaa2b6af3
Zhang Yuexin <zhangyuexin121@live.cn>
2019-09-07 01:06:54 +0800

    Add FM receiver TEA5767 (#697)
    * add tea5767
    
    * cleanup
    
    * fix dispose error
    
    * add SearchDirection and exception
    
    * Update src/devices/RadioReceiver/Devices/Tea5767/README.md
    
    Co-Authored-By: Krzysztof Wicher <mordotymoja@gmail.com>

---
+++ src/devices/RadioReceiver/samples/Program.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Device.I2c;
+using Iot.Device.RadioReceiver;
+
+namespace Tea5767Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            I2cConnectionSettings settings = new I2cConnectionSettings(1, Tea5767.DefaultI2cAddress);
+            I2cDevice device = I2cDevice.Create(settings);
+
+            using (Tea5767 radio = new Tea5767(device, FrequencyRange.Other, 103.3))
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
