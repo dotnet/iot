4457ca3ae1f715a6e5c65e7eaefa75efb30f5168
Robin Müller <muellerobin95@gmail.com>
2019-09-16 20:19:12 +0200

    Add Bh1745 device binding (#511)
    * Fix compensate_temperatur formula
    
    * Remove use of Math.Pow
    
    * Add device driver
    
    * Add samples
    
    * Some formatting and project settings fixes
    
    * Fix build error
    
    * Fix bit manipulation and build error
    
    * Add missing xml documentation
    
    * proper indentation
    
    * fix indentation
    
    * Use caching, convert appropriate properties to methods
    
    * Remove unnecessary default configuration
    
    * fix stackalloc use
    
    * Use project references, update sample
    
    * Update second sample, minor improvements
    
    * Fix reset method
    
    * formatting

---
+++ src/devices/Bh1745/ChannelCompensationMultipliers.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Iot.Device.Bh1745
+{
+    /// <summary>
+    /// Channel compensation multipliers used to compensate the 4 color channels of the Bh1745.
+    /// </summary>
+    public class ChannelCompensationMultipliers
+    {
+        /// <summary>
+        /// Multiplier for the red color channel.
+        /// </summary>
+        public double Red { get; set; }
+
+        /// <summary>
+        /// Multiplier for the green color channel.
+        /// </summary>
+        public double Green { get; set; }
+
+        /// <summary>
+        /// Multiplier for the blue color channel.
+        /// </summary>
+        public double Blue { get; set; }
+
+        /// <summary>
+        /// Multiplier for the clear color channel.
+        /// </summary>
+        public double Clear { get; set; }
+
+    }
+}
