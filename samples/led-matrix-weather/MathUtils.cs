ea5f2d6f8991d9b06edec8392ce389e3742c8803
Krzysztof Wicher <mordotymoja@gmail.com>
2019-09-22 09:58:35 -0700

    Add led-matrix-weather sample

---
+++ samples/led-matrix-weather/MathUtils.cs
@@ -0,0 +1,151 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Iot.Device.LEDMatrix;
+using Iot.Device.Graphics;
+using System.Drawing;
+using System.Numerics;
+
+namespace LedMatrixWeather
+{
+    static class MathUtils
+    {
+        public const float pi = (float)Math.PI;
+
+        // Some of these names are lowercase to match names used in shading languages.
+        // Most of these are used in code similar to fragment shaders
+        // and it makes it easier to convert shading language to C# graphics
+        public static Vector3 clamp(Vector3 c, float a, float b)
+        {
+            return new Vector3(Math.Clamp(c.X, a, b), Math.Clamp(c.Y, a, b), Math.Clamp(c.Z, a, b));
+        }
+
+        public static float mod(float a, float b)
+        {
+            float ret = a % b;
+            if (ret < 0)
+            {
+                ret += b;
+            }
+
+            return ret;
+        }
+
+        public static Vector3 mod(Vector3 a, float b)
+        {
+            return new Vector3(mod(a.X, b), mod(a.Y, b), mod(a.Z, b));
+        }
+
+        public static Vector3 Add(Vector3 v, float s)
+        {
+            return new Vector3(v.X + s, v.Y + s, v.Z + s);
+        }
+
+        public static Vector3 abs(Vector3 vector)
+        {
+            return new Vector3(Math.Abs(vector.X), Math.Abs(vector.Y), Math.Abs(vector.Z));
+        }
+
+        public static Vector3 mix(Vector3 a, Vector3 b, float f)
+        {
+            return a * (1 - f) + b * f;
+        }
+
+        public static Vector3 hsv2rgb_smooth(Vector3 c)
+        {
+            float c1 = c.X + 6.0f;
+
+            Vector3 v1 = Add(new Vector3(0.0f, 4.0f, 2.0f), c.X * 6.0f);
+            Vector3 rgb = clamp(Add(abs(Add(mod(v1, 6.0f), -3.0f)), -1.0f), 0.0f, 1.0f);
+
+            rgb = rgb*rgb*(Add(-2.0f * rgb, 3.0f)); // cubic smoothing
+
+            return c.Z * mix(new Vector3(1.0f, 1.0f, 1.0f), rgb, c.Y);
+        }
+
+        public static float smoothstep(float edge0, float edge1, float x)
+        {
+            // Scale, bias and saturate x to 0..1 range
+            x = Math.Clamp((x - edge0) / (edge1 - edge0), 0.0f, 1.0f);
+            // Evaluate polynomial
+            return x * x * (3 - 2 * x);
+        }
+
+        public static Vector3 HSV(Vector2 uv, float time)
+        {
+            Vector2 p = uv - new Vector2(0.5f, 0.5f);
+            float a = (float)(Math.Atan2(p.Y, p.X) / 2f / Math.PI);
+
+            a = mod(a + time / 10.0f, 1.0f);
+
+            float r = p.Length();
+
+            float ha = a;
+            float h = mod(ha, 1.0f);
+
+            float s = 1.0f;
+            float v = r * 2.0f;
+
+            return hsv2rgb_smooth(new Vector3(a, s, v));
+        }
+
+        public static byte Col(float x)
+        {
+            x *= 255f;
+            x = Math.Clamp(x, 0f, 255f);
+            return (byte)x;
+        }
+
+        public static byte Col(double x, double d, double e)
+        {
+            x *= e;
+            x = Math.Pow(x, d);
+            x = Math.Clamp(x, 0.0f, 1.0f);
+            return (byte)(x * 255);
+        }
+
+        public static byte ColR(double x)
+        {
+            return Col(x, 1.9, 0.95);
+        }
+
+        public static byte ColG(double x)
+        {
+            return Col(x, 1.9, 0.95);
+        }
+
+        public static byte ColB(double x)
+        {
+            return Col(x, 1.9, 0.95);
+        }
+
+        public static Color ToSRGB(double x, double y, double z)
+        {
+            return Color.FromArgb(
+                ColR(x),
+                ColG(y),
+                ColB(z));
+        }
+
+        public static Color ColorFromVec3(Vector3 v)
+        {
+            return Color.FromArgb(Col(v.X), Col(v.Y), Col(v.Z));
+        }
+
+        public static Vector2 Rot(Vector2 uv, float angle)
+        {
+            float s = (float)Math.Sin(angle);
+            float c = (float)Math.Cos(angle);
+            return new Vector2(
+                uv.X * c + uv.Y * s,
+                uv.X * s - uv.Y * c
+            );
+        }
+    }
+}
---
+++ samples/led-matrix-weather/Program.Drawings.cs
@@ -0,0 +1,214 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Iot.Device.LEDMatrix;
+using Iot.Device.Graphics;
+using System.Drawing;
+using System.Numerics;
+
+namespace LedMatrixWeather
+{
+    using static MathUtils;
+    
+    partial class Program
+    {
+        static float Line(Vector2 uv, float len)
+        {
+            if (uv.Y < 0)
+            {
+                return Math.Max(-uv.Y, Math.Abs(uv.X));
+            }
+            else if (uv.Y > len)
+            {
+                return Math.Max(uv.Y - len, Math.Abs(uv.X));
+            }
+            else
+            {
+                return Math.Abs(uv.X);
+            }
+        }
+
+        static Vector3 Clock(Vector2 uv, DateTimeOffset time)
+        {
+            uv -= new Vector2(0.5f, 0.5f);
+
+            float outerRadius = 0.47f;
+            float outerRadiusDist = Math.Abs(uv.Length() - outerRadius);
+            float outerCircle = 1.0f - smoothstep(0f, 0.025f, outerRadiusDist);
+
+            float innerRadius = 0.04f;
+            float innerRadiusDist = uv.Length() - innerRadius;
+
+            float secondsAngle = 2f * pi * ((float)time.Second + time.Millisecond / 1000f) / 60f;
+            float minutesAngle = 2f * pi * (float)time.Minute / 60f + secondsAngle / 60f;
+            float hoursAngle = 2f * pi * (float)(time.Hour % 12) / 12f + minutesAngle / 60f;
+
+            float secondsLine = 1.0f - smoothstep(0f, 0.025f, Line(Rot(uv, secondsAngle), 0.4f));
+            float minutesLine = 1.0f - smoothstep(0f, 0.025f, Line(Rot(uv, minutesAngle), 0.35f));
+            float hoursLine = 1.0f - smoothstep(0f, 2f * 0.025f, Line(Rot(uv, hoursAngle), 0.2f));
+
+            return mix(
+                new Vector3(1, 0, 0),
+                new Vector3(Math.Max(outerCircle, secondsLine), minutesLine, hoursLine),
+                smoothstep(0f, 0.01f, innerRadiusDist));
+        }
+
+        static Vector3 OpenWeatherIcon(Vector2 uv, string icon, float time)
+        {
+            uv -= new Vector2(0.5f, 0.5f);
+
+            switch (icon)
+            {
+                case "01d":
+                case "01d.org":
+                    return IconSunny(uv, time);
+                case "01n":
+                    return IconMoon(uv, time);
+                case "02d":
+                    return IconPartiallySunny(uv, time);
+                case "02n": // partially cloudy at night
+                    return IconMoon(uv, time);
+                case "03d": // clouds day
+                case "03n": // clouds night
+                case "04d": // overcast clouds
+                case "04n":
+                case "11d": // with thunder
+                case "11n":
+                case "13d": // with snow
+                case "13n":
+                case "50d": // no clue what the picture represents but kinda looks like a cloud
+                case "50n":
+                    return IconClouds(uv, time);
+                case "09d":
+                case "09n":
+                case "10d": // with a bit of the sun
+                case "10n":
+                    return IconRain(uv, time);
+                default:
+                {
+                    // Don't know the icon
+                    // We're in Seattle so let's assume rain
+                    return IconRain(uv, time);
+                }
+            }
+        }
+
+        static Vector3 IconSunny(Vector2 uv, float time)
+        {
+            const int NumberOfRays = 10;
+            const float OuterRadius = 0.45f;
+            const float InnerRadius = 0.25f;
+
+            const float RayLength = OuterRadius - InnerRadius;
+
+            // spiral and rotate everything a bit
+            uv = Rot(uv, uv.Length() * 2 * 2 * pi / 10f * (float)Math.Sin(2 * pi * time / 9.0f) - time / 2.0f);
+            float angle = (float)Math.Atan2(uv.Y, uv.X) / 2 / pi * NumberOfRays;
+
+            // for soft rays: (float)Math.Sin(2 * pi * angle);
+            float rayShape = 2 * Math.Abs(mod(angle, 1.0f) - 0.5f);
+            float rayLen = RayLength * rayShape;
+            float radius = InnerRadius + rayLen;
+            float radiusDist = uv.Length() - radius;
+            return mix(
+                new Vector3(1, 1, 0),
+                new Vector3(0, 0, 0),
+                smoothstep(0f, 0.02f, radiusDist));
+        }
+
+        static float Clouds(Vector2 uv, float time)
+        {
+            float r0 = 0.15f;
+            Vector2 p0 = new Vector2(-0.2f, 0.1f + 0.05f * (float)Math.Sin(2 * pi * time / 7f + 0.1f));
+            float c0 = (uv - p0).Length() - r0;
+
+            float r1 = 0.15f + 0.05f * (float)Math.Sin(2 * pi * time / 13f);
+            Vector2 p1 = new Vector2(0f, 0.08f * (float)Math.Sin(2 * pi * time / 5f + 0.7f));
+            float c1 = (uv - p1).Length() - r1;
+
+            float r2 = 0.15f;
+            Vector2 p2 = new Vector2(0.2f, 0.05f - 0.05f * (float)Math.Sin(2 * pi * time / 4f + 0.9f));
+            float c2 = (uv - p2).Length() - r2;
+
+            float clouds = Math.Min(Math.Min(c0, c1), c2);
+            return smoothstep(0f, 0.2f, clouds + 0.05f);
+        }
+
+        static readonly Vector3 s_cloudsColor = new Vector3(0.3f, 0.3f, 0.3f);
+        static Vector3 IconClouds(Vector2 uv, float time)
+        {
+            float clouds = Clouds(uv, time);
+
+            return mix(
+                s_cloudsColor,
+                new Vector3(0, 0, 0),
+                clouds);
+        }
+
+        static Vector3 Rain(Vector2 uv, float time)
+        {
+            if (uv.Y < -0.1 || uv.X <= -0.4 || uv.X >= 0.4)
+            {
+                return new Vector3(0, 0, 0);
+            }
+
+            uv.Y -= time / 3f;
+
+            float dropSpeed = 2 + 0.5f * (float)Math.Sin(2 * pi * uv.X * 77f);
+            float dropNoise = 11 * uv.X + (float)Math.Sin(2 * pi * uv.X * 100f);
+            float rain = smoothstep(0f, 0.05f, 
+                (float)Math.Sin(2 * pi * (dropNoise + dropSpeed * uv.Y)) - 0.9f);
+
+            return mix(
+                new Vector3(0, 0, 0),
+                new Vector3(0, 0, 0.3f),
+                rain);
+        }
+
+        static Vector3 IconRain(Vector2 uv, float time)
+        {
+            float clouds = Clouds(uv - new Vector2(0f, -0.3f), time);
+            return mix(
+                s_cloudsColor,
+                Rain(uv, time),
+                clouds);
+        }
+
+        static Vector3 IconMoon(Vector2 uv, float time)
+        {
+            uv -= new Vector2(0.3f, 0.0f);
+            uv = Rot(uv, 2 * pi / 128 * (float)Math.Sin(2 * pi * time / 3f));
+            uv += new Vector2(0.35f, 0.0f);
+
+            float r0 = 0.3f;
+            Vector2 p0 = new Vector2(0.0f, 0.0f);
+            float c0 = (uv - p0).Length() - r0;
+
+            float r1 = 0.35f;
+            Vector2 p1 = new Vector2(0.20f, 0.0f);
+            float c1 = (uv - p1).Length() - r1;
+
+            float moon = Math.Max(c0, -c1);
+            return mix(
+                new Vector3(0.5f, 0.5f, 0.5f),
+                new Vector3(0, 0, 0),
+                smoothstep(0f, 0.07f, moon));
+            //return IconRain(uv, time);//new Vector3(0, 0, 0);
+        }
+
+        static Vector3 IconPartiallySunny(Vector2 uv, float time)
+        {
+            float clouds = Clouds(uv * 0.85f - new Vector2(0f, 0.15f), time);
+            return mix(
+                s_cloudsColor,
+                IconSunny(uv, time),
+                clouds) * 0.6f;
+        }
+    }
+}
