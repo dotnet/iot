8e85dfc3d7d3a5812b98bfe41dfd9cf4f612a292
Zhang Yuexin <zhangyuexin121@live.cn>
2019-09-30 23:40:16 +0800

    Using V4L2 to realize VideoDevice (#664)
    * draft VideoDevice
    
    * change namespace and add some new settings
    
    * add some settings, common pixels converter, async method
    
    * add license
    
    * fix build error
    
    * resolve comment
    
    * add README.md
    
    * remove xml comment
    
    * update README.md
    
    * edit xml comment
    
    * add Dockerfile
    
    * Update Dockerfile
    
    * remove dockerfile and use current directory
    
    * remove unused interop, add xml docs
    
    * fix docs error
    
    * add docs
    
    * catch TaskCanceledException
    
    * remove async method
    
    * update sample
    
    * move interop and add readme
    
    * fix doc error
    
    * fix some issues
    
    * Update README.md

---
+++ src/devices/Media/README.md
@@ -0,0 +1,15 @@
+# Media
+The project currently includes `VideoDevice`.
+
+## Precondition
+
+### VideoDevice
+
+1. Install `v4l-utils`.
+    ```
+    sudo apt-get install v4l-utils
+    ```
+2. Install `System.Drawing` native dependencies.
+    ```
+    sudo apt-get install libc6-dev libgdiplus libx11-dev
+    ```
---
+++ src/devices/Media/VideoDevice/VideoDeviceValue.cs
@@ -0,0 +1,42 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Iot.Device.Media
+{
+    /// <summary>
+    /// The default and current values of a video device's control.
+    /// </summary>
+    public class VideoDeviceValue
+    {
+        /// <summary>
+        /// Control's name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Minimum value
+        /// </summary>
+        public int Minimum { get; set; }
+
+        /// <summary>
+        /// Maximum value
+        /// </summary>
+        public int Maximum { get; set; }
+
+        /// <summary>
+        /// Value change step size
+        /// </summary>
+        public int Step { get; set; }
+
+        /// <summary>
+        /// Control's default value
+        /// </summary>
+        public int DefaultValue { get; set; }
+
+        /// <summary>
+        /// Control's current value
+        /// </summary>
+        public int CurrentValue { get; set; }
+    }
+}
