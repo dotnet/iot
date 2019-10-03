01616949dddb091ed84618b40f3e5fe3b4ed8f1d
Andrew Gould <Frankenslag@users.noreply.github.com>
2019-10-03 17:07:25 +0100

    Addition of allowing writes to /dev/mem if /dev/gpiomem is not a… (#595)
    * Addition of allowing writes to /dev/mem
    
    * Added the use of the libbcm_host library to determine the peripheral base address.
    
    * Added license text for raspi-gpio.
    
    * Added URL to third party licence text
    
    * Minor and NIT changes.
    
    * Constants and comments added.

---
+++ src/System.Device.Gpio/Interop/Unix/libbcm_host/Interop.libbcmhost.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+
+using System;
+using System.Runtime.InteropServices;
+
+internal partial class Interop
+{
+    internal partial class libbcmhost
+    {
+        private const string LibbcmhostLibrary = "libbcm_host";
+
+        /// <summary>
+        /// Get the peripheral base address of a RaspberryPi.
+        /// </summary>
+        [DllImport(LibbcmhostLibrary, SetLastError = true)]
+        internal static extern uint bcm_host_get_peripheral_address();
+    }
+}
