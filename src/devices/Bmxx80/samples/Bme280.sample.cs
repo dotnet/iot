d6b78d5fffa894cabea83437e2710dc5592e722a
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2019-10-05 11:44:14 +0200

    Update Bme280.sample.cs

--- src/devices/Bmxx80/samples/Bme280.sample.cs
+++ src/devices/Bmxx80/samples/Bme280.sample.cs
@@ -20,7 +20,7 @@ namespace Iot.Device.Samples
             //bus id on the raspberry pi 3
             const int busId = 1;
             //set this to the current sea level pressure in the area for correct altitude readings
-            const double defaultSeaLevelPressure = 1033.00;
+            const double defaultSeaLevelPressure = Iot.Units.Pressure.MeanSeaLevelPressure;
 
             var i2cSettings = new I2cConnectionSettings(busId, Bme280.DefaultI2cAddress);
             var i2cDevice = I2cDevice.Create(i2cSettings);
