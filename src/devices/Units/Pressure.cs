f03741c9542f48d90ea28b287b62a6bc2fda08f7
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2019-10-06 08:29:01 +0200

    Update Pressure.cs

--- src/devices/Units/Pressure.cs
+++ src/devices/Units/Pressure.cs
@@ -9,59 +9,59 @@ namespace Iot.Units
     /// </summary>
     public struct Pressure
     {
-        private const double MbarRatio = 1.0;
-        private const double KpaRatio = 0.1;
-        private const double PaRatio = 0.01;
-        private const double InhgRatio = 0.0295301;
-        private const double MmhgRatio = 0.750062;
-        private double _hpa;
+        private const double MbarRatio = 0.01;
+        private const double KpaRatio = 0.001;
+        private const double HpaRatio = 0.01;
+        private const double InhgRatio = 0.000295301;
+        private const double MmhgRatio = 0.00750062;
+        private long _pa;
         
-        private Pressure(double hpa)
+        private Pressure(long pa)
         {
-            _hpa = hpa;
+            _pa = pa;
         }
         
         /// <summary>
         /// The mean sea-level pressure (MSLP) is the average atmospheric pressure at mean sea level.
         /// </summary>
-        public static Pressure MeanSeaLevel => Pressure.FromHpa(1013.25);
-        
-        /// <summary>
-        /// Pressure in hPa
-        /// </summary>
-        public double Hpa => _hpa;
-        
-        /// <summary>
-        /// Pressure in mbar
-        /// </summary>
-        public double Mbar => MbarRatio * _hpa;
-        
-        /// <summary>
-        /// Pressure in kPa
-        /// </summary>
-        public double Kpa => KpaRatio * _hpa;
+        public static Pressure MeanSeaLevel => Pressure.FromPa(101325);
         
         /// <summary>
         /// Pressure in Pa
         /// </summary>
-        public double Pa => PaRatio * _hpa;
+        public long Pa => _pa;
+        
+        /// <summary>
+        /// Pressure in mbar
+        /// </summary>
+        public double Mbar => MbarRatio * _pa;
+        
+        /// <summary>
+        /// Pressure in kPa
+        /// </summary>
+        public double Kpa => KpaRatio * _pa;
+        
+        /// <summary>
+        /// Pressure in hPa
+        /// </summary>
+        public double Hpa => HpaRatio * _pa;
         
         /// <summary>
         /// Pressure in inHg
         /// </summary>
-        public double Inhg => InhgRatio * _hpa;
+        public double Inhg => InhgRatio * _pa;
         
         /// <summary>
         /// Pressure in mmHg
         /// </summary>
-        public double Mmhg => MmhgRatio * _hpa;
+        public double Mmhg => MmhgRatio * _pa;
         
         /// <summary>
-        /// Creates Pressure instance from pressure in hPa
+        /// Creates Pressure instance from pressure in Pa
         /// </summary>
-        /// <param name="value">Pressure value in hPa</param>
+        /// <param name="value">Pressure value in Pa</param>
         /// <returns>Pressure instance</returns>
-        public static Pressure FromHpa(double value)
+        public static Pressure FromPa(long value)
         {
             return new Pressure(value);
         }
@@ -84,6 +84,16 @@ namespace Iot.Units
         public static Pressure FromKpa(double value)
         {
             return new Pressure(value / KpaRatio);
+        }
+        
+        /// <summary>
+        /// Creates Pressure instance from pressure in hPa
+        /// </summary>
+        /// <param name="value">Pressure value in hPa</param>
+        /// <returns>Pressure instance</returns>
+        public static Pressure FromHpa(double value)
+        {
+            return new Pressure(value / HpaRatio);
         }
         
         /// <summary>
