937e8cf275cade8c09bcf7954ae8e2f4123c1ed4
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2019-10-06 07:58:07 +0200

    Update Pressure.cs

--- src/devices/Units/Pressure.cs
+++ src/devices/Units/Pressure.cs
@@ -5,13 +5,99 @@
 namespace Iot.Units
 {
     /// <summary>
-    /// Class representing pressure
+    /// Structure representing pressure
     /// </summary>
-    public static class Pressure
+    public struct Pressure
     {
         /// <summary>
-        /// The mean sea-level pressure (MSLP) is the average atmospheric pressure at mean sea level.
+        /// The mean sea-level pressure (MSLP), in hPa, is the average atmospheric pressure at mean sea level.
         /// </summary>
-        public const double MeanSeaLevelPressure = 1013.25;
+        public const double MeanSeaLevel = 1013.25;
+        
+        private const double MbarRatio = 1.0;
+        private const double KpaRatio = 0.1;
+        private const double InhgRatio = 0.0295301;
+        private const double MmhgRatio = 0.750062;
+        private double _hpa;
+        
+        private Pressure(double hpa)
+        {
+            _hpa = hpa;
+        }
+        
+        /// <summary>
+        /// Pressure in hPa
+        /// </summary>
+        public double Hpa => _hpa;
+        
+        /// <summary>
+        /// Pressure in mbar
+        /// </summary>
+        public double Mbar => MbarRatio * _hpa;
+        
+        /// <summary>
+        /// Pressure in kPa
+        /// </summary>
+        public double Kpa => KpaRatio * _hpa;
+        
+        /// <summary>
+        /// Pressure in inHg
+        /// </summary>
+        public double Inhg => InhgRatio * _hpa;
+        
+        /// <summary>
+        /// Pressure in mmHg
+        /// </summary>
+        public double Mmhg => MmhgRatio * _hpa;
+        
+        /// <summary>
+        /// Creates Pressure instance from pressure in hPa
+        /// </summary>
+        /// <param name="value">Pressure value in hPa</param>
+        /// <returns>Pressure instance</returns>
+        public static Pressure FromHpa(double value)
+        {
+            return new Pressure(value);
+        }
+        
+        /// <summary>
+        /// Creates Pressure instance from pressure in mbar
+        /// </summary>
+        /// <param name="value">Pressure value in mbar</param>
+        /// <returns>Pressure instance</returns>
+        public static Pressure FromMbar(double value)
+        {
+            return new Pressure(value / MbarRatio);
+        }
+        
+        /// <summary>
+        /// Creates Pressure instance from pressure in kPa
+        /// </summary>
+        /// <param name="value">Pressure value in kPa</param>
+        /// <returns>Pressure instance</returns>
+        public static Pressure FromKpa(double value)
+        {
+            return new Pressure(value / KpaRatio);
+        }
+        
+        /// <summary>
+        /// Creates Pressure instance from pressure in inHg
+        /// </summary>
+        /// <param name="value">Pressure value in inHg</param>
+        /// <returns>Pressure instance</returns>
+        public static Pressure FromInhg(double value)
+        {
+            return new Pressure(value / InhgRatio);
+        }
+        
+        /// <summary>
+        /// Creates Pressure instance from pressure in mmHg
+        /// </summary>
+        /// <param name="value">Pressure value in mmHg</param>
+        /// <returns>Pressure instance</returns>
+        public static Pressure FromMmhg(double value)
+        {
+            return new Pressure(value / MmhgRatio);
+        }
     }
 }
