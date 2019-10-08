311019a6e9a1bb2a0831123e035121ff914e5df5
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2019-10-08 07:55:09 +0200

    Update PressureAndTemperature.Sample.cs

--- src/devices/SenseHat/samples/PressureAndTemperature.Sample.cs
+++ src/devices/SenseHat/samples/PressureAndTemperature.Sample.cs
@@ -19,7 +19,7 @@ namespace Iot.Device.SenseHat.Samples
             {
                 while (true)
                 {
-                    Console.WriteLine($"Temperature: {th.Temperature.Celsius}°C   Humidity: {th.Pressure.HectoPascal}hPa");
+                    Console.WriteLine($"Temperature: {th.Temperature.Celsius}°C   Humidity: {th.Pressure.Hectopascal}hPa");
                     Thread.Sleep(1000);
                 }
             }
