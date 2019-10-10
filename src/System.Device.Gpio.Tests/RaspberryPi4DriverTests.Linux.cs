f25946f2151cbdb7fed55309fc00172dc573ba32
Patrick Grawehr <pgrawehr@hotmail.com>
2019-10-10 21:31:02 +0200

    Add tests for Pull mode driver

---
+++ src/System.Device.Gpio.Tests/RaspberryPi4DriverTests.Linux.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Gpio.Drivers;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace System.Device.Gpio.Tests
+{
+	public class RaspberryPi4DriverTests : GpioControllerTestBase
+	{
+        /// <summary>
+		/// Leave this pin open (unconnected) for the tests
+		/// </summary>
+		private const int OpenPin = 23;
+
+		protected override GpioDriver GetTestDriver()
+		{
+			return new RaspberryPi4Driver();
+		}
+
+		protected override PinNumberingScheme GetTestNumberingScheme() => PinNumberingScheme.Logical;
+
+        [Fact]
+        public void InputPullResistorsWork()
+		{
+			using (GpioController controller = new GpioController(GetTestNumberingScheme(), GetTestDriver()))
+			{
+				controller.OpenPin(OpenPin, PinMode.InputPullUp);
+				Thread.Sleep(10);
+				Assert.Equal(PinValue.High, controller.Read(OpenPin));
+				controller.SetPinMode(OpenPin, PinMode.InputPullDown);
+				Assert.Equal(PinValue.Low, controller.Read(OpenPin));
+				controller.SetPinMode(OpenPin, PinMode.InputPullUp);
+				Assert.Equal(PinValue.High, controller.Read(OpenPin));
+			}
+		}
+	}
+}
