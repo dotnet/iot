4c2aec9d48b470630e5baeacd9ce9b70c3081be1
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2019-10-11 21:15:23 +0200

    Update Bmp180.cs

--- src/devices/Bmp180/Bmp180.cs
+++ src/devices/Bmp180/Bmp180.cs
@@ -123,7 +123,7 @@ namespace Iot.Device.Bmp180
         [ObsoleteAttribute("ReadAltitude is obsolete. Please use Iot.Device.Common.WeatherHelper instead.")]
         public Pressure ReadSeaLevelPressure(double altitude = 0.0)
         {
-            return Pressure.FromPascal(WeatherHelper.SeaLevelPressure(ReadPressure(), altitude, ReadTemperature());
+            return Pressure.FromPascal(WeatherHelper.SeaLevelPressure(ReadPressure(), altitude, ReadTemperature()));
         }
 
         /// <summary>
