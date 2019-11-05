7161067ac45b1c78b741756d36b99bf3a3423d5e
nahueltaibo <nahueltaibo@gmail.com>
2019-11-04 23:09:54 -0500

    MotorHat clean device binding from template

---
+++ src/devices/MotorHat/MotorHat.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Device.Gpio;
+using System.Device.I2c;
+using System.Device.Spi;
+
+namespace Iot.Device.MotorHat
+{
+    public class MotorHat : IDisposable
+    {
+        public MotorHat()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
