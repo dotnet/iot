8132b852143c42c321e08a76fff394fda69fca0b
nahueltaibo <nahueltaibo@gmail.com>
2019-11-04 23:35:23 -0500

    Added Iot.Device.Pwm.Pca9685 reference

--- src/devices/MotorHat/MotorHat.cs
+++ src/devices/MotorHat/MotorHat.cs
@@ -2,19 +2,36 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Iot.Device.Pwm;
 using System;
-using System.Device.Gpio;
-using System.Device.I2c;
-using System.Device.Spi;
 
 namespace Iot.Device.MotorHat
 {
+    /// <summary>
+    /// Raspberry Pi Motor Controller Hat based on PCA9685 PWM controller
+    /// </summary>
     public class MotorHat : IDisposable
     {
+        /// <summary>
+        /// Default I2C address of Motor Hat
+        /// </summary>
+        public const int I2cAddressBase = 0x60;
+
+        /// <summary>
+        /// Motor Hat is built on top of a PCa9685
+        /// </summary>
+        private readonly Pca9685 pca9685;
+
+        /// <summary>
+        /// 
+        /// </summary>
         public MotorHat()
         {
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public void Dispose()
         {
         }
