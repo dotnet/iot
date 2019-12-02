e51091b68ae9f36cb80e2855359a9e5ddcb050d7
ibebbs <github@bebbs.co.uk>
2019-12-02 13:41:48 +0000

    Added sample and readme

---
+++ src/devices/Pigpio/ReadMe.md
@@ -0,0 +1,24 @@
+﻿# Pigpio
+
+This GpioDriver implementation allows you to remotely control a Raspberry Pi's GPIO pins via a wired or wireless network connection. It is compatible with (at time of writing) every Raspberry Pi including Model A, A+, B, B+, Zero, ZeroW, Pi2B, Pi3B (Pi4 support is currently experimental).
+
+## Setting up the Raspberry Pi
+
+1. Install a recent version of Raspbian onto the Pi; Stretch or Buster is fine.
+2. Make sure the Pi has network access so either via a wired or wireless connection
+3. Configured the raspberry pi to allow remote GPIO control by following the steps in section 4.1 [here](https://gpiozero.readthedocs.io/en/stable/remote_gpio.html#preparing-the-raspberry-pi).
+
+And you're done.
+
+## Running the sample
+
+The sample application (Pigpio.Sample) will periodically set GPIO4 high then low. If you connected an LED (with a current limiting resistor) to GPIO4 you then run the application you should see if turn on or off every second.
+
+To run the sample you'll need to determine the IP address of the Pi you configured above and specify it as the first argument to the application; for example:
+
+```
+Pigpio.Sample.exe 192.168.1.101
+```
+
+If all goes to plan, you should see something like [this](https://www.youtube.com/watch?v=F9m0fqZjOGQ)
+
---
+++ src/devices/Pigpio/samples/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Pigpio.Sample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please supply the IP Address of the Raspberry Pi to connect to");
+            }
+            else if (IPAddress.TryParse(args[0], out IPAddress ipAddress))
+            {
+                using (var driver = new Iot.Device.Pigpio.Driver(new IPEndPoint(ipAddress, 8888)))
+                {
+                    await driver.ConnectAsync();
+
+                    // Line above doesn't current wait for connection so
+                    // delay here to give the socket time to get connected
+                    await Task.Delay(TimeSpan.FromSeconds(2));
+
+                    using (GpioController controller = new GpioController(PinNumberingScheme.Logical, driver))
+                    {
+                        controller.OpenPin(4);
+                        controller.SetPinMode(4, PinMode.Output);
+
+                        var value = 0;
+                        while (true)
+                        {
+                            value = (value + 1) % 2;
+
+                            PinValue pinValue = (PinValue)value;
+
+                            Console.WriteLine($"Writing PinValue of '{pinValue}' to pin '4'");
+
+                            controller.Write(4, pinValue);
+
+                            await Task.Delay(TimeSpan.FromSeconds(1));
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("The specified IP address is invalid.");
+            }
+        }
+    }
+}
