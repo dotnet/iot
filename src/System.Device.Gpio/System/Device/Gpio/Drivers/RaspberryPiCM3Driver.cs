2ea4740a8cf68ef564cce66977ff6ad9683d3ea0
JTrotta <gerardo.trotta@euronet.aero>
2020-04-02 11:49:28 +0200

    Added Board class and CM3 Driver

---
+++ src/System.Device.Gpio/System/Device/Gpio/Drivers/RaspberryPiCM3Driver.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Device.Gpio.Drivers
+{
+    /// <summary>
+    /// A GPIO driver for the Raspberry Pi 3.
+    /// </summary>
+    public partial class RaspberryPiCM3Driver // Different base classes declared in RaspberryPi3Driver.Linux.cs and RaspberryPi3Driver.Windows.cs
+    {
+        /// <summary>
+        /// Raspberry CM3 has 28 GPIO pins.
+        /// </summary>
+        protected internal override int PinCount => 48;
+
+        private void ValidatePinNumber(int pinNumber)
+        {
+            if (pinNumber < 0 || pinNumber > PinCount)
+            {
+                throw new ArgumentException("The specified pin number is invalid.", nameof(pinNumber));
+            }
+        }
+
+        /// <summary>
+        /// Converts a board pin number to the driver's logical numbering scheme.
+        /// </summary>
+        /// <param name="pinNumber">The board pin number to convert.</param>
+        /// <returns>The pin number in the driver's logical numbering scheme.</returns>
+        protected internal override int ConvertPinNumberToLogicalNumberingScheme(int pinNumber)
+        {
+            // CM3 has no logical numbering scheme
+            return pinNumber;
+        }
+    }
+}
---
+++ src/System.Device.Gpio/System/Device/Gpio/Processor.cs
@@ -0,0 +1,23 @@
+﻿namespace System.Device.Gpio.System.Device.Gpio
+{
+    /// <summary>
+    /// The board processor.
+    /// </summary>
+    public enum Processor
+    {
+        /// <summary>
+        /// Processor is unknown.
+        /// </summary>
+        Unknown,
+
+        /// <summary>
+        /// Processor is a BCM2708.
+        /// </summary>
+        Bcm2708,
+
+        /// <summary>
+        /// Processor is a BCM2709.
+        /// </summary>
+        Bcm2709,
+    }
+}
