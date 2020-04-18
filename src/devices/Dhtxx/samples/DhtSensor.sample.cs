80e582bfeb21c2d829a0ad78e661d10fd0e84bc9
Patrick Grawehr <pgrawehr@hotmail.com>
2020-04-18 08:27:35 +0200

    Fix indentation

--- src/devices/Dhtxx/samples/DhtSensor.sample.cs
+++ src/devices/Dhtxx/samples/DhtSensor.sample.cs
@@ -14,29 +14,31 @@ namespace Iot.Device.DHTxx.Samples
     {
         public static void Main(string[] args)
         {
-        Console.WriteLine("Hello DHT!");
+            Console.WriteLine("Hello DHT!");
 
-        // Init DHT10 through I2C
-        I2cConnectionSettings settings = new I2cConnectionSettings(1, Dht10.DefaultI2cAddress);
-        I2cDevice device = I2cDevice.Create(settings);
+            // Init DHT10 through I2C
+            I2cConnectionSettings settings = new I2cConnectionSettings(1, Dht10.DefaultI2cAddress);
+            I2cDevice device = I2cDevice.Create(settings);
 
-        using (Dht10 dht = new Dht10(device))
-        {
-            while (true)
+            using (Dht10 dht = new Dht10(device))
             {
-                var tempValue = dht.Temperature;
-                var humValue = dht.Humidity;
+                while (true)
+                {
+                    var tempValue = dht.Temperature;
+                    var humValue = dht.Humidity;
 
-                Console.WriteLine($"Temperature: {tempValue.Celsius:0.#}\u00B0C");
-                Console.WriteLine($"Relative humidity: {humValue:0.#}%");
+                    Console.WriteLine($"Temperature: {tempValue.Celsius:0.#}\u00B0C");
+                    Console.WriteLine($"Relative humidity: {humValue:0.#}%");
 
-                // WeatherHelper supports more calculations, such as the summer simmer index, saturated vapor pressure, actual vapor pressure and absolute humidity.
-                Console.WriteLine($"Heat index: {WeatherHelper.CalculateHeatIndex(tempValue, humValue).Celsius:0.#}\u00B0C");
-                Console.WriteLine($"Dew point: {WeatherHelper.CalculateDewPoint(tempValue, humValue).Celsius:0.#}\u00B0C");
+                    // WeatherHelper supports more calculations, such as the summer simmer index, saturated vapor pressure, actual vapor pressure and absolute humidity.
+                    Console.WriteLine(
+                        $"Heat index: {WeatherHelper.CalculateHeatIndex(tempValue, humValue).Celsius:0.#}\u00B0C");
+                    Console.WriteLine(
+                        $"Dew point: {WeatherHelper.CalculateDewPoint(tempValue, humValue).Celsius:0.#}\u00B0C");
 
-                Thread.Sleep(2000);
+                    Thread.Sleep(2000);
+                }
             }
         }
-    }
     }
 }
