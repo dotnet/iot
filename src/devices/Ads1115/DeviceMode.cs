b5a39be839d910733a03c4f1029c801d6a8407e7
Patrick Grawehr <pgrawehr@hotmail.com>
2020-04-28 00:18:26 +0200

    Ads1115 extensions (#915)
    Co-Authored-By: Krzysztof Wicher <mordotymoja@gmail.com>

--- src/devices/Ads1115/DeviceMode.cs
+++ src/devices/Ads1115/DeviceMode.cs
@@ -12,7 +12,7 @@ namespace Iot.Device.Ads1115
         /// <summary>Continuous mode</summary>
         Continuous = 0x00,
 
-        /// <summary>Power down mode</summary>
+        /// <summary>Power down mode, the chip is shutting down after the next conversion</summary>
         PowerDown = 0x01
     }
 }
