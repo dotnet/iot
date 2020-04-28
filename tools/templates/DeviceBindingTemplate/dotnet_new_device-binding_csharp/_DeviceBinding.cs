8507416e68694083f507fa88099f9698458cbfc3
Patrick Grawehr <pgrawehr@hotmail.com>
2020-04-28 20:38:03 +0200

    Template needs documentation, too
    Build fails otherwise

--- tools/templates/DeviceBindingTemplate/dotnet_new_device-binding_csharp/_DeviceBinding.cs
+++ tools/templates/DeviceBindingTemplate/dotnet_new_device-binding_csharp/_DeviceBinding.cs
@@ -9,12 +9,19 @@ using System.Device.Spi;
 
 namespace Iot.Device._DeviceBinding
 {
+    /// <summary>
+    /// Add documentation here
+    /// </summary>
     public class _DeviceBinding : IDisposable
     {
+        /// <summary>
+        /// Creates a new instance of this device.
+        /// </summary>
         public _DeviceBinding()
         {
         }
 
+        /// <inheritdoc />
         public void Dispose()
         {
         }
