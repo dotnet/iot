cd0a431c210f8369bc72fed7819d5fbd0f7bfcb7
姜涵 <tlos142857@live.cn>
2020-05-22 00:43:28 +0800

    fixed codepage error

--- src/devices/KeyMatrix/samples/KeyMatrix.Sample.cs
+++ src/devices/KeyMatrix/samples/KeyMatrix.Sample.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -90,6 +90,7 @@ namespace Iot.Device.KeyMatrix.Samples
                 {
                     Console.Write(rv[c] == PinValue.Low ? " ." : " #");
                 }
+
                 Console.WriteLine();
             }
         }
