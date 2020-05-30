d0a8d17ed62b839183e10142f58d2a7547a959db
Krzysztof Wicher <kwicher@microsoft.com>
2020-05-30 15:13:21 -0700

    Make System.Device.Gpio work on Mono

--- src/System.Device.Gpio/System/Device/Pwm/PwmChannel.cs
+++ src/System.Device.Gpio/System/Device/Pwm/PwmChannel.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Runtime.CompilerServices;
+
 namespace System.Device.Pwm
 {
     /// <summary>
@@ -61,7 +63,7 @@ namespace System.Device.Pwm
         {
             if (Environment.OSVersion.Platform == PlatformID.Win32NT)
             {
-                return new Channels.Windows10PwmChannel(
+                return CreateWindows10PwmChannel(
                     chip,
                     channel,
                     frequency,
@@ -75,6 +77,20 @@ namespace System.Device.Pwm
                     frequency,
                     dutyCyclePercentage);
             }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static PwmChannel CreateWindows10PwmChannel(int chip, int channel, int frequency, double dutyCyclePercentage)
+        {
+            // This wrapper is needed to prevent Mono from loading Windows10PwmChannel
+            // which causes all fields to be loaded - one of such fields is WinRT type which does not
+            // exist on Linux which causes TypeLoadException.
+            // Using NoInlining and no explicit type prevents this from happening.
+            return new Channels.Windows10PwmChannel(
+                    chip,
+                    channel,
+                    frequency,
+                    dutyCyclePercentage);
         }
     }
 }
