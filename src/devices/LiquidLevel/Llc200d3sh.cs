cf3cf53ac7b7bee508b6e7f0f2068c8d9f88934f
Darren Fahy <dfahy@tvcl.co.uk>
2020-06-03 10:13:29 +0100

    Code Review Changes
    Changes from the code review

--- src/devices/LiquidLevel/Llc200d3sh.cs
+++ src/devices/LiquidLevel/Llc200d3sh.cs
@@ -4,21 +4,21 @@
 
 using System.Device.Gpio;
 
-namespace Iot.Device.LiquidLevelSwitch
+namespace Iot.Device.LiquidLevel
 {
     /// <summary>
     /// Optomax LLC200D3SH digital liquid level switch
     /// </summary>
-    public class Llc200d3sh : LiquidLevelSwitchBase
+    public class Llc200d3sh : LiquidLevelSwitch
     {
-        /// <summary>Initializes a new instance of the <see cref="Llc200d3sh" /> class.</summary>
+        /// <summary>Creates a new instance of the Llc200d3sh.</summary>
         /// <param name="pin">The data pin</param>
         /// <param name="pinNumberingScheme">Use the logical or physical pin layout</param>
         /// <param name="gpioController">A Gpio Controller if you want to use a specific one</param>
         /// <param name="shouldDispose">True to dispose the Gpio Controller</param>
         public Llc200d3sh(int pin, GpioController gpioController = null,
                           PinNumberingScheme pinNumberingScheme = PinNumberingScheme.Logical, bool shouldDispose = true)
-            : base(pin, PinMode.Low, gpioController, pinNumberingScheme, shouldDispose)
+            : base(pin, PinValue.Low, gpioController, pinNumberingScheme, shouldDispose)
         {
         }
     }
--- src/devices/LiquidLevel/samples/LiquidLevelSwitch.sample.cs
+++ src/devices/LiquidLevel/samples/LiquidLevelSwitch.sample.cs
@@ -3,15 +3,17 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Device.Gpio;
 using System.Threading;
+using Iot.Device.LiquidLevel;
 
-namespace Iot.Device.LiquidLevelSwitch.Samples
+namespace LiquidLevel.Sample
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            using (Llc200d3sh sensor = new Llc200d3sh(23))
+            using (LiquidLevelSwitch sensor = new LiquidLevelSwitch(23, PinValue.Low))
             {
                 while (true)
                 {
