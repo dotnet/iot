589d6345fdf4f70bce557d9f85197f7b1b59d6e1
姜涵 <tlos142857@live.cn>
2020-06-04 18:55:27 +0800

    Added support for APA102

---
+++ src/devices/Apa102/Apa102.cs
@@ -0,0 +1,80 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Device.Spi;
+using System.Drawing;
+
+namespace Iot.Device.Apa102
+{
+    /// <summary>
+    /// Driver for APA102. A double line transmission integrated control LED
+    /// </summary>
+    public class Apa102 : IDisposable, IEnumerable<Color>
+    {
+        /// <summary>
+        /// Colors of LEDs
+        /// </summary>
+        public Span<Color> Pixels => _pixels;
+
+        private readonly SpiDevice _spi;
+        private Color[] _pixels;
+        private byte[] _buffer;
+
+        /// <summary>
+        /// Initializes a new instance of the APA102 device.
+        /// </summary>
+        /// <param name="spi">The SPI device used for communication.</param>
+        /// <param name="length">Number of LEDs</param>
+        public Apa102(SpiDevice spi, int length)
+        {
+            _spi = spi ?? throw new ArgumentNullException(nameof(spi));
+
+            _pixels = new Color[length];
+            _buffer = new byte[(length + 2) * 4];
+
+            _buffer.AsSpan(0, 4).Fill(0x00); // start frame
+            _buffer.AsSpan((length + 1) * 4, 4).Fill(0xFF); // end frame
+        }
+
+        /// <summary>
+        /// Update color data to LEDs
+        /// </summary>
+        public void Flush()
+        {
+            for (var i = 0; i < _pixels.Length; i++)
+            {
+                var pixel = _buffer.AsSpan((i + 1) * 4);
+                pixel[0] = (byte)((_pixels[i].A >> 3) | 0b11100000); // global brighrness (alpha)
+                pixel[1] = _pixels[i].B; // blue
+                pixel[2] = _pixels[i].G; // green
+                pixel[3] = _pixels[i].R; // red
+            }
+
+            _spi.Write(_buffer);
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            _spi.Dispose();
+
+            _pixels = null;
+            _buffer = null;
+        }
+
+        /// <inheritdoc/>
+        public IEnumerator<Color> GetEnumerator()
+        {
+            return ((IEnumerable<Color>)_pixels).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _pixels.GetEnumerator();
+        }
+    }
+}
---
+++ src/devices/Apa102/samples/Apa102.Samples/Apa102.Samples.cs
@@ -0,0 +1,48 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Device.Spi;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+
+namespace Iot.Device.Apa102.Samples
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var spiDevice = SpiDevice.Create(new SpiConnectionSettings(0, 0)
+            {
+                ClockFrequency = 30_000_000, // up to 30 MHz
+                DataFlow = DataFlow.MsbFirst,
+                Mode = SpiMode.Mode0 // ensure data is ready at clock rising edge
+            });
+
+            var apa102 = new Apa102(spiDevice, 16);
+
+            var random = new Random();
+            for (var i = 0; i < apa102.Count(); i++)
+            {
+                apa102.Pixels[i] = Color.FromArgb(255, random.Next(256), random.Next(256), random.Next(256));
+            }
+
+            while (true)
+            {
+                Flow(apa102.Pixels);
+                apa102.Flush();
+                Thread.Sleep(10);
+            }
+        }
+
+        private static void Flow(Span<Color> pixels)
+        {
+            for (var i = 0; i < pixels.Length; i++)
+            {
+                pixels[i] = Color.FromArgb(pixels[i].A, pixels[i].R, pixels[i].G, pixels[i].B);
+            }
+        }
+    }
+}
