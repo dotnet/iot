bbeda340ed447de5a4bb18847f9840068bb05cf5
姜涵 <tlos142857@live.cn>
2020-06-05 14:31:55 +0800

    update samples

--- src/devices/Apa102/samples/Apa102.Samples.cs
+++ src/devices/Apa102/samples/Apa102.Samples.cs
@@ -14,34 +14,25 @@ namespace Iot.Device.Apa102.Samples
     {
         public static void Main(string[] args)
         {
+            var random = new Random();
+
             var spiDevice = SpiDevice.Create(new SpiConnectionSettings(0, 0)
             {
                 ClockFrequency = 30_000_000, // up to 30 MHz
                 DataFlow = DataFlow.MsbFirst,
                 Mode = SpiMode.Mode0 // ensure data is ready at clock rising edge
             });
-
             var apa102 = new Apa102(spiDevice, 16);
-
-            var random = new Random();
-            for (var i = 0; i < apa102.Count(); i++)
-            {
-                apa102.Pixels[i] = Color.FromArgb(255, random.Next(256), random.Next(256), random.Next(256));
-            }
 
             while (true)
             {
-                Flow(apa102.Pixels);
-                apa102.Flush();
-                Thread.Sleep(10);
-            }
-        }
+                for (var i = 0; i < apa102.Count(); i++)
+                {
+                    apa102.Pixels[i] = Color.FromArgb(255, random.Next(256), random.Next(256), random.Next(256));
+                }
 
-        private static void Flow(Span<Color> pixels)
-        {
-            for (var i = 0; i < pixels.Length; i++)
-            {
-                pixels[i] = Color.FromArgb(pixels[i].A, pixels[i].R, pixels[i].G, pixels[i].B);
+                apa102.Flush();
+                Thread.Sleep(1000);
             }
         }
     }
