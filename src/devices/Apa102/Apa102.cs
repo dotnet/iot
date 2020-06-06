57d80e21169f619f515ac18d9800afc312122f0c
姜涵 <tlos142857@live.cn>
2020-06-06 16:21:47 +0800

    removed using

--- src/devices/Apa102/Apa102.cs
+++ src/devices/Apa102/Apa102.cs
@@ -3,8 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Device.Spi;
 using System.Drawing;
 
