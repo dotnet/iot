97f6e7df8ec9df2e7b54d948bddf2c084d5ff327
László Gárdonyi <gLes@users.noreply.github.com>
2020-06-10 19:35:53 +0200

    Fix Dot enum for Large4Digit7SegmentDisplay (#1066)
    The values for LeftUpper and LeftLower dots were interchanged

--- src/devices/Display/Dot.cs
+++ src/devices/Display/Dot.cs
@@ -26,17 +26,17 @@ namespace Iot.Device.Display
         /// <summary>
         /// Left colon
         /// </summary>
-        LeftColon = LeftLower | LeftUpper,
-
-        /// <summary>
-        /// Left lower dot
-        /// </summary>
-        LeftLower = 0b0000_0100,
+        LeftColon = LeftUpper | LeftLower,
 
         /// <summary>
         /// Left upper dot
         /// </summary>
-        LeftUpper = 0b0000_1000,
+        LeftUpper = 0b0000_0100,
+
+        /// <summary>
+        /// Left lower dot
+        /// </summary>
+        LeftLower = 0b0000_1000,
 
         /// <summary>
         /// Decimal point (between third and fourth digits)
