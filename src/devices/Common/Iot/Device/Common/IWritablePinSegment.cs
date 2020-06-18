a32942ee2bb396bc8c11f8ecd8f3562d49d01f41
Rich Lander <rlander@microsoft.com>
2020-06-18 12:30:38 -0700

    Add IWritablePinSegment interface

---
+++ src/devices/Common/Iot/Device/Common/IWritablePinSegment.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace Iot.Device.Common
+{
+    /// <summary>
+    /// Interface that abstracts pin multiplexing.
+    /// </summary>
+    public interface IWritablePinSegment
+    {
+        /// <summary>
+        /// Length of segment (number of pins)
+        /// </summary>
+        int Length { get; }
+
+        /// <summary>
+        /// Writes a high or low value to pin
+        /// </summary>
+        void Write(int pin, int value);
+    }
+}
