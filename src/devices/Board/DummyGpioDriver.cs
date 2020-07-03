04035333dbb200e4e82f4cc52179f83ff347ce1f
Patrick Grawehr <pgrawehr@hotmail.com>
2020-07-03 21:49:11 +0200

    Extra GPIO drivers, used for testing
    DummyGpioDriver is a driver that has no pins, but can be
    instantiated regardless of any hardware support.
    
    KeyboardGpioDriver is a driver that uses the keyboard as GPIO "pins".

---
+++ src/devices/Board/DummyGpioDriver.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Gpio;
+using System.Text;
+using System.Threading;
+
+namespace Iot.Device.Board
+{
+    /// <summary>
+    /// A GPIO driver that has zero pins. Use to fulfill the interface.
+    /// </summary>
+    public class DummyGpioDriver : GpioDriver
+    {
+        protected override int PinCount => 0;
+        protected override int ConvertPinNumberToLogicalNumberingScheme(int pinNumber)
+        {
+            return 0;
+        }
+
+        protected override void OpenPin(int pinNumber)
+        {
+            throw new NotSupportedException("No such pin");
+        }
+
+        protected override void ClosePin(int pinNumber)
+        {
+            throw new NotSupportedException("No such pin");
+        }
+
+        protected override void SetPinMode(int pinNumber, PinMode mode)
+        {
+            throw new NotSupportedException("No such pin");
+        }
+
+        protected override PinMode GetPinMode(int pinNumber)
+        {
+            throw new NotSupportedException("No such pin");
+        }
+
+        protected override bool IsPinModeSupported(int pinNumber, PinMode mode)
+        {
+            throw new NotSupportedException("No such pin");
+        }
+
+        protected override PinValue Read(int pinNumber)
+        {
+            throw new NotSupportedException("No such pin");
+        }
+
+        protected override void Write(int pinNumber, PinValue value)
+        {
+            throw new NotSupportedException("No such pin");
+        }
+
+        protected override WaitForEventResult WaitForEvent(int pinNumber, PinEventTypes eventTypes, CancellationToken cancellationToken)
+        {
+            throw new NotSupportedException("No such pin");
+        }
+
+        protected override void AddCallbackForPinValueChangedEvent(int pinNumber, PinEventTypes eventTypes, PinChangeEventHandler callback)
+        {
+            throw new NotSupportedException("No such pin");
+        }
+
+        protected override void RemoveCallbackForPinValueChangedEvent(int pinNumber, PinChangeEventHandler callback)
+        {
+            throw new NotSupportedException("No such pin");
+        }
+    }
+}
