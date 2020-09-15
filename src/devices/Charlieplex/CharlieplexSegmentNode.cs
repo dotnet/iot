0bd13b48730526058094070cedbff5829d1276bb
Rich Lander <rlander@microsoft.com>
2020-09-14 21:14:43 -0700

    Add Charlieplex binding (#1126)
    * Add Charlieplex binding
    
    * Update images in README
    
    * Update README.md
    
    * Display entire segment
    
    * Update pin mode scheme
    
    * Update core algorithm
    
    * Adopt TimeSpan for DisplaySegment
    
    * Add initial message for wiring circuit
    
    * Improve README
    
    * Rename README
    
    * Control height of images
    
    * Rename README file
    
    * Fix link to test sample
    
    * Update per feedback
    
    * Shorten default delay
    
    * Update per feedback
    
    * Update per feedback
    
    * Switch namespace to .Multiplexing

---
+++ src/devices/Charlieplex/CharlieplexSegmentNode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Iot.Device.Multiplexing
+{
+    /// <summary>
+    /// Represents a node in a Charlieplexed circuit.
+    /// https://wikipedia.org/wiki/Charlieplexing
+    /// </summary>
+    public struct CharlieplexSegmentNode
+    {
+        /// <summary>
+        /// Anode leg (power) for a device/load
+        /// </summary>
+        public int Anode;
+
+        /// <summary>
+        /// Cathode leg (ground) for a device/load
+        /// </summary>
+        public int Cathode;
+
+        /// <summary>
+        /// Value of node
+        /// </summary>
+        public PinValue Value;
+    }
+}
