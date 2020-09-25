c395b8f306be0d7e3ad67716bded78c1ef6879db
Rasmus b. Nielsen <67422846+R10Rasmus@users.noreply.github.com>
2020-09-25 10:39:44 +0200

    Add device binding for uFire_ISE (#1134)
    * Add device binding for uFire_ISE
    
    * change delay to Milliseconds and make sure Math.Round is not called with more than 15 digits
    
    * fix bug in write register (wrong index of array)
    
    * fix all the comments made from Ellerbach on the 23-08-2020
    
    * fix all the comments made from Ellerbach on the 24-08-2020. Except for documentation for IseCommunicationDelay (I have send a request to the manufacturer to document this delay)
    
    * Fix feedback from Ellerbach on 25-08-2020. Still need to fix folder name
    
    * rename device folder to fit naming convention
    
    * Added feedback from Ellerbach on 26-08-2020
    
    * Fix wrong ref. in project file
    
    * removed not needed initialize of span
    
    * change folder name and file names
    
    * VS will not let me just change the first letter of the folder name to uppercase, sow change to to another name and in next check in I will change it back
    
    * change to folder name to the correct name
    
    * The manufacturer has updated the documentation regarding communication delays. This is now referenced
    
    * fix feedback from krwq on the 23/09/2020
    
    * fix build error
    
    * added feedback from krwq on 24/09/2020
    
    * GetVersion return major and GetFirmwareVersion return minor, as the manufacturer  see https://github.com/u-fire/ISE_Probe/blob/master/python/RaspberryPi/shell.py#L91
    
    * fix build isse
    
    * fix more resolved issues
    
    * GetFirmwareVersion and GetVersion now return byte
    
    * it now use TryWriteBytes in WriteRegister
    
    Co-authored-by: Rasmus Bang Nielsen <rbn@addanmark.dk>

---
+++ src/devices/UFireIse/UFireOrp.cs
@@ -0,0 +1,67 @@
+﻿// Licensed to the.NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Device.I2c;
+using UnitsNet;
+
+namespace Iot.Device.UFire
+{
+    /// <summary>
+    /// Get ORP (oxidation-reduction potential) measuremens from μFire ISE (ion-selective electrode) Probe Interface
+    /// </summary>
+    public class UFireOrp : UFireIse
+    {
+        /// <summary>
+        /// Oxidation-reduction potential (ORP) measuremens
+        /// It makes it possible to the Oxidation-Reduction Potential (ORP) measurement without doing a measuremen (the retunres the old measurement)
+        /// </summary>
+        public ElectricPotential LastOxidationReducationPotential { get; private set; } = new ElectricPotential();
+
+        /// <summary>
+        /// Reduction potential (Eh) measuremens
+        /// It makes it possible to the Reduction potential (Eh) measurement without doing a measuremen (the retunres the old measurement)
+        /// </summary>
+        public ElectricPotential LastReductionPotential { get; private set; } = new ElectricPotential();
+
+        /// <summary>
+        /// The probe potential
+        /// </summary>
+        public ElectricPotential ProbePotential => ElectricPotential.FromMillivolts(ReadEeprom(Register.POTENTIAL_REGISTER));
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UFireOrp"/> class.
+        /// </summary>
+        /// <param name="i2cDevice">The I2C device to be used</param>
+        public UFireOrp(I2cDevice i2cDevice)
+            : base(i2cDevice)
+        {
+        }
+
+        /// <summary>
+        /// Tries to measure ORP (Oxidation-Reduction Potential).
+        /// </summary>
+        /// <param name="orp">ORP (Oxidation-Reduction Potential) measurement</param>
+        /// <returns>True if it could measure ORP (Oxidation-Reduction Potential) else false</returns>
+        public bool TryMeasureOxidationReductionPotential(out ElectricPotential orp)
+        {
+            ElectricPotential mV = ReadElectricPotential();
+            LastOxidationReducationPotential = mV;
+            LastReductionPotential = new ElectricPotential(mV.Millivolts + GetProbePotential(), UnitsNet.Units.ElectricPotentialUnit.Millivolt);
+
+            if (double.IsNaN(mV.Value) || double.IsInfinity(mV.Value))
+            {
+                LastOxidationReducationPotential = new ElectricPotential();
+                LastReductionPotential = new ElectricPotential();
+            }
+
+            orp = mV;
+            return double.IsNaN(mV.Value) || double.IsInfinity(mV.Value);
+        }
+
+        private float GetProbePotential()
+        {
+            return ReadEeprom(Register.POTENTIAL_REGISTER);
+        }
+    }
+}
