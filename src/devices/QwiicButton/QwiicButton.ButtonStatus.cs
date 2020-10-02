77a7e938fa897d6cfab36487d5d8238706c565b0
Esbob <evi@schultz.dk>
2020-10-02 06:39:09 +0200

    Split QwiicButton class into partial files

---
+++ src/devices/QwiicButton/QwiicButton.ButtonStatus.cs
@@ -0,0 +1,67 @@
+﻿//// Licensed to the .NET Foundation under one or more agreements.
+//// The .NET Foundation licenses this file to you under the MIT license.
+//// See the LICENSE file in the project root for more information.
+
+namespace Iot.Device.QwiicButton
+{
+    public partial class QwiicButton
+    {
+        /// <summary>
+        /// Returns whether the button is pressed.
+        /// </summary>
+        public bool IsPressed()
+        {
+            var status = new StatusRegisterBitField(_i2cBus.ReadSingleRegister(Register.BUTTON_STATUS));
+            return status.IsPressed;
+        }
+
+        /// <summary>
+        /// Returns whether the button is clicked.
+        /// </summary>
+        public bool HasBeenClicked()
+        {
+            var status = new StatusRegisterBitField(_i2cBus.ReadSingleRegister(Register.BUTTON_STATUS));
+            return status.HasBeenClicked;
+        }
+
+        /// <summary>
+        /// Returns whether a new button status event has occurred.
+        /// </summary>
+        public bool EventAvailable()
+        {
+            var status = new StatusRegisterBitField(_i2cBus.ReadSingleRegister(Register.BUTTON_STATUS));
+            return status.EventAvailable;
+        }
+
+        /// <summary>
+        /// Sets <see cref="IsPressed"/>, <see cref="HasBeenClicked"/> and <see cref="EventAvailable"/> to false.
+        /// </summary>
+        public byte ClearEventBits()
+        {
+            var status = new StatusRegisterBitField(_i2cBus.ReadSingleRegister(Register.BUTTON_STATUS))
+            {
+                EventAvailable = false,
+                HasBeenClicked = false,
+                IsPressed = false
+            };
+            return _i2cBus.WriteSingleRegisterWithReadback(Register.BUTTON_STATUS, status.StatusRegisterValue);
+        }
+
+        /// <summary>
+        /// Returns the time in milliseconds that the button waits for the mechanical contacts to settle.
+        /// </summary>
+        public ushort GetDebounceTime()
+        {
+            return _i2cBus.ReadDoubleRegister(Register.BUTTON_DEBOUNCE_TIME);
+        }
+
+        /// <summary>
+        /// Sets the time in milliseconds that the button waits for the mechanical contacts to settle and checks if the register was set properly.
+        /// Returns 0 on success, 1 on register I2C write fail, and 2 if the value didn't get written into the register properly.
+        /// </summary>
+        public byte SetDebounceTime(ushort time)
+        {
+            return (byte)_i2cBus.WriteDoubleRegisterWithReadback(Register.BUTTON_DEBOUNCE_TIME, time);
+        }
+    }
+}
---
+++ src/devices/QwiicButton/QwiicButton.LedConfiguration.cs
@@ -0,0 +1,42 @@
+﻿//// Licensed to the .NET Foundation under one or more agreements.
+//// The .NET Foundation licenses this file to you under the MIT license.
+//// See the LICENSE file in the project root for more information.
+
+namespace Iot.Device.QwiicButton
+{
+    public partial class QwiicButton
+    {
+        /// <summary>
+        /// Turns the onboard LED on with the specified brightness.
+        /// <param name="brightness">LED brightness value between 0 (off) and 255 (max). Defaults to max.</param>
+        /// </summary>
+        public bool LedOn(byte brightness = 255)
+        {
+            return LedConfig(brightness, 0, 0);
+        }
+
+        /// <summary>
+        /// Turns the onboard LED off.
+        /// </summary>
+        public bool LedOff()
+        {
+            return LedConfig(0, 0, 0);
+        }
+
+        /// <summary>
+        /// Configures the onboard LED with the given max brightness, granularity, cycle time, and off time.
+        /// <param name="brightness">LED brightness value between 0 (off) and 255 (max). Default is 255.</param>
+        /// <param name="cycleTime">Total pulse cycle time (in ms). Does not include off time.</param>
+        /// <param name="offTime">Off time between pulses (in ms). Default is 500 ms.</param>
+        /// <param name="granularity">Amount of steps it takes to get to the set brightness level (1 is fine for most applications).</param>
+        /// </summary>
+        public bool LedConfig(byte brightness, ushort cycleTime, ushort offTime, byte granularity = 1)
+        {
+            bool success = _i2cBus.WriteSingleRegister(Register.LED_BRIGHTNESS, brightness);
+            success &= _i2cBus.WriteSingleRegister(Register.LED_PULSE_GRANULARITY, granularity);
+            success &= _i2cBus.WriteDoubleRegister(Register.LED_PULSE_CYCLE_TIME, cycleTime);
+            success &= _i2cBus.WriteDoubleRegister(Register.LED_PULSE_OFF_TIME, offTime);
+            return success;
+        }
+    }
+}
