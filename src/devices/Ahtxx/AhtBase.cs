f16e86b4ae637ef5817f7689011366cde2024011
Marcus Fehde <marcus.fehde@gmx.net>
2020-10-08 10:39:34 +0200

    Update src/devices/Ahtxx/AhtBase.cs
    Co-authored-by: Krzysztof Wicher <mordotymoja@gmail.com>

--- src/devices/Ahtxx/AhtBase.cs
+++ src/devices/Ahtxx/AhtBase.cs
@@ -74,7 +74,7 @@ namespace Iot.Device.Ahtxx
         /// Gets the current relative humidity reading from the sensor.
         /// Reading the humidity takes between 10 ms and 80 ms.
         /// </summary>
-        /// <returns>Temperature reading</returns>
+        /// <returns>Relative humidity reading</returns>
         public Ratio GetHumidity()
         {
             Measure();
