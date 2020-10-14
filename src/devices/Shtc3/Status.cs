61a22db3a3d6f53f325cba52182ebf61f6f7fb39
famblard <48769175+famblard86@users.noreply.github.com>
2020-10-14 11:38:33 +0200

    Added SHTC3 binding (#1067)
    * Added SHTC3 binding
    
    * Changes from the code review
    
    * change .csproj
    
    * Changes from the code review
    
    * add use Ratio
    
    * fix readme and use ReadUInt16BigEndian
    
    * remove bad use of ReadUInt16BigEndian the temperature can be negative not humidity
    
    * Update src/devices/Shtc3/Shtc3.cs
    
    Co-authored-by: Krzysztof Wicher <mordotymoja@gmail.com>
    
    * Update src/devices/Shtc3/Shtc3.cs
    
    Co-authored-by: Krzysztof Wicher <mordotymoja@gmail.com>
    
    * Update src/devices/Shtc3/Shtc3.cs
    
    Co-authored-by: Krzysztof Wicher <mordotymoja@gmail.com>
    
    * Update src/devices/Shtc3/Shtc3.cs
    
    Co-authored-by: Krzysztof Wicher <mordotymoja@gmail.com>
    
    * Update src/devices/Shtc3/Shtc3.cs
    
    Co-authored-by: Krzysztof Wicher <mordotymoja@gmail.com>
    
    * update after review
    
    * change status to internal
    
    Co-authored-by: Krzysztof Wicher <mordotymoja@gmail.com>

---
+++ src/devices/Shtc3/Status.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Iot.Device.Shtc3
+{
+    /// <summary>
+    /// Shtc3 Status
+    /// </summary>
+    internal enum Status
+    {
+        /// <summary>
+        /// Sensor ready to use
+        /// </summary>
+        Idle,
+
+        /// <summary>
+        /// Sensor in sleep mode
+        /// </summary>
+        Sleep
+    }
+}
