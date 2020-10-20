f5b974b09ae623805050edd6721cf6faa45a80af
Jose Perez Rodriguez <joperezr@microsoft.com>
2020-10-20 10:45:42 -0700

    Adding crosstargeting in order to be able to call WinRT APIs in net5.0 (#1217)
    * Adding crosstargeting in order to be able to call WinRT APIs in net5.0
    
    * Only build Windows-specific build when running on Windows SDK
    
    * Addressing PR Feedback
    
    * Removing accidental push changing TFMs
    
    * Removing blank spaces on Error message

---
+++ src/System.Device.Gpio/System/Device/I2c/I2cDevice.Windows.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.CompilerServices;
+
+namespace System.Device.I2c
+{
+    /// <summary>
+    /// The communications channel to a device on an I2C bus.
+    /// </summary>
+    public abstract partial class I2cDevice
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static I2cDevice CreateWindows10I2cDevice(I2cConnectionSettings settings)
+        {
+            // This wrapper is needed to prevent Mono from loading Windows10I2cDevice
+            // which causes all fields to be loaded - one of such fields is WinRT type which does not
+            // exist on Linux which causes TypeLoadException.
+            // Using NoInlining and no explicit type prevents this from happening.
+            return new Windows10I2cDevice(settings);
+        }
+    }
+}
