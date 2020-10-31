e9f79425ea89e71270c7473046d8d3a8d27d6d17
Rich Lander <rlander@microsoft.com>
2020-10-31 08:56:21 -0700

    Update projects

---
+++ src/devices/Apa102/samples/Program.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.Spi;
+using System.Drawing;
+using System.Threading;
+using Iot.Device.Apa102;
+
+var random = new Random();
+
+var spiDevice = SpiDevice.Create(new SpiConnectionSettings(0, 0)
+{
+    ClockFrequency = 20_000_000,
+    DataFlow = DataFlow.MsbFirst,
+    Mode = SpiMode.Mode0 // ensure data is ready at clock rising edge
+});
+var apa102 = new Apa102(spiDevice, 16);
+
+while (true)
+{
+    for (var i = 0; i < apa102.Pixels.Length; i++)
+    {
+        apa102.Pixels[i] = Color.FromArgb(255, random.Next(256), random.Next(256), random.Next(256));
+    }
+
+    apa102.Flush();
+    Thread.Sleep(1000);
+}
