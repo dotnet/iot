94c645dae7c744079fc183888d93132c12c100a1
Rich Lander <rlander@microsoft.com>
2020-11-02 13:51:11 -0800

    Update SofPwm sample

---
+++ src/devices/SoftPwm/samples/Program.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Device.Pwm.Drivers;
+using System.Threading;
+
+Console.WriteLine("Hello PWM!");
+
+using var pwmChannel = new SoftwarePwmChannel(17, 200, 0);
+pwmChannel.Start();
+for (double fill = 0.0; fill <= 1.0; fill += 0.01)
+{
+    pwmChannel.DutyCycle = fill;
+    Thread.Sleep(500);
+}
