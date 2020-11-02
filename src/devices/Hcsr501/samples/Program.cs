11da804b417e8cd33a28185ba6c93e7f719343cf
Rich Lander <rlander@microsoft.com>
2020-11-01 21:39:13 -0800

    Update Hcsr501 project

--- src/devices/Hcsr501/samples/Program.cs
+++ src/devices/Hcsr501/samples/Program.cs
@@ -7,40 +7,29 @@ using System.Device.Gpio;
 
 using Iot.Device.Hcsr501;
 
-namespace Hcsr501.Samples
+const int Hcsr501Pin = 17;
+const int LedPin = 27;
+
+GpioController ledController = new GpioController();
+ledController.OpenPin(LedPin, PinMode.Output);
+
+using Iot.Device.Hcsr501.Hcsr501 sensor =
+    new Iot.Device.Hcsr501.Hcsr501(Hcsr501Pin);
+while (true)
 {
-    internal class Program
+    // adjusting the detection distance and time by rotating the potentiometer on the sensor
+    if (sensor.IsMotionDetected)
     {
-        private const int Hcsr501Pin = 17;
-        private const int LedPin = 27;
-
-        public static void Main(string[] args)
-        {
-            GpioController ledController = new GpioController();
-            ledController.OpenPin(LedPin, PinMode.Output);
-
-            using (Iot.Device.Hcsr501.Hcsr501 sensor =
-                new Iot.Device.Hcsr501.Hcsr501(Hcsr501Pin))
-            {
-                while (true)
-                {
-                    // adjusting the detection distance and time by rotating the potentiometer on the sensor
-                    if (sensor.IsMotionDetected)
-                    {
-                        // turn the led on when the sensor detected infrared heat
-                        ledController.Write(LedPin, PinValue.High);
-                        Console.WriteLine("Detected! Turn the LED on.");
-                    }
-                    else
-                    {
-                        // turn the led off when the sensor undetected infrared heat
-                        ledController.Write(LedPin, PinValue.Low);
-                        Console.WriteLine("Undetected! Turn the LED off.");
-                    }
-
-                    Thread.Sleep(1000);
-                }
-            }
-        }
+        // turn the led on when the sensor detected infrared heat
+        ledController.Write(LedPin, PinValue.High);
+        Console.WriteLine("Detected! Turn the LED on.");
     }
+    else
+    {
+        // turn the led off when the sensor undetected infrared heat
+        ledController.Write(LedPin, PinValue.Low);
+        Console.WriteLine("Undetected! Turn the LED off.");
+    }
+
+    Thread.Sleep(1000);
 }
