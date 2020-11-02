4fa1ab799fb444bf231a6f8bbe5e4b3b4c3c4ced
Rich Lander <rlander@microsoft.com>
2020-11-01 22:01:11 -0800

    Update Lps25h project

---
+++ src/devices/Lps25h/samples/Program.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Threading;
+using System.Device.I2c;
+using Iot.Device.Common;
+using Iot.Device.Lps25h;
+using UnitsNet;
+
+// I2C address on SenseHat board
+const int I2cAddress = 0x5c;
+
+// set this to the current sea level pressure in the area for correct altitude readings
+var defaultSeaLevelPressure = WeatherHelper.MeanSeaLevel;
+
+using var th = new Lps25h(CreateI2cDevice());
+while (true)
+{
+    var tempValue = th.Temperature;
+    var preValue = th.Pressure;
+    var altValue = WeatherHelper.CalculateAltitude(preValue, defaultSeaLevelPressure, tempValue);
+
+    Console.WriteLine($"Temperature: {tempValue.DegreesCelsius:0.#}\u00B0C");
+    Console.WriteLine($"Pressure: {preValue.Hectopascals:0.##}hPa");
+    Console.WriteLine($"Altitude: {altValue:0.##}m");
+    Thread.Sleep(1000);
+}
+
+I2cDevice CreateI2cDevice()
+{
+    var settings = new I2cConnectionSettings(1, I2cAddress);
+    return I2cDevice.Create(settings);
+}
