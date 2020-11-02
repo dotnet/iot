91a84f64516d90d284621d2d6bcd2e6560b97881
Rich Lander <rlander@microsoft.com>
2020-11-02 11:30:09 -0800

    Update RadioReceiver project

--- src/devices/RadioReceiver/samples/Program.cs
+++ src/devices/RadioReceiver/samples/Program.cs
@@ -6,19 +6,8 @@ using System.Device.I2c;
 using Iot.Device.RadioReceiver;
 using UnitsNet;
 
-namespace Tea5767Samples
-{
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
-            I2cConnectionSettings settings = new I2cConnectionSettings(1, Tea5767.DefaultI2cAddress);
-            I2cDevice device = I2cDevice.Create(settings);
+I2cConnectionSettings settings = new I2cConnectionSettings(1, Tea5767.DefaultI2cAddress);
+I2cDevice device = I2cDevice.Create(settings);
 
-            using (Tea5767 radio = new Tea5767(device, FrequencyRange.Other, Frequency.FromMegahertz(103.3)))
-            {
-                Console.ReadKey();
-            }
-        }
-    }
-}
+using Tea5767 radio = new Tea5767(device, FrequencyRange.Other, Frequency.FromMegahertz(103.3));
+Console.ReadKey();
