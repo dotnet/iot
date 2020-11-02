3543d300e2d44e4cdec0671f9c93c794c7b31e74
Rich Lander <rlander@microsoft.com>
2020-11-02 11:32:20 -0800

    Update RadioTransmitter project

--- src/devices/RadioTransmitter/samples/Program.cs
+++ src/devices/RadioTransmitter/samples/Program.cs
@@ -5,21 +5,9 @@ using System;
 using System.Device.I2c;
 using Iot.Device.RadioTransmitter;
 
-namespace RadioTransmitter
-{
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
-            I2cConnectionSettings settings = new I2cConnectionSettings(1, Kt0803.DefaultI2cAddress);
-            I2cDevice device = I2cDevice.Create(settings);
+I2cConnectionSettings settings = new I2cConnectionSettings(1, Kt0803.DefaultI2cAddress);
+I2cDevice device = I2cDevice.Create(settings);
 
-            using (Kt0803 radio = new Kt0803(device, 106.6, Region.China))
-            {
-                Console.WriteLine($"The radio is running on FM {radio.Frequency.ToString("0.0")}MHz");
-
-                Console.ReadKey();
-            }
-        }
-    }
-}
+using Kt0803 radio = new Kt0803(device, 106.6, Region.China);
+Console.WriteLine($"The radio is running on FM {radio.Frequency.ToString("0.0")}MHz");
+Console.ReadKey();
