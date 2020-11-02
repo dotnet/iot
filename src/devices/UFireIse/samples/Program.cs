9e95ade6dee78f89c8ae4ad8d15481406ebe682f
Rich Lander <rlander@microsoft.com>
2020-11-02 14:25:29 -0800

    Update UFireIse sample

---
+++ src/devices/UFireIse/samples/Program.cs
@@ -0,0 +1,90 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Device.I2c;
+using Iot.Device.UFire;
+using UnitsNet;
+
+const int BusId = 1;
+
+PrintHelp();
+
+I2cConnectionSettings settings = new I2cConnectionSettings(BusId, UFireIse.I2cAddress);
+using I2cDevice device = I2cDevice.Create(settings);
+Console.WriteLine(
+        $"UFire_ISE is ready on I2C bus {device.ConnectionSettings.BusId} with address {device.ConnectionSettings.DeviceAddress}");
+
+Console.WriteLine();
+
+while (true)
+{
+    var command = Console.ReadLine().ToLower().Split(' ');
+    if (string.IsNullOrEmpty(command[0]))
+    {
+        return;
+    }
+
+    switch (command[0][0])
+    {
+        case 'b':
+            Basic(device);
+            return;
+        case '0':
+            Orp(device);
+            return;
+        case 'p':
+            Ph(device);
+            return;
+    }
+}
+
+void PrintHelp()
+{
+    Console.WriteLine("Command:");
+    Console.WriteLine("    B           Basic");
+    Console.WriteLine("    O           Read Orp (Oxidation-reduction potential) value");
+    Console.WriteLine("    P           Read pH (Power of Hydrogen) value");
+    Console.WriteLine();
+}
+
+void Basic(I2cDevice device)
+{
+    using (UFireIse uFireIse = new UFireIse(device))
+    {
+        Console.WriteLine("mV:" + uFireIse.ReadElectricPotential().Millivolts);
+    }
+}
+
+void Orp(I2cDevice device)
+{
+    using (UFireOrp uFireOrp = new UFireOrp(device))
+    {
+        if (uFireOrp.TryMeasureOxidationReductionPotential(out ElectricPotential orp))
+        {
+            Console.WriteLine("Eh:" + orp.Millivolts);
+        }
+        else
+        {
+            Console.WriteLine("Not possible to measure pH");
+        }
+    }
+}
+
+void Ph(I2cDevice device)
+{
+    using (UFirePh uFire_pH = new UFirePh(device))
+    {
+        Console.WriteLine("mV:" + uFire_pH.ReadElectricPotential().Millivolts);
+
+        if (uFire_pH.TryMeasurepH(out float pH))
+        {
+            Console.WriteLine("pH:" + pH);
+        }
+        else
+        {
+            Console.WriteLine("Not possible to measure pH");
+        }
+    }
+}
