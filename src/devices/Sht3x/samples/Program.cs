f84cbcbcd58925a5ad5e9c5696c7da8006ff7bb9
Rich Lander <rlander@microsoft.com>
2020-11-02 13:35:22 -0800

    Update Sht3x project

--- src/devices/Sht3x/samples/Program.cs
+++ src/devices/Sht3x/samples/Program.cs
@@ -5,35 +5,25 @@ using System;
 using System.Device.I2c;
 using System.Threading;
 using Iot.Device.Common;
+using Iot.Device.Sht3x;
 using UnitsNet;
 
-namespace Iot.Device.Sht3x.Samples
+I2cConnectionSettings settings = new I2cConnectionSettings(1, (byte)I2cAddress.AddrLow);
+I2cDevice device = I2cDevice.Create(settings);
+
+using Sht3x sensor = new Sht3x(device);
+while (true)
 {
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
-            I2cConnectionSettings settings = new I2cConnectionSettings(1, (byte)I2cAddress.AddrLow);
-            I2cDevice device = I2cDevice.Create(settings);
+    var tempValue = sensor.Temperature;
+    var humValue = sensor.Humidity;
 
-            using (Sht3x sensor = new Sht3x(device))
-            {
-                while (true)
-                {
-                    var tempValue = sensor.Temperature;
-                    var humValue = sensor.Humidity;
+    Console.WriteLine($"Temperature: {tempValue.DegreesCelsius:0.#}\u00B0C");
+    Console.WriteLine($"Relative humidity: {humValue:0.#}%");
 
-                    Console.WriteLine($"Temperature: {tempValue.DegreesCelsius:0.#}\u00B0C");
-                    Console.WriteLine($"Relative humidity: {humValue:0.#}%");
+    // WeatherHelper supports more calculations, such as saturated vapor pressure, actual vapor pressure and absolute humidity.
+    Console.WriteLine($"Heat index: {WeatherHelper.CalculateHeatIndex(tempValue, humValue).DegreesCelsius:0.#}\u00B0C");
+    Console.WriteLine($"Dew point: {WeatherHelper.CalculateDewPoint(tempValue, humValue).DegreesCelsius:0.#}\u00B0C");
+    Console.WriteLine();
 
-                    // WeatherHelper supports more calculations, such as saturated vapor pressure, actual vapor pressure and absolute humidity.
-                    Console.WriteLine($"Heat index: {WeatherHelper.CalculateHeatIndex(tempValue, humValue).DegreesCelsius:0.#}\u00B0C");
-                    Console.WriteLine($"Dew point: {WeatherHelper.CalculateDewPoint(tempValue, humValue).DegreesCelsius:0.#}\u00B0C");
-                    Console.WriteLine();
-
-                    Thread.Sleep(1000);
-                }
-            }
-        }
-    }
+    Thread.Sleep(1000);
 }
