23b39f084324ba5339a3b5bcf25ae1fdea2c8f33
Rich Lander <rlander@microsoft.com>
2020-11-01 22:04:24 -0800

    Update Lsm9Ds1 sample

---
+++ src/devices/Lsm9Ds1/samples/Program.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Threading;
+using System.Device.I2c;
+using Iot.Device.Lsm9Ds1.Samples;
+
+// uncomment to run accelerometer sample
+// AccelerometerAndGyroscope.Run();
+Magnetometer.Run();
