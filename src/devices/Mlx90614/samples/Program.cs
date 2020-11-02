b40d4c0a82aa5762353247a37d77179f06f01e04
Rich Lander <rlander@microsoft.com>
2020-11-02 07:29:20 -0800

    Update Mlx90614 project

--- src/devices/Mlx90614/samples/Program.cs
+++ src/devices/Mlx90614/samples/Program.cs
@@ -4,27 +4,17 @@
 using System;
 using System.Device.I2c;
 using System.Threading;
+using Iot.Device.Mlx90614;
 
-namespace Iot.Device.Mlx90614.Sample
+I2cConnectionSettings settings = new I2cConnectionSettings(1, Mlx90614.DefaultI2cAddress);
+I2cDevice i2cDevice = I2cDevice.Create(settings);
+
+using Mlx90614 sensor = new Mlx90614(i2cDevice);
+while (true)
 {
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
-            I2cConnectionSettings settings = new I2cConnectionSettings(1, Mlx90614.DefaultI2cAddress);
-            I2cDevice i2cDevice = I2cDevice.Create(settings);
+    Console.WriteLine($"Ambient: {sensor.ReadAmbientTemperature().DegreesCelsius} ℃");
+    Console.WriteLine($"Object: {sensor.ReadObjectTemperature().DegreesCelsius} ℃");
+    Console.WriteLine();
 
-            using (Mlx90614 sensor = new Mlx90614(i2cDevice))
-            {
-                while (true)
-                {
-                    Console.WriteLine($"Ambient: {sensor.ReadAmbientTemperature().DegreesCelsius} ℃");
-                    Console.WriteLine($"Object: {sensor.ReadObjectTemperature().DegreesCelsius} ℃");
-                    Console.WriteLine();
-
-                    Thread.Sleep(1000);
-                }
-            }
-        }
-    }
+    Thread.Sleep(1000);
 }
