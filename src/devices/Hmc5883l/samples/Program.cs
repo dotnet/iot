ea88236e0816b79c4130df963f111eeca4db331b
Rich Lander <rlander@microsoft.com>
2020-11-01 21:42:20 -0800

    Update Hmc5883l project

--- src/devices/Hmc5883l/samples/Program.cs
+++ src/devices/Hmc5883l/samples/Program.cs
@@ -4,40 +4,30 @@
 using System;
 using System.Device.I2c;
 using System.Threading;
+using Iot.Device.Hmc5883l;
 
-namespace Iot.Device.Hmc5883l.Samples
+I2cConnectionSettings settings = new I2cConnectionSettings(1, Hmc5883l.DefaultI2cAddress);
+I2cDevice device = I2cDevice.Create(settings);
+
+using Hmc5883l sensor = new Hmc5883l(device);
+while (true)
 {
-    internal class Program
+    // read heading
+    Console.WriteLine($"Heading: {sensor.Heading.ToString("0.00")} °");
+
+    var status = sensor.DeviceStatus;
+    Console.Write("Statuses: ");
+    foreach (Status item in Enum.GetValues(typeof(Status)))
     {
-        public static void Main(string[] args)
+        if (status.HasFlag(item))
         {
-            I2cConnectionSettings settings = new I2cConnectionSettings(1, Hmc5883l.DefaultI2cAddress);
-            I2cDevice device = I2cDevice.Create(settings);
-
-            using (Hmc5883l sensor = new Hmc5883l(device))
-            {
-                while (true)
-                {
-                    // read heading
-                    Console.WriteLine($"Heading: {sensor.Heading.ToString("0.00")} °");
-
-                    var status = sensor.DeviceStatus;
-                    Console.Write("Statuses: ");
-                    foreach (Status item in Enum.GetValues(typeof(Status)))
-                    {
-                        if (status.HasFlag(item))
-                        {
-                            Console.Write($"{item} ");
-                        }
-                    }
-
-                    Console.WriteLine();
-                    Console.WriteLine();
-
-                    // wait for a second
-                    Thread.Sleep(1000);
-                }
-            }
+            Console.Write($"{item} ");
         }
     }
+
+    Console.WriteLine();
+    Console.WriteLine();
+
+    // wait for a second
+    Thread.Sleep(1000);
 }
