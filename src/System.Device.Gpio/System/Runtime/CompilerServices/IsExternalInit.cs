2f1da076634a854bdaf42537b820eef7a80478b8
Rich Lander <rlander@microsoft.com>
2020-11-06 09:50:42 -0800

    Move record to primary class file

---
+++ src/System.Device.Gpio/System/Runtime/CompilerServices/IsExternalInit.cs
@@ -0,0 +1,10 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#pragma warning disable CS1591
+namespace System.Runtime.CompilerServices
+{
+    public class IsExternalInit
+    {
+    }
+}
