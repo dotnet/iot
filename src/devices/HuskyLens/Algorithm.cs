b3849f0a9076c214c69dd9e85ef10f7a7d9bf58b
Paul Meyer <paulmey@microsoft.com>
2020-11-09 07:49:57 -0800

    Add header to cs files

--- src/devices/HuskyLens/Algorithm.cs
+++ src/devices/HuskyLens/Algorithm.cs
@@ -1,4 +1,7 @@
-﻿namespace Iot.Device.HuskyLens
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.HuskyLens
 {
     /// <summary>
     /// Algorithms for HuskyLens
