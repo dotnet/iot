fabefaf7a96456550273505c814b670da07bbca5
Rich Lander <rlander@microsoft.com>
2020-11-12 22:24:22 -0800

    Update Ssd13xx sample (#1289)

--- src/devices/Ssd13xx/samples/Ssd1306Extensions.cs
+++ src/devices/Ssd13xx/samples/Ssd1306Extensions.cs
@@ -18,7 +18,7 @@ namespace Iot.Device.Ssd13xx.Samples
         /// </summary>
         /// <param name="s">Ssd1306 object.</param>
         /// <param name="image">Image to display.</param>
-        internal static void DisplayImage(this Ssd1306 s, Image<Gray16> image)
+        internal static void DisplayImage(this Ssd1306 s, Image<L16> image)
         {
             Int16 width = 128;
             Int16 pages = 4;
