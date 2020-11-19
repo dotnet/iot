41093404181d651b2bec2677f3a19aa86fbf56e6
Patrick Grawehr <pgrawehr@hotmail.com>
2020-11-19 17:28:48 +0100

    Unseal GpioController (#1254)
    * Unseal GpioController
    
    Need to implement a derived class later
    
    * Fix usage of NumberingScheme
    
    All public methods must translate the NumberingScheme to
    identify the correct pin that is meant.
    
    * Add unit tests for GpioController
    
    These run purely in software.
    
    * Use default NumberingScheme everywhere in Controller
    
    Simplifies method implementation
    
    * Add missing license header
    
    * Remove unnecessary cleanup call
    
    There's no finalizer present
    
    * Ensure a null driver doesn't cause problems in Dispose
    
    * IsPinOpen doesn't need to be virtual

---
+++ src/System.Device.Gpio.Tests/PinValueTests.cs
@@ -0,0 +1,42 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace System.Device.Gpio.Tests
+{
+    public class PinValueTests
+    {
+        [Fact]
+        public void VerifyConstants()
+        {
+            Assert.True(PinValue.High.Equals(1));
+            Assert.True(PinValue.Low.Equals(0));
+        }
+
+        [Fact]
+        public void VerifyEquality()
+        {
+            Assert.True(PinValue.High == 1);
+            Assert.True(PinValue.Low == 0);
+        }
+
+        [Fact]
+        public void VerifyInequality()
+        {
+            Assert.True(PinValue.High != 0);
+            Assert.True(PinValue.Low != 1);
+            Assert.True(PinValue.Low != 2);
+        }
+
+        [Fact]
+        public void Inverse()
+        {
+            Assert.Equal(PinValue.Low, !PinValue.High);
+            Assert.Equal(PinValue.High, !PinValue.Low);
+        }
+    }
+}
