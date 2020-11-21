32ce21200c07cbc88fdd563e4e17ab542f844552
Krzysztof Wicher <mordotymoja@gmail.com>
2020-11-21 17:43:44 +0100

    Update device listing (#1300)

--- src/devices/RotaryEncoder/samples/RotaryEncoder.Sample.cs
+++ src/devices/RotaryEncoder/samples/RotaryEncoder.Sample.cs
@@ -6,20 +6,17 @@ using System.Device.Gpio;
 using Iot.Device.RotaryEncoder;
 
 Console.WriteLine("Tune your radio station with RotaryEncoder and press a key to exit");
-using (GpioController controller = new GpioController())
+// create a RotaryEncoder that represents an FM Radio tuning dial with a range of 88 -> 108 MHz
+ScaledQuadratureEncoder encoder = new ScaledQuadratureEncoder(pinA: 5, pinB: 6, PinEventTypes.Falling, pulsesPerRotation: 20, pulseIncrement: 0.1, rangeMin: 88.0, rangeMax: 108.0) { Value = 88 };
+// 2 milliseconds debonce time
+encoder.Debounce = TimeSpan.FromMilliseconds(2);
+// Register to Value change events
+encoder.ValueChanged += (o, e) =>
 {
-    // create a RotaryEncoder that represents an FM Radio tuning dial with a range of 88 -> 108 MHz
-    ScaledQuadratureEncoder encoder = new ScaledQuadratureEncoder(pinA: 5, pinB: 6, PinEventTypes.Falling, pulsesPerRotation: 20, pulseIncrement: 0.1, rangeMin: 88.0, rangeMax: 108.0) { Value = 88 };
-    // 2 milliseconds debonce time
-    encoder.Debounce = TimeSpan.FromMilliseconds(2);
-    // Register to Value change events
-    encoder.ValueChanged += (o, e) =>
-    {
-        Console.WriteLine($"Tuned to {e.Value}MHz");
-    };
+    Console.WriteLine($"Tuned to {e.Value}MHz");
+};
 
-    while (!Console.KeyAvailable)
-    {
-        System.Threading.Tasks.Task.Delay(100).Wait();
-    }
+while (!Console.KeyAvailable)
+{
+    System.Threading.Tasks.Task.Delay(100).Wait();
 }
