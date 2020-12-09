7fc1865c3b80f07a8cd3d64ea8d575ffb2d673d4
Robin Müller <muellerobin95@gmail.com>
2020-12-09 15:23:36 +0100

    Bmxx80 API modification (#797)
    * Add ReadResult class
    
    * Add Read/ReadAsync methods for ease of use
    
    * Use nullable Properties, update samples
    
    * Fix UnitsNet related exceptions, remove redundancies

--- src/devices/Bmxx80/Bmp280.cs
+++ src/devices/Bmxx80/Bmp280.cs
@@ -1,9 +1,12 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Device.I2c;
-using Iot.Device.Bmxx80.Register;
+using System.Threading;
+using System.Threading.Tasks;
+using Iot.Device.Bmxx80.PowerMode;
+using Iot.Device.Bmxx80.ReadResult;
+using UnitsNet;
 
 namespace Iot.Device.Bmxx80
 {
@@ -25,6 +28,42 @@ namespace Iot.Device.Bmxx80
             : base(DeviceId, i2cDevice)
         {
             _communicationProtocol = CommunicationProtocol.I2c;
+        }
+
+        /// <summary>
+        /// Performs a synchronous reading.
+        /// </summary>
+        /// <returns><see cref="Bmp280ReadResult"/></returns>
+        public Bmp280ReadResult Read()
+        {
+            if (ReadPowerMode() != Bmx280PowerMode.Normal)
+            {
+                SetPowerMode(Bmx280PowerMode.Forced);
+                Thread.Sleep(GetMeasurementDuration());
+            }
+
+            var tempSuccess = TryReadTemperatureCore(out var temperature);
+            var pressSuccess = TryReadPressureCore(out var pressure, skipTempFineRead: true);
+
+            return new Bmp280ReadResult(tempSuccess ? temperature : null, pressSuccess ? pressure : null);
+        }
+
+        /// <summary>
+        /// Performs an asynchronous reading.
+        /// </summary>
+        /// <returns><see cref="Bmp280ReadResult"/></returns>
+        public async Task<Bmp280ReadResult> ReadAsync()
+        {
+            if (ReadPowerMode() != Bmx280PowerMode.Normal)
+            {
+                SetPowerMode(Bmx280PowerMode.Forced);
+                await Task.Delay(GetMeasurementDuration());
+            }
+
+            var tempSuccess = TryReadTemperatureCore(out var temperature);
+            var pressSuccess = TryReadPressureCore(out var pressure, skipTempFineRead: true);
+
+            return new Bmp280ReadResult(tempSuccess ? temperature : null, pressSuccess ? pressure : null);
         }
     }
 }
