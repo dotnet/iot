c039a129bd19bde00c5dc56eecec892180916161
Mike Insch <mike@insch.info>
2020-12-25 14:43:24 +0000

    Add basic sample.

--- src/devices/IS31FL3730/samples/IS31FL3730.Sample.cs
+++ src/devices/IS31FL3730/samples/IS31FL3730.Sample.cs
@@ -6,5 +6,19 @@ using System.Device.Gpio;
 using System.Device.I2c;
 using System.Device.Spi;
 using System.Threading;
+using Iot.Device.IS31FL3730;
 
-Console.WriteLine("Hello IS31FL3730 Sample!");
+Console.WriteLine("IS31FL3730 Sample!");
+
+I2cDevice device = I2cDevice.Create(new I2cConnectionSettings(1, IS31FL3730.DefaultI2cAddress));
+IS31FL3730 matrixController = new IS31FL3730(device, new DriverConfiguration()
+{
+  IsShutdown = false,
+  IsAudioInputEnabled = false,
+  Layout = MatrixLayout.Matrix8by8,
+  Mode = MatrixMode.Both
+});
+
+matrixController.Reset();
+
+matrixController.SetMatrix(MatrixMode.Both, new byte[] { 0xFF, 0x7F, 0x0F, 0xFF, 0xF7, 0xF0, 0x77, 0xAA });
