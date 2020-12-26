57c241bc274d10b48bcac6c1e6212d2e209ffde7
Mike Insch <mike@insch.info>
2020-12-26 21:57:24 +0000

    Initial Template.

---
+++ src/devices/Sgp30/Sgp30.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.Gpio;
+using System.Device.I2c;
+using System.Device.Spi;
+
+namespace Iot.Device.Sgp30
+{
+    /// <summary>
+    /// Add documentation here
+    /// </summary>
+    public class Sgp30 : IDisposable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Sgp30"/> class.
+        /// </summary>
+        public Sgp30()
+        {
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+        }
+    }
+}
