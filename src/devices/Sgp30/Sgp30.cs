c60328ac771c95eae5c82dde98d9fb60c91d7e03
Mike Insch <mike@insch.info>
2020-12-27 00:45:17 +0000

    Fix Get Featureset Version (device seems to pad an additional 0x00 at the beginning of every response).

--- src/devices/Sgp30/Sgp30.cs
+++ src/devices/Sgp30/Sgp30.cs
@@ -83,14 +83,14 @@ namespace Iot.Device.Sgp30
         /// <returns>Device Features.</returns>
         public ushort GetFeaturesetVersion()
         {
-            Span<byte> resultBuffer = stackalloc byte[3];
+            Span<byte> resultBuffer = stackalloc byte[4];
             _i2cDevice.Write(new ReadOnlySpan<byte>(new byte[] { (byte)((SGP30_GET_FEATURESET_VERSION & 0xFF00) >> 8), (byte)(SGP30_GET_FEATURESET_VERSION & 0x00FF) }));
             _i2cDevice.Read(resultBuffer.Slice(1));
             byte[] resultArray = resultBuffer.ToArray();
-            ushort result = (ushort)(resultArray[0] << 8 | resultArray[1]);
+            ushort result = (ushort)(resultArray[1] << 8 | resultArray[2]);
             byte checksum = CalculateChecksum(result);
 
-            if (checksum != resultArray[2])
+            if (checksum != resultArray[3])
             {
                 throw new ChecksumFailedException();
             }
