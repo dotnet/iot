ab8a36b1985c7b2773ec8bc62529aac096e46a00
amgarnier <62999302+amgarnier@users.noreply.github.com>
2021-01-14 06:40:45 -0500

    Added MAX31856 sensor to devices library (#1343)
    * Added MAX31856 sensor to devices library
    
    * update1 to MAX31856 device
    
    * Update MAX31856.cs
    
    * Update2 to max31856
    
    * change3 Max31856
    
    * Updated some comments and read me
    
    * updated README
    
    * Updated Readme and several other changes
    
    * updataed try/catch and readme
    
    * Updated data to simplify
    
    * updates with simpler reading of bytes
    
    * updated the sample as well
    
    * removed consoleapp
    
    * Updated names to reflect Max31856 vice MAX31856 should fix linux build issue
    
    * removed console.writeline for debugging
    
    * renamed to MAX31856-2
    
    * changed file name as per conversation
    
    * Update stylecop.json
    
    updated it in github to remove line
    
    * revert change in eng/stylecop.json
    
    Co-authored-by: Krzysztof Wicher <kwicher@microsoft.com>

---
+++ src/devices/Max31856/ThermocoupleType.cs
@@ -0,0 +1,51 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Max31856
+{
+    /// <summary>
+    /// Sets the Thermocouple Type
+    /// </summary>
+    public enum ThermocoupleType : byte
+    {
+        /// <summary>
+        /// Type B thermocouple
+        /// </summary>
+        B = 0x00,
+
+        /// <summary>
+        /// Type E thermocouple
+        /// </summary>
+        E = 0x01,
+
+        /// <summary>
+        /// Type J thermocouple
+        /// </summary>
+        J = 0x02,
+
+        /// <summary>
+        /// Type K thermocouple
+        /// </summary>
+        K = 0x03,
+
+        /// <summary>
+        /// Type N thermocouple
+        /// </summary>
+        N = 0x04,
+
+        /// <summary>
+        /// Type R thermocouple
+        /// </summary>
+        R = 0x05,
+
+        /// <summary>
+        /// Type S thermocouple
+        /// </summary>
+        S = 0x06,
+
+        /// <summary>
+        /// Type T thermocouple
+        /// </summary>
+        T = 0x07
+    }
+}
---
+++ src/devices/Max31856/samples/Max31856.Sample.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.Spi;
+using System.Threading;
+using Iot.Device.Max31856;
+using UnitsNet;
+
+SpiConnectionSettings settings = new(0, 0)
+{
+    ClockFrequency = Max31856.SpiClockFrequency,
+    Mode = Max31856.SpiModeSetup,
+    DataFlow = Max31856.SpiDataFlow
+};
+
+using SpiDevice device = SpiDevice.Create(settings);
+using Max31856 sensor = new(device, ThermocoupleType.K);
+while (true)
+{
+    Temperature tempThermocouple = sensor.GetTemperature();
+    Console.WriteLine($"Temperature Thermocouple: {tempThermocouple.DegreesFahrenheit} F");
+    Temperature tempColdJunction = sensor.GetColdJunctionTemperature();
+    Console.WriteLine($"Temperature Cold Junction: {tempColdJunction.DegreesFahrenheit} F");
+    Thread.Sleep(2000);
+}
