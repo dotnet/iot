e350326badccf9965e31c582ef867a67902c7983
Henry Hunt <henryhunt.dev@gmail.com>
2021-01-21 17:01:57 +0000

    Update DS3231 binding to add support for the alarms (#1150)
    * Implemented DS3231 alarm 1 and alarm 2

---
+++ src/devices/Rtc/Devices/Ds3231/Ds3231Alarm.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Rtc
+{
+    /// <summary>
+    /// Available alarms on the DS3231
+    /// </summary>
+    public enum Ds3231Alarm
+    {
+        /// <summary>
+        /// Indicates none of the alarms
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Indicates the first alarm
+        /// </summary>
+        AlarmOne,
+
+        /// <summary>
+        /// Indicates the second alarm
+        /// </summary>
+        AlarmTwo
+    }
+}
---
+++ src/devices/Rtc/Devices/Ds3231/Ds3231AlarmOneMatchMode.cs
@@ -0,0 +1,41 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Rtc
+{
+    /// <summary>
+    /// Available modes for determining when alarm 1 should trigger
+    /// </summary>
+    public enum Ds3231AlarmOneMatchMode : byte
+    {
+        /// <summary>
+        /// Alarm 1 triggers at the start of every second
+        /// </summary>
+        OncePerSecond = 0x0F,
+
+        /// <summary>
+        /// Alarm 1 triggers when the seconds match
+        /// </summary>
+        Seconds = 0x0E,
+
+        /// <summary>
+        /// Alarm 1 triggers when the minutes and seconds match
+        /// </summary>
+        MinutesSeconds = 0x0C,
+
+        /// <summary>
+        /// Alarm 1 triggers when the hours, minutes and seconds match
+        /// </summary>
+        HoursMinutesSeconds = 0x08,
+
+        /// <summary>
+        /// Alarm 1 triggers when the day of the month, hours, minutes and seconds match
+        /// </summary>
+        DayOfMonthHoursMinutesSeconds = 0x00,
+
+        /// <summary>
+        /// Alarm 1 triggers when the day of the week, hours, minutes and seconda match. Sunday is day 1
+        /// </summary>
+        DayOfWeekHoursMinutesSeconds = 0x10
+    }
+}
