8938de68c278229c9b5a9e9119456d8fef6f0aa1
Vorn Mom <vorn@vorn.us>
2021-01-20 21:25:21 -0500

    Fix samples

--- src/devices/LidarLiteV3/samples/Program.cs
+++ src/devices/LidarLiteV3/samples/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 using System;
 using System.Threading;
 using System.Device.I2c;
 using Iot.Device.DistanceSensor;
+using UnitsNet;
 
 using (var llv3 = new LidarLiteV3(CreateI2cDevice()))
 {
     // Take 10 measurements, each one second apart.
     for (int i = 0; i < 10; i++)
     {
-        int currentDistance = llv3.MeasureDistance();
-        Console.WriteLine($"Current Distance: {currentDistance} cm");
+        Length currentDistance = llv3.MeasureDistance();
+        Console.WriteLine($"Current Distance: {currentDistance.Centimeters} cm");
         Thread.Sleep(1000);
     }
 }
