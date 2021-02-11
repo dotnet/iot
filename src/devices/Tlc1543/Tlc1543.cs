3ec3c24e04a166ff26aa329450b8be09b51150a0
Dawid <dawd785@gmail.com>
2021-02-11 08:04:40 +0100

    Add device binding for Tlc1543 binding (#1192)
    * Add Tlc1543 binding
    
    * Deleting older sample code
    
    * Suggested PR changes
    
    * GPIO and naming fixes
    
    * Deleted unnecessary property
    
    * Fixed missing default value
    
    * Added helper method for checking channel validity
    
    * Implemented GPIO transfer modes
    
    * Use SPI
    
    * Fix the build
    
    Co-authored-by: Krzysztof Wicher <kwicher@microsoft.com>
    Co-authored-by: Jose Perez Rodriguez <joperezr@microsoft.com>

---
+++ src/devices/Tlc1543/Tlc1543.cs
@@ -0,0 +1,132 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device;
+using System.Device.Gpio;
+using System.Collections.Generic;
+using System.Device.Spi;
+
+namespace Iot.Device.Tlc1543
+{
+    /// <summary>
+    /// Add documentation here
+    /// </summary>
+    public class Tlc1543 : IDisposable
+    {
+        /// <summary>
+        /// DataBitLength to set on SPI device.
+        /// </summary>
+        public const int SpiDataBitLength = 5;
+
+        // 21 microseconds wait time in case end-of-conversion pin is disconnected (spec: operating characteristic, t_conv)
+        // Adding 999 is to perform Math.Ceil without going through double and Math.Ceil
+        private static readonly TimeSpan _conversionTime = new TimeSpan((21 * TimeSpan.TicksPerMillisecond + 999) / 1000);
+        private readonly int _endOfConversion;
+        private readonly bool _shouldDispose;
+        private SpiDevice _spiDevice;
+        private GpioController _controller;
+
+        /// <summary>
+        /// Constructor for Tlc1543
+        /// </summary>
+        /// <param name="spiDevice">Device used for SPI communication.</param>
+        /// <param name="endOfConversion">End of Conversion pin, if GpioController is not provided logical numbering scheme is used (default for GpioController).</param>
+        /// <param name="controller">The GPIO controller for defined external pins. If not specified, the default controller will be used.</param>
+        /// <param name="shouldDispose">True to dispose the GPIO controller and SPI device.</param>
+        public Tlc1543(SpiDevice spiDevice, int endOfConversion = -1, GpioController? controller = null, bool shouldDispose = true)
+        {
+            if (spiDevice == null)
+            {
+                throw new ArgumentNullException(nameof(spiDevice));
+            }
+
+            if (spiDevice.ConnectionSettings.DataBitLength != SpiDataBitLength)
+            {
+                throw new ArgumentException($"SpiDevice is required to be have DataBitLength={SpiDataBitLength}.", nameof(spiDevice));
+            }
+
+            _spiDevice = spiDevice;
+            _shouldDispose = controller == null || shouldDispose;
+            _controller = controller ?? new GpioController();
+
+            _endOfConversion = endOfConversion;
+
+            if (_endOfConversion != -1)
+            {
+                _controller.OpenPin(_endOfConversion, PinMode.InputPullUp);
+            }
+        }
+
+        private static bool IsValidChannel(Channel channel) => channel >= Channel.A0 && channel <= Channel.SelfTestMax;
+
+        /// <summary>
+        /// Reads previous reading and prepares next reading for specified channel
+        /// </summary>
+        /// <param name="channelToCharge">Channel to prepare</param>
+        /// <returns>10 bit value corresponding to relative voltage level on channel</returns>
+        public int ReadPreviousAndChargeChannel(Channel channelToCharge)
+        {
+            if (!IsValidChannel(channelToCharge))
+            {
+                throw new ArgumentOutOfRangeException(nameof(channelToCharge));
+            }
+
+            Span<byte> readBuffer = stackalloc byte[2];
+            Span<byte> writeBuffer = stackalloc byte[2];
+
+            writeBuffer[0] = (byte)((int)channelToCharge << 1);
+            _spiDevice.TransferFullDuplex(writeBuffer, readBuffer);
+
+            int previousReading = ((readBuffer[0] & 0b11111) << 5) | (readBuffer[1] & 0b11111);
+            if (_endOfConversion != -1)
+            {
+                // Wait for ADC to report end of conversion or timeout at max conversion time
+                _controller.WaitForEvent(_endOfConversion, PinEventTypes.Rising, _conversionTime);
+            }
+            else
+            {
+                // Max conversion time (21us) as seen in table on page 10 in TLC1543 documentation
+                DelayHelper.Delay(_conversionTime, false);
+            }
+
+            return previousReading;
+        }
+
+        /// <summary>
+        /// Reads sensor value.
+        /// First cycle: Ask for value on the channel <paramref name="channelNumber"/>.
+        /// Second cycle: Return value from the channel while charging <paramref name="nextChannelToCharge"/>.
+        /// </summary>
+        /// <param name="channelNumber">Channel to be read</param>
+        /// <param name="nextChannelToCharge">Next channel to charge</param>
+        /// <returns>A 10 bit value corresponding to relative voltage level on specified device channel</returns>
+        public int ReadChannel(Channel channelNumber, Channel nextChannelToCharge = Channel.SelfTestHalf)
+        {
+            ReadPreviousAndChargeChannel(channelNumber);
+            return ReadPreviousAndChargeChannel(nextChannelToCharge);
+        }
+
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        public void Dispose()
+        {
+            if (_controller != null)
+            {
+                if (_shouldDispose)
+                {
+                    _controller.Dispose();
+                    _spiDevice.Dispose();
+                }
+                else if (_endOfConversion != -1)
+                {
+                    _controller.ClosePin(_endOfConversion);
+                }
+
+                _controller = null!;
+                _spiDevice = null!;
+            }
+        }
+    }
+}
---
+++ src/devices/Tlc1543/samples/Tlc1543.Sample.cs
@@ -0,0 +1,66 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Device.Spi;
+using Iot.Device.Spi;
+
+namespace Iot.Device.Tlc1543.Samples
+{
+    /// <summary>
+    /// Samples for Tlc1543
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        public static void Main()
+        {
+            SoftwareSpi spi = new SoftwareSpi(
+                clk: 25,
+                sdi: 23,
+                sdo: 24,
+                cs: 5,
+                settings: new SpiConnectionSettings(-1) { DataBitLength = Tlc1543.SpiDataBitLength });
+
+            Tlc1543 adc = new Tlc1543(spi);
+            Channel[] channels = new Channel[]
+            {
+                Channel.A0,
+                Channel.A1,
+                Channel.A2,
+                Channel.A3,
+                Channel.A4
+            };
+
+            foreach (Channel channel in channels)
+            {
+                Console.WriteLine($"Channel {channel}: {adc.ReadChannel(channel)}");
+            }
+
+            // or a bit faster
+            // we ignore the first reading
+            adc.ReadPreviousAndChargeChannel(channels[0]);
+            for (int i = 0; i < channels.Length; i++)
+            {
+                // For last reading we need to pass something so let's pass test channel
+                Channel nextChannel = i < channels.Length - 1 ? channels[i + 1] : Channel.SelfTestHalf;
+                int previous = adc.ReadPreviousAndChargeChannel(nextChannel);
+                Console.WriteLine($"Channel {channels[i]}: {previous}");
+            }
+
+            // now continuously read from one channel
+            Channel ch = Channel.A0;
+            int numberOfReadings = 10;
+            adc.ReadPreviousAndChargeChannel(ch);
+
+            for (int i = 0; i < numberOfReadings; i++)
+            {
+                Console.WriteLine($"Channel {ch}: {adc.ReadPreviousAndChargeChannel(ch)}");
+            }
+        }
+    }
+}
