debac4d03ecc2398205ba5962525205bcf7b47dd
Zhang Yuexin <zhangyuexin121@live.cn>
2021-02-11 14:12:46 +0800

    Add a generic GPIO driver for Allwinner SoCs and Orange Pi Zero/Lite/Lite2 pin map (#1130)
    * add cs files
    
    * add csproj
    
    * resolve some feedbacks
    
    * MSBuild fail
    
    * derive from UnixDriver
    
    * reference source files
    
    * reference csproj
    
    * resolve some issues
    
    * using GpioController
    
    * _interruptController is assigned a value in the constructor
    
    * _interruptController nullable type
    
    * nullable check
    
    * SunxiDriver instead of inheriting SysFsDriver; add samples
    
    * add readme; update samples
    
    * add pin mode check; add static to _pinNumberConverter
    
    * call initialize in constructor
    
    * update README.md
    
    * fix init problem; add some validates
    
    * add Orange Pi Lite driver
    
    * clean up; fix cpux-port initial problem; remove internal exceptions
    
    * remove tiny delay
    
    * private opi zero _pinNumberConverter
    
    * add xml comment
    
    * update README.md
    
    * fix error SA1208
    
    * fix copyright headers
    
    * cleanup
    
    * fix error SA1028
    
    * fix CPUX port offset
    
    * count the number of pins; update README.md
    
    * fix typo
    
    * Fix Interop duplication
    
    Co-authored-by: Jose Perez Rodriguez <joperezr@microsoft.com>

---
+++ src/devices/Gpio/Drivers/OrangePiZeroDriver.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Iot.Device.Gpio.Drivers
+{
+    /// <summary>
+    /// A GPIO driver for the Orange Pi Zero.
+    /// </summary>
+    /// <remarks>
+    /// SoC: Allwinner H2+ (sun8iw7p1)
+    /// </remarks>
+    public class OrangePiZeroDriver : Sun8iw7p1Driver
+    {
+        private static readonly int[] _pinNumberConverter = new int[]
+        {
+            -1, -1, -1, MapPinNumber('A', 12), -1, MapPinNumber('A', 11), -1, MapPinNumber('A', 6), MapPinNumber('G', 6), -1,
+            MapPinNumber('G', 7), MapPinNumber('A', 1), MapPinNumber('A', 7), MapPinNumber('A', 0), -1, MapPinNumber('A', 3),
+            MapPinNumber('A', 19), -1, MapPinNumber('A', 18), MapPinNumber('A', 15), -1, MapPinNumber('A', 16), MapPinNumber('A', 2),
+            MapPinNumber('A', 14), MapPinNumber('A', 13), -1, MapPinNumber('A', 10)
+        };
+
+        /// <inheritdoc/>
+        protected override int PinCount => 17;
+
+        /// <inheritdoc/>
+        protected override int ConvertPinNumberToLogicalNumberingScheme(int pinNumber)
+        {
+            int num = _pinNumberConverter[pinNumber];
+
+            return num != -1 ? num : throw new ArgumentException($"Board (header) pin {pinNumber} is not a GPIO pin on the {GetType().Name} device.", nameof(pinNumber));
+        }
+    }
+}
---
+++ src/devices/Gpio/Drivers/Sunxi/Sun50iw6p1Driver.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Gpio.Drivers
+{
+    /// <summary>
+    /// A GPIO driver for Allwinner H6.
+    /// </summary>
+    public class Sun50iw6p1Driver : SunxiDriver
+    {
+        /// <inheritdoc/>
+        protected override int CpuxPortBaseAddress => 0x0300B000;
+
+        /// <inheritdoc/>
+        protected override int CpusPortBaseAddress => 0x07022000;
+    }
+}
