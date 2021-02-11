fe22a8054b49cb42adaf5782d7f731ca1eea0cc6
Zhang Yuexin <zhangyuexin121@live.cn>
2021-02-11 14:12:46 +0800

    Add a generic GPIO driver for Allwinner SoCs and Orange Pi Zero/Lite/Lite2 pin map (#1130)
    * add cs files
    
    * add csproj
    
    * resolve some feedbacks
    
    * MSBuild fail
    
    * derive from UnixDriver
    
    * reference source files
    
    * reference csproj
    
    * resolve some issues
    
    * using GpioController
    
    * _interruptController is assigned a value in the constructor
    
    * _interruptController nullable type
    
    * nullable check
    
    * SunxiDriver instead of inheriting SysFsDriver; add samples
    
    * add readme; update samples
    
    * add pin mode check; add static to _pinNumberConverter
    
    * call initialize in constructor
    
    * update README.md
    
    * fix init problem; add some validates
    
    * add Orange Pi Lite driver
    
    * clean up; fix cpux-port initial problem; remove internal exceptions
    
    * remove tiny delay
    
    * private opi zero _pinNumberConverter
    
    * add xml comment
    
    * update README.md
    
    * fix error SA1208
    
    * fix copyright headers
    
    * cleanup
    
    * fix error SA1028
    
    * fix CPUX port offset
    
    * count the number of pins; update README.md
    
    * fix typo
    
    * Fix Interop duplication
    
    Co-authored-by: Jose Perez Rodriguez <joperezr@microsoft.com>

---
+++ src/devices/Gpio/Drivers/OrangePiLite2Driver.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Iot.Device.Gpio.Drivers
+{
+    /// <summary>
+    /// A GPIO driver for the Orange Pi Lite 2.
+    /// </summary>
+    /// <remarks>
+    /// SoC: Allwinner H6 (sun50iw6p1)
+    /// </remarks>
+    public class OrangePiLite2Driver : Sun50iw6p1Driver
+    {
+        private static readonly int[] _pinNumberConverter = new int[]
+        {
+            -1, -1, -1, MapPinNumber('H', 6), -1, MapPinNumber('H', 5), -1, MapPinNumber('H', 4), MapPinNumber('D', 21), -1,
+            MapPinNumber('D', 22), MapPinNumber('D', 24), MapPinNumber('C', 9), MapPinNumber('D', 23), -1, MapPinNumber('D', 26),
+            MapPinNumber('C', 8), -1, MapPinNumber('C', 7), MapPinNumber('C', 2), -1, MapPinNumber('C', 3), MapPinNumber('D', 25),
+            MapPinNumber('C', 0), MapPinNumber('C', 5), -1, MapPinNumber('H', 3)
+        };
+
+        /// <inheritdoc/>
+        protected override int PinCount => 17;
+
+        /// <inheritdoc/>
+        protected override int ConvertPinNumberToLogicalNumberingScheme(int pinNumber)
+        {
+            int num = _pinNumberConverter[pinNumber];
+
+            return num != -1 ? num : throw new ArgumentException($"Board (header) pin {pinNumber} is not a GPIO pin on the {GetType().Name} device.", nameof(pinNumber));
+        }
+    }
+}
---
+++ src/devices/Gpio/Drivers/OrangePiLiteDriver.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Iot.Device.Gpio.Drivers
+{
+    /// <summary>
+    /// A GPIO driver for the Orange Pi Lite.
+    /// </summary>
+    /// <remarks>
+    /// SoC: Allwinner H3 (sun8iw7p1)
+    /// </remarks>
+    public class OrangePiLiteDriver : Sun8iw7p1Driver
+    {
+        private static readonly int[] _pinNumberConverter = new int[]
+        {
+            -1, -1, -1, MapPinNumber('A', 12), -1, MapPinNumber('A', 11), -1, MapPinNumber('A', 6), MapPinNumber('A', 13), -1,
+            MapPinNumber('A', 14), MapPinNumber('A', 1), MapPinNumber('D', 14), MapPinNumber('A', 0), -1, MapPinNumber('A', 3),
+            MapPinNumber('C', 4), -1, MapPinNumber('C', 7), MapPinNumber('C', 0), -1, MapPinNumber('C', 1), MapPinNumber('A', 2),
+            MapPinNumber('C', 2), MapPinNumber('C', 3), -1, MapPinNumber('A', 21), MapPinNumber('A', 19), MapPinNumber('A', 18),
+            MapPinNumber('A', 7), -1, MapPinNumber('A', 8), MapPinNumber('G', 8), MapPinNumber('A', 9), -1, MapPinNumber('A', 10),
+            MapPinNumber('G', 9), MapPinNumber('A', 20), MapPinNumber('G', 6), -1, MapPinNumber('G', 7)
+        };
+
+        /// <inheritdoc/>
+        protected override int PinCount => 28;
+
+        /// <inheritdoc/>
+        protected override int ConvertPinNumberToLogicalNumberingScheme(int pinNumber)
+        {
+            int num = _pinNumberConverter[pinNumber];
+
+            return num != -1 ? num : throw new ArgumentException($"Board (header) pin {pinNumber} is not a GPIO pin on the {GetType().Name} device.", nameof(pinNumber));
+        }
+    }
+}
