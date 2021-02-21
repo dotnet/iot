2547d6868147e2da5b2f4dd8e21c23d5fe5979b7
Zhang Yuexin <zhangyuexin121@live.cn>
2021-02-21 16:58:58 +0800

    add rockchip driver; add benchmark

---
+++ src/devices/Gpio/Drivers/NanoPiR2sDriver.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Iot.Device.Gpio.Drivers
+{
+    /// <summary>
+    /// A GPIO driver for the NanoPi R2S
+    /// </summary>
+    /// <remarks>
+    /// SoC: Rockchip RK3328
+    /// </remarks>
+    public class NanoPiR2sDriver : Rk3328Driver
+    {
+        private static readonly int[] _pinNumberConverter = new int[]
+        {
+            -1, -1, -1,  MapPinNumber(2, 'D', 1), -1, MapPinNumber(2, 'D', 0), -1,
+            MapPinNumber(2, 'A', 2), MapPinNumber(3, 'A', 4), -1, MapPinNumber(3, 'A', 6)
+        };
+
+        /// <inheritdoc/>
+        protected override int PinCount => 5;
+
+        /// <inheritdoc/>
+        protected override int ConvertPinNumberToLogicalNumberingScheme(int pinNumber)
+        {
+            int num = _pinNumberConverter[pinNumber];
+
+            return num != -1 ? num : throw new ArgumentException($"Board (header) pin {pinNumber} is not a GPIO pin on the {GetType().Name} device.", nameof(pinNumber));
+        }
+    }
+}
