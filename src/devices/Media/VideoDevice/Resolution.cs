e9626c9a44dea8db23cbcd0963fa1ce77558d6b5
Omar Raad <omarraad@gmail.com>
2021-03-09 19:39:15 +1100

    Split classes into files

--- src/devices/Media/VideoDevice/Resolution.cs
+++ src/devices/Media/VideoDevice/Resolution.cs
@@ -1,31 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-
 namespace Iot.Device.Media
 {
-    /// <summary>
-    /// The resolution type of a video device.
-    /// </summary>
-    public enum ResolutionType : uint
-    {
-        /// <summary>
-        /// Discrete
-        /// </summary>
-        Discrete = v4l2_frmsizetypes.V4L2_FRMSIZE_TYPE_DISCRETE,
-
-        /// <summary>
-        /// Continuous
-        /// </summary>
-        Continuous = v4l2_frmsizetypes.V4L2_FRMSIZE_TYPE_CONTINUOUS,
-
-        /// <summary>
-        /// Stepwise
-        /// </summary>
-        Stepwise = v4l2_frmsizetypes.V4L2_FRMSIZE_TYPE_STEPWISE
-    }
-
     /// <summary>
     /// The resolution of a video device.
     /// </summary>
