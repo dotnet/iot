886e8a5336f91f74dc197ce9360ce706a6f436b2
Patrick Grawehr <pgrawehr@hotmail.com>
2021-03-12 21:11:21 +0100

    Add a generic logging infrastructure (#1332)
    * Include Microsoft.Extensions.Logging.Abstractions everywhere
    
    * Provide base infrastructure for using Microsoft.Extensions.Logging.Abstractions
    
    Allows to have a common base for logging
    withing all bindings.
    
    Simple console logger provided as well, but
    in a larger application, you might want to
    forward the logs to some logging framework
    such as NLog.
    
    * Add missing documentation
    
    * Properly format csharp code
    
    * Use correct version of Microsoft.Extensions.Logging
    
    * Few small changes on how package versions are handled
    
    Co-authored-by: Jose Perez Rodriguez <joperezr@microsoft.com>

---
+++ src/devices/Common/Iot/Device/Common/LogDispatcher.cs
@@ -0,0 +1,83 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace Iot.Device.Common
+{
+    /// <summary>
+    /// This class contains static members that provide log support.
+    /// </summary>
+    public static class LogDispatcher
+    {
+        private static object _lock = new object();
+
+        /// <summary>
+        /// The default logger factory for the whole assembly.
+        /// If this is null (the default), logging is disabled
+        /// </summary>
+        public static ILoggerFactory? LoggerFactory { get; set; } = null;
+
+        /// <summary>
+        /// Gets a logger with the given name
+        /// </summary>
+        /// <param name="loggerName">Name of the logger</param>
+        /// <returns>A reference to a <see cref="ILogger"/>.</returns>
+        public static ILogger GetLogger(string loggerName)
+        {
+            if (LoggerFactory == null)
+            {
+                return new NullLogger();
+            }
+
+            return LoggerFactory.CreateLogger(loggerName);
+        }
+
+        /// <summary>
+        /// Gets a logger with the name of the current class
+        /// </summary>
+        /// <param name="currentClass">The class whose logger shall be retrieved</param>
+        /// <returns>A <see cref="ILogger"/> instance</returns>
+        public static ILogger GetCurrentClassLogger(this object currentClass)
+        {
+            string? name = currentClass.GetType().FullName;
+
+            // This is true if the method is used from an incomplete (generic) type
+            if (name == null)
+            {
+                name = currentClass.GetType().Name;
+            }
+
+            return GetLogger(name);
+        }
+
+        private class NullLogger : ILogger
+        {
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+            }
+
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return false;
+            }
+
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                return new ScopeDisposable();
+            }
+        }
+
+        /// <summary>
+        /// This doesn't really do anything
+        /// </summary>
+        internal sealed class ScopeDisposable : IDisposable
+        {
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
---
+++ src/devices/Common/Iot/Device/Common/SimpleConsoleLoggerFactory.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Iot.Device.Common
+{
+    /// <summary>
+    /// Provides a very simple console logger that does not require a reference to Microsoft.Extensions.Logging.dll
+    /// </summary>
+    public class SimpleConsoleLoggerFactory : ILoggerFactory
+    {
+        /// <summary>
+        /// The console logger is built-in here
+        /// </summary>
+        /// <param name="provider">Argument is ignored</param>
+        public void AddProvider(ILoggerProvider provider)
+        {
+        }
+
+        /// <inheritdoc/>
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new SimpleConsoleLogger(categoryName);
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+        }
+    }
+}
