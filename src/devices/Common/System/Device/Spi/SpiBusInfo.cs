32554e223d72c7dfcde692355f026bc237434cb2
captncode <captncode@hotmail.com>
2021-03-19 06:21:08 -0400

    SpiBusInfo stub added (#1438)

---
+++ src/devices/Common/System/Device/Spi/SpiBusInfo.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Device.Spi
+{
+    public static class SpiBusInfo
+    {
+        private static int s_bufferSize = GetBufferSize();
+
+        // we would need to decide if this should throw when size can't be found or just return -1 or other placeholder
+        public static int BufferSize => s_bufferSize;
+
+        private static int GetBufferSize()
+        {
+            // non-throwing version of int.Parse(File.ReadAllText("/sys/module/spidev/parameters/bufsiz"))
+            // return -1 on error
+        }
+    }
+}
