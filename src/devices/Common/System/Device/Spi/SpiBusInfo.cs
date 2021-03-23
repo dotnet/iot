2255466051d81a65bc430433b4363f981f1a7cc3
captncode <captncode@hotmail.com>
2021-03-22 21:26:59 -0400

    SpiBusInfo.GetBufferSize updated to retrieve and return buffer size. (#1483)

--- src/devices/Common/System/Device/Spi/SpiBusInfo.cs
+++ src/devices/Common/System/Device/Spi/SpiBusInfo.cs
@@ -1,19 +1,41 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.IO;
+using System.Runtime.InteropServices;
+
 namespace System.Device.Spi
 {
+    /// <summary>
+    /// Base class for SPI bus information.
+    /// </summary>
     public static class SpiBusInfo
     {
         private static int s_bufferSize = GetBufferSize();
 
-        // we would need to decide if this should throw when size can't be found or just return -1 or other placeholder
+        /// <summary>
+        /// Buffer size assigned to the SPI bus or -1 if the buffer size is not available.
+        /// </summary>
         public static int BufferSize => s_bufferSize;
 
         private static int GetBufferSize()
         {
-            // non-throwing version of int.Parse(File.ReadAllText("/sys/module/spidev/parameters/bufsiz"))
-            // return -1 on error
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                const string parameterPath = "/sys/module/spidev/parameters/bufsiz";
+                if (File.Exists(parameterPath) && int.TryParse(File.ReadAllText(parameterPath), out int buferSize))
+                {
+                    return buferSize;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else
+            {
+                throw new PlatformNotSupportedException("This value is only supported on Linux operating systems");
+            }
         }
     }
 }
