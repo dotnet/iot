405d4290383f688de113c19dcf99ce094d4ff41f
aexhg <8421323+aexhg@users.noreply.github.com>
2021-06-10 21:51:56 +0100

    Add bindings for SensorHub board (#1535)
    * Add bindings for SensorHub board
    
    * PR feedback
    
    Remove units from code documentation to remarks section.
    Use BinaryPrimitives for conversion and enums.
    Remove static constructor and default bus id.
    Update README with correct formatting and example.
    
    * Update src/devices/SensorHub/SensorHub.csproj
    
    Adding netstandard2.0 support
    
    Co-authored-by: Patrick Grawehr <Patrick.Grawehr@leica-geosystems.com>
    
    * Check for connected SensorHub in constructor.
    
    Co-authored-by: aexhg <aexhg@users.noreply.github.com>
    Co-authored-by: Jose Perez Rodriguez <joperezr@microsoft.com>
    Co-authored-by: Patrick Grawehr <Patrick.Grawehr@leica-geosystems.com>

---
+++ src/devices/SensorHub/samples/SensorHub.Sample.cs
@@ -0,0 +1,52 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.I2c;
+using System.Threading;
+using Iot.Device.SensorHub;
+
+const int I2cBusId = 1;
+I2cConnectionSettings connectionSettings = new(I2cBusId, SensorHub.DefaultI2cAddress);
+SensorHub sh = new(I2cDevice.Create(connectionSettings));
+
+while (true)
+{
+    if (sh.TryReadOffBoardTemperature(out var t))
+    {
+        Console.WriteLine($"OffBoard temperature {t}");
+    }
+
+    if (sh.TryReadBarometerPressure(out var p))
+    {
+        Console.WriteLine($"Pressure {p}");
+    }
+
+    if (sh.TryReadBarometerTemperature(out var bt))
+    {
+        Console.WriteLine($"Barometer temperature {bt}");
+    }
+
+    if (sh.TryReadIlluminance(out var l))
+    {
+        Console.WriteLine($"Illuminance {l}");
+    }
+
+    if (sh.TryReadOnBoardTemperature(out var ot))
+    {
+        Console.WriteLine($"OnBoard temperature {ot}");
+    }
+
+    if (sh.TryReadRelativeHumidity(out var h))
+    {
+        Console.WriteLine($"Relative humidity {h}");
+    }
+
+    if (sh.IsMotionDetected)
+    {
+        Console.WriteLine("Motion detected");
+    }
+
+    Console.WriteLine();
+    Thread.Sleep(TimeSpan.FromSeconds(1));
+}
---
+++ src/devices/SensorHub/tests/SensorHub.Tests.cs
@@ -0,0 +1,139 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.I2c;
+using System.IO;
+using Moq;
+using Xunit;
+
+namespace Iot.Device.SensorHub.Tests
+{
+    public class SensorHubTests
+    {
+        private Mock<I2cDevice> _mockI2cDevice;
+
+        public SensorHubTests()
+        {
+            _mockI2cDevice = new();
+            _mockI2cDevice.SetupGet(c => c.ConnectionSettings).Returns(new I2cConnectionSettings(1, 0x17));
+        }
+
+        [Fact]
+        public void Should_Throw_When_No_Offboard_Sensor_Found()
+        {
+            SensorHub sh = new(_mockI2cDevice.Object);
+            _mockI2cDevice.SetupSequence(x => x.ReadByte())
+                .Returns((byte)SensorHub.RegisterStatusErrors.OFFBOARD_TEMPERATURE_SENSOR_NOT_FOUND);
+
+            var exception = Assert.Throws<IOException>(() => sh.TryReadOffBoardTemperature(out var t));
+            Assert.Equal("No offboard temperature sensor found", exception.Message);
+        }
+
+        [Fact]
+        public void Should_Throw_When_NoDevice_Found()
+        {
+            Func<SensorHub> sh = () => new(_mockI2cDevice.Object);
+            _mockI2cDevice.Setup(x => x.ReadByte()).Throws(new IOException("Not Connected"));
+            var exception = Assert.Throws<IOException>(sh);
+            Assert.StartsWith("No response from SensorHub", exception.Message);
+        }
+
+        [Fact]
+        public void Should_Read_Correct_Offboard_Sensor_Temperature()
+        {
+            SensorHub sh = new(_mockI2cDevice.Object);
+            _mockI2cDevice.SetupSequence(x => x.ReadByte())
+                .Returns((byte)SensorHub.RegisterStatusErrors.NO_ERROR)
+                .Returns(128);
+
+            sh.TryReadOffBoardTemperature(out var t);
+            double expected = 128.0;
+            Assert.Equal(t.DegreesCelsius, expected);
+        }
+
+        [Fact]
+        public void Should_Read_Correct_Barometer_Temperature()
+        {
+            SensorHub sh = new(_mockI2cDevice.Object);
+            _mockI2cDevice.SetupSequence(x => x.ReadByte())
+                .Returns((byte)SensorHub.RegisterStatusErrors.NO_ERROR)
+                .Returns(50);
+
+            sh.TryReadBarometerTemperature(out var t);
+            double expected = 50.0;
+            Assert.Equal(t.DegreesCelsius, expected);
+        }
+
+        [Fact]
+        public void Should_Read_Correct_Barometer_Pressure()
+        {
+            SensorHub sh = new(_mockI2cDevice.Object);
+            _mockI2cDevice.SetupSequence(x => x.ReadByte())
+                .Returns((byte)SensorHub.RegisterStatusErrors.NO_ERROR)
+                .Returns(0) // H
+                .Returns(19) // M
+                .Returns(136); // L
+
+            sh.TryReadBarometerPressure(out var p);
+            double expected = 5000.0;
+            Assert.Equal(p.Pascals, expected);
+        }
+
+        [Fact]
+        public void Should_Read_Correct_Illuminance()
+        {
+            SensorHub sh = new(_mockI2cDevice.Object);
+            _mockI2cDevice.SetupSequence(x => x.ReadByte())
+                .Returns((byte)SensorHub.RegisterStatusErrors.NO_ERROR)
+                .Returns(3) // H
+                .Returns(12); // L
+
+            sh.TryReadIlluminance(out var l);
+            double expected = 780.0;
+            Assert.Equal(l.Lux, expected);
+        }
+
+        [Fact]
+        public void Should_Read_Correct_Humidity()
+        {
+            SensorHub sh = new(_mockI2cDevice.Object);
+            _mockI2cDevice.SetupSequence(x => x.ReadByte())
+                .Returns((byte)SensorHub.RegisterStatusErrors.NO_ERROR)
+                .Returns(50);
+
+            sh.TryReadRelativeHumidity(out var h);
+            double expected = 50.0;
+            Assert.Equal(h.Percent, expected);
+        }
+
+        [Fact]
+        public void Should_Read_Correct_Onboard_Sensor_Temperature()
+        {
+            SensorHub sh = new(_mockI2cDevice.Object);
+            _mockI2cDevice.SetupSequence(x => x.ReadByte())
+                .Returns((byte)SensorHub.RegisterStatusErrors.NO_ERROR)
+                .Returns(50);
+
+            sh.TryReadOnBoardTemperature(out var t);
+            double expected = 50.0;
+            Assert.Equal(t.DegreesCelsius, expected);
+        }
+
+        [Fact]
+        public void Should_MotionDetect()
+        {
+            SensorHub sh = new(_mockI2cDevice.Object);
+            _mockI2cDevice.Setup(x => x.ReadByte()).Returns(1);
+            Assert.True(sh.IsMotionDetected);
+        }
+
+        [Fact]
+        public void Should_Not_MotionDetect()
+        {
+            SensorHub sh = new(_mockI2cDevice.Object);
+            _mockI2cDevice.Setup(x => x.ReadByte()).Returns(0);
+            Assert.False(sh.IsMotionDetected);
+        }
+    }
+}
