61614c31525b014fceed8c0daf738781c1753813
Laurent Ellerbach <laurelle@microsoft.com>
2021-07-01 20:53:42 +0300

    Adding missing headers

--- src/devices/Card/Ultralight/UltralightCard.cs
+++ src/devices/Card/Ultralight/UltralightCard.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
 using Iot.Device.Common;
 using Iot.Device.Ndef;
 using Microsoft.Extensions.Logging;
