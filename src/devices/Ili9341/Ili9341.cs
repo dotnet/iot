f125bad1b95a4a1c3b4b2ed4a91c6b8b759f9362
Morten Nielsen <mn@iter.dk>
2021-08-18 22:56:12 -0700

    Updated Ili9341 to latest and cleaned up code
    Follows SSD1351 implementation

--- src/devices/Ili9341/Ili9341.cs
+++ src/devices/Ili9341/Ili9341.cs
@@ -1,238 +1,255 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 using System;
-using System.Device;
 using System.Device.Gpio;
-using System.Device.I2c;
-using System.Device.I2c.Drivers;
 using System.Device.Spi;
-using System.Device.Spi.Drivers;
 using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Iot.Device.Ili9341
 {
-    public class Ili9341 : IDisposable
+    public partial class Ili9341 : IDisposable
     {
-        public const int Width = 240;
-        public const int Height = 320;
-        private SpiDevice _spi;
-        private IGpioController _controller;
-        private int _dc;
-        private int _reset;
+        private const int ScreenWidthPx = 240;
+        private const int ScreenHeightPx = 320;
+        private const int DefaultSPIBufferSize = 0x1000;
+        private readonly int _dcPinId;
+        private readonly int _resetPinId;
+        private readonly int _backlitPin;
+        private readonly int _spiBufferSize;
+        private readonly bool _shouldDispose;
+        
+        private SpiDevice _spiDevice;
+        private GpioController _gpioDevice;
 
-        public Ili9341(SpiDevice spiDevice, int dc, int reset, IGpioController gpioController = null)
+        const byte lcdPortraitConfig = 8 | 0x40;
+        const byte lcdLandscapeConfig = 44;
+
+        public Ili9341(SpiDevice spiDevice, int dataCommandPin, int resetPin, int backlightPin = -1, int spiBufferSize = DefaultSPIBufferSize, GpioController gpioController = null, bool shouldDispose = true)
         {
-            _spi = spiDevice;
-            _dc = dc;
-            _reset = reset;
-            _controller = gpioController ?? new GpioController();
-            _controller.OpenPin(_dc, PinMode.Output);
-            _controller.OpenPin(_reset, PinMode.Output);
-
-            HardwareReset();
-
-            // init
-            // SendCommand((Command)0xEF);
-            // SendData(0x03);
-            // SendData(0x80);
-            // SendData(0x02);
-            // SendCommand((Command)0xCF);
-            // SendData(0x00);
-            // SendData(0XC1);
-            // SendData(0X30);
-            // SendCommand((Command)0xED);
-            // SendData(0x64);
-            // SendData(0x03);
-            // SendData(0X12);
-            // SendData(0X81);
-            // SendCommand((Command)0xE8);
-            // SendData(0x85);
-            // SendData(0x00);
-            // SendData(0x78);
-            // SendCommand((Command)0xCB);
-            // SendData(0x39);
-            // SendData(0x2C);
-            // SendData(0x00);
-            // SendData(0x34);
-            // SendData(0x02);
-            // SendCommand((Command)0xF7);
-            // SendData(0x20);
-            // SendCommand((Command)0xEA);
-            // SendData(0x00);
-            // SendData(0x00);
-            SendCommand(Command.PowerControl1);
-            SendData(0x23);
-            SendCommand(Command.PowerControl2);
-            SendData(0x10);
-            SendCommand(Command.VcomControl1);
-            SendData(0x3e);
-            SendData(0x28);
-            SendCommand(Command.VcomControl2);
-            SendData(0x86);
-            SendCommand(Command.MemoryAccessControl);
-            SendData(0x48);
-            SendCommand(Command.ColModPixelFormatSet);
-            SendData(0x55);
-            SendCommand(Command.FrameRateControlInNormalMode);
-            SendData(0x00);
-            SendData(0x18);
-            SendCommand(Command.DisplayFunctionControl);
-            SendData(0x08);
-            SendData(0x82);
-            SendData(0x27);
-            // SendCommand((Command)0xF2);
-            // SendData(0x00);
-            SendCommand(Command.GammaSet);
-            SendData(0x01);
-            SendCommand(Command.PositiveGammaCorrection);
-            SendData(0x0F);
-            SendData(0x31);
-            SendData(0x2B);
-            SendData(0x0C);
-            SendData(0x0E);
-            SendData(0x08);
-            SendData(0x4E);
-            SendData(0xF1);
-            SendData(0x37);
-            SendData(0x07);
-            SendData(0x10);
-            SendData(0x03);
-            SendData(0x0E);
-            SendData(0x09);
-            SendData(0x00);
-            SendCommand(Command.NegativeGammaCorrection);
-            SendData(0x00);
-            SendData(0x0E);
-            SendData(0x14);
-            SendData(0x03);
-            SendData(0x11);
-            SendData(0x07);
-            SendData(0x31);
-            SendData(0xC1);
-            SendData(0x48);
-            SendData(0x08);
-            SendData(0x0F);
-            SendData(0x0C);
-            SendData(0x31);
-            SendData(0x36);
-            SendData(0x0F);
-            SendCommand(Command.SleepOut);
-            DelayHelper.DelayMilliseconds(120, allowThreadYield: true);
-            SendCommand(Command.DisplayOn);
-        }
-
-        public void Draw(Bitmap bitmap)
-        {
-            SetWindow();
-            byte[] buffer = new byte[Width * Height * 2];
-
-            // byte r = (byte)(color.R >> 3);
-            // byte g = (byte)(color.G >> 2);
-            // byte b = (byte)(color.B >> 3);
-            // ushort col = (ushort)((r << 11) | (g << 5) | b);
-
-            for (int i = 0; i < buffer.Length; i++)
+            if (!InRange((uint)spiBufferSize, 0x1000, 0x10000))
             {
-                int p = i / 2;
-                int x = p % Width;
-                int y = p / Width;
-                if (i % 2 == 0)
-                {
-                    float fx = (x - Width / 2);
-                    float fy = (y - Height / 2);
-                    float r = (float)Math.Sqrt(fx * fx + fy * fy);
-                    //float a = (float)Math.Atan2(fy, fx);
-
-                    if (r < 50)
-                    {
-                        buffer[i] = 0x78;
-                    }
-                }
-                // if (i % 4 < 2)
-                // {
-                    // buffer[i] = 0x70;
-                // }
+                throw new ArgumentException(nameof(spiBufferSize), "Value must be between 4096 and 65536.");
             }
 
-            Span<byte> sp = new Span<byte>(buffer);
-            SendData(sp);
-        }
+            _spiDevice = spiDevice;
+            _dcPinId = dataCommandPin;
+            _resetPinId = resetPin;
+            _backlitPin = backlightPin;
+            _gpioDevice = gpioController ?? new GpioController();
+            _shouldDispose = shouldDispose || gpioController is null;
 
-        private void HardwareReset()
-        {
-            _controller.Write(_reset, PinValue.High);
-            DelayHelper.DelayMilliseconds(5, allowThreadYield: true);
-            _controller.Write(_reset, PinValue.Low);
-            DelayHelper.DelayMilliseconds(20, allowThreadYield: true);
-            _controller.Write(_reset, PinValue.High);
-            DelayHelper.DelayMilliseconds(150, allowThreadYield: true);
-        }
+            _gpioDevice.OpenPin(_dcPinId, PinMode.Output);
+            _gpioDevice.OpenPin(_resetPinId, PinMode.Output);
 
-        private void SetWindow(uint x0 = 0, uint y0 = 0, uint x1 = Width - 1, uint y1 = Height - 1)
-        {
-            SendCommand(Command.ColumnAddressSet);
-            Span<byte> data = stackalloc byte[4]
+            _spiBufferSize = spiBufferSize;
+
+            if (_backlitPin >= 0)
             {
-                (byte)(x0 >> 8),
-                (byte)x0,
-                (byte)(x1 >> 8),
-                (byte)x1,
+                _gpioDevice.OpenPin(_backlitPin, PinMode.Output);
+                TurnBacklightOn();
+            }
+            ResetDisplayAsync().Wait();
+
+            SendCommand(Ili9341Command.SoftwareReset);
+            SendCommand(Ili9341Command.DisplayOff);
+            Thread.Sleep(10);
+            SendCommand(Ili9341Command.MemoryAccessControl, lcdPortraitConfig);
+            SendCommand(Ili9341Command.ColModPixelFormatSet, 0x55);//16-bits per pixel
+            SendCommand(Ili9341Command.FrameRateControlInNormalMode, 0x00, 0x1B);
+            SendCommand(Ili9341Command.GammaSet, 0x01);
+            SendCommand(Ili9341Command.ColumnAddressSet, 0x00, 0x00, 0x00, 0xEF); //width of the screen
+            SendCommand(Ili9341Command.PageAddressSet, 0x00, 0x00, 0x01, 0x3F); //height of the screen
+            SendCommand(Ili9341Command.EntryModeSet, 0x07);
+            SendCommand(Ili9341Command.DisplayFunctionControl, 0x0A, 0x82, 0x27, 0x00);
+            SendCommand(Ili9341Command.SleepOut);
+            Thread.Sleep(120);
+            SendCommand(Ili9341Command.DisplayOn);
+            Thread.Sleep(100);
+            SendCommand(Ili9341Command.MemoryWrite);
+        }
+
+        // <summary>
+        /// Convert a color structure to a byte tuple representing the colour in 565 format.
+        /// </summary>
+        /// <param name="color">The color to be converted.</param>
+        /// <returns>
+        /// This method returns the low byte and the high byte of the 16bit value representing RGB565 or BGR565 value
+        ///
+        /// byte    11111111 00000000
+        /// bit     76543210 76543210
+        ///
+        /// For ColorSequence.RGB
+        ///         RRRRRGGG GGGBBBBB
+        ///         43210543 21043210
+        ///
+        /// For ColorSequence.BGR
+        ///         BBBBBGGG GGGRRRRR
+        ///         43210543 21043210
+        /// </returns>
+        private (byte Low, byte High) Color565(Color color)
+        {
+            // get the top 5 MSB of the blue or red value
+            UInt16 retval = (UInt16)(color.B >> 3);
+            // shift right to make room for the green Value
+            retval <<= 6;
+            // combine with the 6 MSB if the green value
+            retval |= (UInt16)(color.G >> 2);
+            // shift right to make room for the red or blue Value
+            retval <<= 5;
+            // combine with the 6 MSB if the red or blue value
+            retval |= (UInt16)(color.R >> 3);
+            return ((byte)(retval >> 8), (byte)(retval & 0xFF));
+        }
+
+        /// <summary>
+        /// Send filled rectangle to the ILI9341 display.
+        /// </summary>
+        /// <param name="color">The color to fill the rectangle with.</param>
+        /// <param name="x">The x co-ordinate of the point to start the rectangle at in pixels.</param>
+        /// <param name="y">The y co-ordinate of the point to start the rectangle at in pixels.</param>
+        /// <param name="w">The width of the rectangle in pixels.</param>
+        /// <param name="h">The height of the rectangle in pixels.</param>
+        public void FillRect(Color color, uint x, uint y, uint w, uint h)
+        {
+            Span<byte> colourBytes = stackalloc byte[2]; // create a short span that holds the colour data to be sent to the display
+            Span<byte> displayBytes = stackalloc byte[(int)(w * h * 2)]; // span used to form the data to be written out to the SPI interface
+
+            // set the colourbyte array to represent the fill colour
+            (colourBytes[0], colourBytes[1]) = Color565(color);
+
+            // set the pixels in the array representing the raw data to be sent to the display
+            // to the fill color
+            for (int i = 0; i < w * h; i++)
+            {
+                displayBytes[i * 2 + 0] = colourBytes[0];
+                displayBytes[i * 2 + 1] = colourBytes[1];
+            }
+
+            // specify a location for the rows and columns on the display where the data is to be written
+            SetWindow(x, y, w, h);
+
+            // write out the pixel data
+            //SendCommand(Ssd1351Command.WriteRam, displayBytes);
+            SendData(displayBytes);
+        }
+
+        /// <summary>
+        /// Clears screen
+        /// </summary>
+        public void ClearScreen()
+        {
+            FillRect(Color.Black, 0, 0, 320, 240);
+        }
+
+        /// <summary>
+        /// Resets the display.
+        /// </summary>
+        public async Task ResetDisplayAsync()
+        {
+            _gpioDevice.Write(_resetPinId, PinValue.High);
+            await Task.Delay(20).ConfigureAwait(false);
+            _gpioDevice.Write(_resetPinId, PinValue.Low);
+            await Task.Delay(20).ConfigureAwait(false);
+            _gpioDevice.Write(_resetPinId, PinValue.High);
+            await Task.Delay(20).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Verifies value is within a specific range.
+        /// </summary>
+        /// <param name="value">Value to check.</param>
+        /// <param name="start">Starting value of range.</param>
+        /// <param name="end">Ending value of range.</param>
+        /// <returns>Determines if value is within range.</returns>
+        internal static bool InRange(uint value, uint start, uint end)
+        {
+            return value >= start && value <= end;
+        }
+
+        /// <summary>
+        /// Send a command to the the display controller along with associated parameters.
+        /// </summary>
+        /// <param name="command">Command to send.</param>
+        /// <param name="commandParameters">parameteters for the command to be sent</param>
+        private void SendCommand(Ili9341Command command, params byte[] commandParameters)
+        {
+            Span<byte> paramSpan = stackalloc byte[commandParameters.Length];
+            for (int i = 0; i < commandParameters.Length; paramSpan[i] = commandParameters[i], i++)
+            {
+            }
+
+            SendCommand(command, paramSpan);
+        }
+
+        /// <summary>
+        /// Send a command to the the display controller along with parameters.
+        /// </summary>
+        /// <param name="command">Command to send.</param>
+        /// <param name="data">Span to send as parameters for the command.</param>
+        private void SendCommand(Ili9341Command command, Span<byte> data)
+        {
+            Span<byte> commandSpan = stackalloc byte[]
+            {
+                (byte)command
             };
-            SendData(data);
-            SendCommand(Command.PageAddressSet);
-            Span<byte> data2 = stackalloc byte[4]
+
+            SendSPI(commandSpan, true);
+
+            if (data != null && data.Length > 0)
             {
-                (byte)(y0 >> 8),
-                (byte)y0,
-                (byte)(y1 >> 8),
-                (byte)y1,
-            };
-            SendData(data2);
-            SendCommand(Command.MemoryWrite);
-        }
-
-        private void SendCommand(Command command)
-        {
-            Span<byte> buff = stackalloc byte[1] { (byte)command };
-            Send(buff, isData: false);
-        }
-
-        private void SendData(ReadOnlySpan<byte> buffer)
-        {
-            Send(buffer, isData: true);
-        }
-
-        private void SendData(byte data)
-        {
-            Span<byte> buff = stackalloc byte[1] { data };
-            SendData(buff);
-        }
-
-        private void Send(ReadOnlySpan<byte> buffer, bool isData)
-        {
-            _controller.Write(_dc, isData);
-
-            int bufLen = 4096 >> 1;
-            while (buffer.Length > bufLen)
-            {
-                _spi.Write(buffer.Slice(0, bufLen));
-                buffer = buffer.Slice(bufLen);
-            }
-
-            if (buffer.Length > 0)
-            {
-                _spi.Write(buffer);
+                SendSPI(data);
             }
         }
 
+        /// <summary>
+        /// Send data to the display controller.
+        /// </summary>
+        /// <param name="data">The data to send to the display controller.</param>
+        private void SendData(Span<byte> buffer)
+        {
+            SendSPI(buffer, blnIsCommand: false);
+        }
+
+        /// <summary>
+        /// Write a block of data to the SPI device
+        /// </summary>
+        /// <param name="data">The data to be sent to the SPI device</param>
+        /// <param name="blnIsCommand">A flag indicating that the data is really a command when true or data when false.</param>
+        private void SendSPI(Span<byte> data, bool blnIsCommand = false)
+        {
+            int index = 0;
+            int len;
+
+            // set the DC pin to indicate if the data being sent to the display is DATA or COMMAND bytes.
+            _gpioDevice.Write(_dcPinId, blnIsCommand ? PinValue.Low : PinValue.High);
+
+            // write the array of bytes to the display. (in chunks of SPI Buffer Size)
+            do
+            {
+                // calculate the amount of spi data to send in this chunk
+                len = Math.Min(data.Length - index, _spiBufferSize);
+                // send the slice of data off set by the index and of length len.
+                _spiDevice.Write(data.Slice(index, len));
+                // add the length just sent to the index
+                index += len;
+            }
+            while (index < data.Length); // repeat until all data sent.
+        }
+
+        /// <inheritdoc/>
         public void Dispose()
         {
-            _spi?.Dispose();
-            _spi = null;
-            //_controller?.Dispose();
-            //_controller = null;
+            if (_shouldDispose)
+            {
+                _gpioDevice?.Dispose();
+                _gpioDevice = null!;
+            }
+            
+            _spiDevice?.Dispose();
+            _spiDevice = null;
         }
     }
 }
