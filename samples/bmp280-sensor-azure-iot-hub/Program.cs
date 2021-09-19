72e09fcc0b108cca8ac3a1541f71bf1cad062ee4
Ibrahim KIVANC <ikivanc@hotmail.com>
2021-09-19 23:36:18 +0300

    resolve pr comments

--- samples/bmp280-sensor-azure-iot-hub/Program.cs
+++ samples/bmp280-sensor-azure-iot-hub/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
 using System.Device.I2c;
 using System.Threading;
 using Iot.Device.Bmxx80;
@@ -13,24 +16,27 @@ namespace DNSensorAzureIoTHub
 {
     public class Program
     {
-
         // set up IoT Hub message
         const string DeviceID = "<replace-with-your-device-id>";
         const string IotBrokerAddress = "<replace-with-your-iot-hub-name>.azure-devices.net";
+
+        // LED constraints 
+        const int pin = 18;
+        const int lightTime = 1000;
+        const int dimTime = 2000;
+
+        // busId number for I2C pins
+        const int busId = 1;
 
         public static void Main()
         {
             Console.WriteLine($".Net IoT with BMP280 Sensor!");
 
             // set up for LED and pin
-            const int pin = 18;
-            const int lightTime = 1000;
-            const int dimTime = 2000;
             using GpioController led = new();
             led.OpenPin(pin, PinMode.Output);
     
             // setup for BMP280
-            const int busId = 1;
             I2cConnectionSettings i2cSettings = new(busId, Bmp280.DefaultI2cAddress);
             I2cDevice i2cDevice = I2cDevice.Create(i2cSettings);
             using var i2CBmp280 = new Bmp280(i2cDevice);
@@ -47,9 +53,10 @@ namespace DNSensorAzureIoTHub
             else
             {
                 Console.WriteLine("Successfully created DeviceClient!");
+                Console.WriteLine("Press any key to stop the application.");
             }
 
-            while (true)
+            while (!Console.KeyAvailable)
             {
                 try
                 {
