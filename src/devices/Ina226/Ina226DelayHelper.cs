ac06502ce202eb27e3ae947393d1eaf377cbad75
mizady <90141999+mizady@users.noreply.github.com>
2022-01-11 11:20:20 -0500

    Attempt to Fix Ina226DelayHelper conflict

--- src/devices/Ina226/Ina226DelayHelper.cs
+++ src/devices/Ina226/Ina226DelayHelper.cs
@@ -1,10 +1,11 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Diagnostics;
 using System.Threading;
 
-namespace System.Device
+namespace Iot.Device.Ina226
 {
     /// <summary>
     /// Helpers for short waits.
