83779f3be24c63b6e6f8702e24a21b8e6ee58b79
Patrick Grawehr <pgrawehr@hotmail.com>
2022-01-29 21:23:08 +0100

    A few more steps

--- eng/ArduinoCsCI.cmd
+++ eng/ArduinoCsCI.cmd
@@ -11,18 +11,26 @@ arduino-cli config add board_manager.additional_urls https://dl.espressif.com/dl
 arduino-cli core update-index
 
 set ArduinoRootDir=%1\Documents\Arduino
+set acspath=%~dp0\..\tools\ArduinoCsCompiler\Frontend\bin\%2\net6.0\acs.exe
 dir %ArduinoRootDir%
 
-git clone https://github.com/firmata/ConfigurableFirmata %ArduinoRootDir%\libraries
-git checkout https://github.com/pgrawehr/ExtendedConfigurableFirmata %ArduinoRootDir%
+git clone https://github.com/firmata/ConfigurableFirmata %ArduinoRootDir%\libraries\ConfigurableFirmata
+git clone https://github.com/pgrawehr/ExtendedConfigurableFirmata %ArduinoRootDir%\ExtendedConfigurableFirmata
 arduino-cli core install esp32:esp32
 
 dir %ArduinoRootDir%
 
-pushd
-cd %ArduinoRootDir%\ExtendedConfigurableFirmata
+%acspath% --help
+
+rem Write runtime data to ExtendedConfigurableFirmata directory, before building
+%acspath% prepare
+
+pushd %ArduinoRootDir%\ExtendedConfigurableFirmata
+
+dir
 arduino-cli compile --fqbn esp32:esp32:esp32:PSRAM=disabled,PartitionScheme=default,CPUFreq=240,FlashMode=qio,FlashFreq=80,FlashSize=4M,UploadSpeed=921600,DebugLevel=none ./ExtendedConfigurableFirmata.ino --warnings more
 popd
+
 
 if errorlevel 1 goto error
 
@@ -30,5 +38,5 @@ Echo All done!
 
 :usage
 :error
-echo Usage: ArduinoCsCI.cmd [path-to-home-directory]
+echo Usage: ArduinoCsCI.cmd [path-to-home-directory] [Configuration]
 
