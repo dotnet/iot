043ff8b8607f16a35fc01c95350448772a2109f8
Patrick Grawehr <pgrawehr@hotmail.com>
2022-01-31 18:18:01 +0100

    Now gluing everything together

--- eng/ArduinoCsCI.cmd
+++ eng/ArduinoCsCI.cmd
@@ -2,7 +2,7 @@ echo on
 
 if %1!==! goto :usage
 
-choco install -y arduino-cli
+choco install -y --no-progress arduino-cli
 arduino-cli lib install "DHT sensor library"
 arduino-cli lib install "Servo"
 
@@ -28,11 +28,26 @@ rem Write runtime data to ExtendedConfigurableFirmata directory, before building
 pushd %ArduinoRootDir%\ExtendedConfigurableFirmata
 
 dir
+REM First build the code for the ESP32 (this just verifies that the code builds, it does no run-time checks at all)
 arduino-cli compile --fqbn esp32:esp32:esp32:PSRAM=disabled,PartitionScheme=default,CPUFreq=240,FlashMode=qio,FlashFreq=80,FlashSize=4M,UploadSpeed=921600,DebugLevel=none ./ExtendedConfigurableFirmata.ino --warnings default
-popd
-
-
 if errorlevel 1 goto error
+
+REM then build the simulator code as Visual Studio C++ project
+msbuild /p:Configuration=%1 ExtendedConfigurableFirmataSim\ExtendedConfigurableFirmataSim.vcxproj
+if errorlevel 1 goto error
+
+REM Start the simulator asynchronously
+start ExtendedConfigurableFirmataSim\%1\ExtendedConfigurableFirmataSim.exe
+
+popd
+pushd %~dp0\..\tools\ArduinoCsCompiler\
+
+REM and finally run the Arduino tests, now including the ones skipped previously
+dotnet test -c %1 --no-build --no-restore --filter feature=firmata -v:n -maxcpucount:1
+if errorlevel 1 goto error
+
+popd
+taskkill /im ExtendedConfigurableFirmataSim.exe /f
 
 Echo All done!
 
