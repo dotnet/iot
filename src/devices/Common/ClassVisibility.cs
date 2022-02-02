1a6a766e8c6fde74a460eacc97e6605f7f541c45
Patrick Grawehr <pgrawehr@hotmail.com>
2022-02-02 21:06:11 +0100

    Also add the other case, to prevent accidental change to public

--- src/devices/Common/ClassVisibility.cs
+++ src/devices/Common/ClassVisibility.cs
@@ -7,11 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#pragma warning disable SA1403 // File may only contain a single namespace
+
 // These instructions make the classes public for building the individual projects.
 // The default visibility is "internal" so they stay hidden if this section is removed for the final library build.
 #if !BUILDING_IOT_DEVICE_BINDINGS
-
-#pragma warning disable SA1403 // File may only contain a single namespace
 
 public partial class Interop
 {
@@ -38,6 +38,37 @@ namespace System.Device.Gpio
     }
 
     public partial struct PinVector64
+    {
+    }
+}
+
+#else
+
+internal partial class Interop
+{
+}
+
+namespace Iot.Device.Common
+{
+    internal partial class NumberHelper
+    {
+    }
+}
+
+namespace System.Device
+{
+    internal partial class DelayHelper
+    {
+    }
+}
+
+namespace System.Device.Gpio
+{
+    internal partial struct PinVector32
+    {
+    }
+
+    internal partial struct PinVector64
     {
     }
 }
