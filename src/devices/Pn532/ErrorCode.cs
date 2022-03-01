76c12ad803ebb4cf076e5ea1452f15f1f99821ca
HumphreyJ <tlos142857@live.cn>
2022-03-01 10:00:46 +0800

    Fix namespace

--- src/devices/Pn532/ErrorCode.cs
+++ src/devices/Pn532/ErrorCode.cs
@@ -1,7 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace IoT.Device.Pn532
+namespace Iot.Device.Pn532
 {
     /// <summary>
     /// All errors that can be returned by the PN532
