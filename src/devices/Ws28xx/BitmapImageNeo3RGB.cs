cf3887b5605a96ab7fe78517f6773789ba930385
Oliver Japes <rdlprmpf@gmx.de>
2022-03-07 15:09:39 +0100

    Added support for WS2815B (#1800)
    * Introduced color mode, added support for WS2815B
    
    * Revamped the sample
    
    * Changed sample
    
    * Fixed namespace
    
    * Updated Readme
    
    * Removed YouTube-Link
    
    * Added copyright headers
    
    * Removed color mode, added derivation of BitmapImageNeo3 for RGB-format
    
    * Added new commented line that shows that the initialization of Ws2815b is identical to the other strips
    
    * Broken up the menu output for better readability
    
    * Moved properties (back) to protected const fields

---
+++ src/devices/Ws28xx/BitmapImageNeo3RGB.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Drawing;
+
+namespace Iot.Device.Ws28xx
+{
+    /// <summary>
+    /// Special 24bit RGB format for Neo pixel LEDs where each bit is converted to 3 bits.
+    /// A one is converted to 110, a zero is converted to 100.
+    /// </summary>
+    /// <seealso cref="Iot.Device.Ws28xx.BitmapImageNeo3" />
+    internal class BitmapImageNeo3Rgb : BitmapImageNeo3
+    {
+        public BitmapImageNeo3Rgb(int width, int height)
+            : base(width, height)
+        {
+        }
+
+        public override void SetPixel(int x, int y, Color c)
+        {
+            var offset = y * Stride + x * BytesPerPixel;
+            Data[offset++] = _lookup[c.R * BytesPerComponent + 0];
+            Data[offset++] = _lookup[c.R * BytesPerComponent + 1];
+            Data[offset++] = _lookup[c.R * BytesPerComponent + 2];
+            Data[offset++] = _lookup[c.G * BytesPerComponent + 0];
+            Data[offset++] = _lookup[c.G * BytesPerComponent + 1];
+            Data[offset++] = _lookup[c.G * BytesPerComponent + 2];
+            Data[offset++] = _lookup[c.B * BytesPerComponent + 0];
+            Data[offset++] = _lookup[c.B * BytesPerComponent + 1];
+            Data[offset++] = _lookup[c.B * BytesPerComponent + 2];
+        }
+    }
+}
