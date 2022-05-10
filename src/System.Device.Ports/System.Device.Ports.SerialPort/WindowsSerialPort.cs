74e6db5f613314e6c2fd4dfe1e9715426c374c7c
Raf <raffaeler@vevy.com>
2022-05-10 21:20:34 +0200

    Added the CsWin32 code generator to generate the interop declarations for Windows

--- src/System.Device.Ports/System.Device.Ports.SerialPort/WindowsSerialPort.cs
+++ src/System.Device.Ports/System.Device.Ports.SerialPort/WindowsSerialPort.cs
@@ -1,6 +1,10 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Windows.Win32;
+using Windows.Win32.Devices;
+using Windows.Win32.Devices.Communication;
+
 namespace System.Device.Ports.SerialPort
 {
     internal class WindowsSerialPort : SerialPort
