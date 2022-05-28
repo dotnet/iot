c5295ae406721ee1b2184f780f17683ba95a9e1e
Florian Gschwandtner <github@gschwand.eu>
2022-05-28 09:21:45 +0200

    Use interrupt to detect position and push switch changes

--- src/devices/Seesaw/samples/Seesaw.Sample.Encoder.cs
+++ src/devices/Seesaw/samples/Seesaw.Sample.Encoder.cs
@@ -2,23 +2,47 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Device.Gpio;
 using System.Device.I2c;
 using Iot.Device.Seesaw;
 
-using Seesaw seesawDevice = new Seesaw(I2cDevice.Create(new I2cConnectionSettings(1, 0x36)));
+const byte AdafruitSeesawRotaryEncoderI2cAddress = 0x36;
+const byte AdafruitSeesawRotaryEncoderI2cBus = 0x1;
+const byte AdafruitSeesawRotaryEncoderPushSwitchPin = 24;
 
+const int EncoderPositionInitialValue = 100;
+const int HostInterruptPin = 6;
+
+using GpioController gpioController = new(PinNumberingScheme.Logical);
+using Seesaw seesawDevice = new(I2cDevice.Create(new I2cConnectionSettings(AdafruitSeesawRotaryEncoderI2cBus, AdafruitSeesawRotaryEncoderI2cAddress)));
+
+// set initial encoder position value
+seesawDevice.SetEncoderPosition(EncoderPositionInitialValue);
+
+// enable interrupt for position changes on Seesaw encoder
 seesawDevice.EnableEncoderInterrupt();
 
-seesawDevice.SetEncoderPosition(100);
+// enable interrupt for rotary encoder push switch
+uint encoderPushSwitchPinMask = 1U << (AdafruitSeesawRotaryEncoderPushSwitchPin);
+seesawDevice.SetGpioPinMode(AdafruitSeesawRotaryEncoderPushSwitchPin, PinMode.InputPullUp);
+seesawDevice.SetGpioInterrupts(encoderPushSwitchPinMask, true);
 
-int? lastPosition = null;
-while (true)
+// enable host interrupt and register callback
+gpioController.OpenPin(HostInterruptPin, PinMode.InputPullUp);
+gpioController.RegisterCallbackForPinValueChangedEvent(HostInterruptPin, PinEventTypes.Falling, (s, e) =>
 {
-    var encoderPosition = seesawDevice.GetEncoderPosition();
-
-    if (lastPosition == null || encoderPosition != lastPosition)
+    uint interruptFlags = seesawDevice.ReadGpioInterruptFlags();
+    if ((interruptFlags & encoderPushSwitchPinMask) > 0)
     {
-        lastPosition = encoderPosition;
-        Console.WriteLine($"Position: {encoderPosition}");
+        // interrupt for push switch pin -> push switch status changed
+        bool pushSwitchReleased = seesawDevice.ReadGpioDigital(AdafruitSeesawRotaryEncoderPushSwitchPin);
+        Console.WriteLine($"Encoder switch {(pushSwitchReleased ? "released" : "pressed")}");
+        return;
     }
-}
+
+    int encoderPosition = seesawDevice.GetEncoderPosition();
+    Console.WriteLine($"Encoder position changed: {encoderPosition}");
+});
+
+// press a key to exit application
+Console.ReadLine();
