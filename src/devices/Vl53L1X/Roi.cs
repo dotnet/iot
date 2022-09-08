733bb850202dea19e594a8c781d9a85bf2cebdaf
manthey.michael <manthey.michael@googlemail.com>
2022-09-08 14:34:42 +0200

    Implementation of VL53L1X sensor including sample and documentation

---
+++ src/devices/Vl53L1X/Roi.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Vl53L1X
+{
+    /// <summary>
+    /// Holds the ROI information of the device
+    /// </summary>
+    public class Roi
+    {
+        /// <summary>
+        /// Width of the ROI
+        /// </summary>
+        public ushort Width { get; }
+
+        /// <summary>
+        /// Height of the ROI
+        /// </summary>
+        public ushort Height { get; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="width">Width of the ROI</param>
+        /// <param name="height">Height of the ROI</param>
+        public Roi(ushort width, ushort height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
---
+++ src/devices/Vl53L1X/samples/Program.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+using System;
+using System.Threading;
+using Iot.Device.Vl53L1X;
+
+const int xShutPinNo = 23;
+const int newI2CAddress = 0x30;
+
+Console.WriteLine("Hello VL53L1X!");
+
+using Vl53L1X vl53L1X = new(i2cAddress: newI2CAddress, xShutPin: xShutPinNo);
+
+Console.WriteLine($"SensorID: {vl53L1X.GetSensorId():X}");
+Console.WriteLine($"Offset in µm: {vl53L1X.GetOffset()}, Signal rate: {vl53L1X.GetSignalRate()}");
+Console.WriteLine($"Distance Mode: {vl53L1X.GetDistanceMode()}");
+Console.WriteLine($"TimingBudget: {vl53L1X.GetTimingBudgetInMs()}");
+vl53L1X.SetDistanceMode(DistanceMode.Short);
+Console.WriteLine($"Distance Mode: {vl53L1X.GetDistanceMode()}");
+Console.WriteLine($"TimingBudget: {vl53L1X.GetTimingBudgetInMs()}");
+Console.WriteLine($"SpadNb: {vl53L1X.GetSpadNb()}");
+Console.WriteLine($"InterMeasurementInMs: {vl53L1X.GetInterMeasurementInMs()}");
+
+while (!Console.KeyAvailable)
+{
+   try
+   {
+      var dist = vl53L1X.Distance;
+      var rangeStatus = vl53L1X.GetRangeStatus();
+      Console.WriteLine($"RangeStatus {rangeStatus}");
+      Console.WriteLine($"Distance: {dist}");
+   }
+   catch (Exception ex)
+   {
+      Console.WriteLine($"Exception: {ex.Message}");
+   }
+
+   Thread.Sleep(500);
+}
