9467d62dcf1f0cd3631a57a762c895aa514e25ca
Rich Lander <rich@lander.ca>
2022-09-13 22:42:31 -0700

    Add IS31FL3731 LED matrix binding

---
+++ src/devices/Is31Fl3731/Is31Fl3731.cs
@@ -0,0 +1,224 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.I2c;
+using System.Threading;
+
+namespace Iot.Device.Display
+{
+    /// <summary>
+    /// Represents an IS31FL3731 LED Matrix driver
+    /// </summary>
+    // Datasheet: https://cdn-learn.adafruit.com/assets/assets/000/030/994/original/31FL3731.pdf
+    // Product: https://www.adafruit.com/product/2946
+    // Product: https://www.adafruit.com/product/2974
+    public class Is31Fl3731
+    {
+        // Register addresses
+        // Command register
+        // Points pages one to nine
+        // table 2 in datasheet
+        private const byte COMMAND_REGISTER = 0xFD;
+        private const byte FUNCTION_REGISTER = 0x0B;
+
+        // Response register
+        // Writes data to pages one to eight
+        // table 3 in datasheet
+        private const byte LED_REGISTER = 0x0;
+        private const int LED_REGISTER_LENGTH = BLINK_REGISTER - LED_REGISTER;
+        private const byte BLINK_REGISTER = 0x12;
+        private const byte BLINK_REGISTER_LENGTH = PWM_REGISTER - BLINK_REGISTER;
+        private const byte PWM_REGISTER = 0x24;
+        private const byte PWM_REGISTER_END = 0xB3;
+        private const byte PWM_REGISTER_LENGTH = (PWM_REGISTER_END - PWM_REGISTER) + 1;
+
+        // Function register
+        // Writes data to page nine
+        // table 3 in datasheet
+        private const byte CONFIGURATION_REGISTER = 0x0B;
+        private const byte SHUTDOWN = 0x0A;
+
+        // Values
+        private readonly byte[] _disable_all_leds_data = new byte[LED_REGISTER_LENGTH];
+        private readonly byte[] _enable_all_leds_data = new byte[LED_REGISTER_LENGTH];
+
+        private I2cDevice _i2cDevice;
+
+        /// <summary>
+        /// Initialize IS31FL3731 device
+        /// </summary>
+        /// <param name="i2cDevice">The <see cref="System.Device.I2c.I2cDevice"/> to create with.</param>
+        public Is31Fl3731(I2cDevice i2cDevice)
+        {
+            _i2cDevice = i2cDevice;
+            _enable_all_leds_data.AsSpan().Fill(0xff);
+        }
+
+        /// <summary>
+        /// Initialize IS31FL3731 device
+        /// </summary>
+        /// <param name="i2cDevice">The <see cref="System.Device.I2c.I2cDevice"/> to create with.</param>
+        /// <param name="width">The width of the LED matrix.</param>
+        /// <param name="height">The height of the LED matrix.</param>
+        public Is31Fl3731(I2cDevice i2cDevice, int width, int height)
+        : this(i2cDevice)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        /// <summary>
+        /// IS31FL3731 default I2C address
+        /// </summary>
+        public static readonly int DefaultI2cAddress = 0x74;
+
+        /// <summary>
+        /// Width of LED matrix (x axis).
+        /// </summary>
+        public readonly int Width = 16;
+
+        /// <summary>
+        /// Height of LED matrix (y axis).
+        /// </summary>
+        public readonly int Height = 9;
+
+        /// <summary>
+        /// Indexer for updating matrix, with PWM register.
+        /// </summary>
+        public byte this[int x, int y]
+        {
+            get => ReadLedPwm(x, y);
+            set => WriteLedPwm(x, y, value);
+        }
+
+        /// <summary>
+        /// Set value for LED.
+        /// </summary>
+        public void WritePixel(int x, int y, byte brightness, bool enable, bool blink)
+        {
+            WriteLed(x, y, enable);
+            WriteLedPwm(x, y, brightness);
+        }
+
+        /// <summary>
+        /// Fill all LEDs.
+        /// </summary>
+        public void Fill(byte brightness = 128, byte page = 0)
+        {
+            EnableAllLeds(page);
+            byte[] data = new byte[PWM_REGISTER_LENGTH];
+            data.AsSpan().Fill(brightness);
+            Write(page, PWM_REGISTER, data);
+        }
+
+        /// <summary>
+        /// Enable all LEDs.
+        /// </summary>
+        public void EnableAllLeds(byte page = 0)
+        {
+            Write(page, LED_REGISTER, _enable_all_leds_data);
+        }
+
+        /// <summary>
+        /// Disable all LEDs.
+        /// </summary>
+        public void DisableAllLeds(byte page = 0)
+        {
+            Write(page, LED_REGISTER, _disable_all_leds_data);
+        }
+
+        /// <summary>
+        /// Initialize LED driver.
+        /// </summary>
+        public void Initialize()
+        {
+            // Reset device
+            Reset();
+
+            // set display mode (bits d4:d3)
+            // 00 = picture mode
+            // 01 = auto frame
+            // 1x audio frame play mode
+            Write(FUNCTION_REGISTER, CONFIGURATION_REGISTER, 0);
+            // set data page
+            _i2cDevice.Write(new byte[] { COMMAND_REGISTER, 0 });
+        }
+
+        /// <summary>
+        /// Reset device.
+        /// </summary>
+        public void Reset()
+        {
+            // Reset device
+            Shutdown(true);
+            Thread.Sleep(10);
+            Shutdown(false);
+        }
+
+        /// <summary>
+        /// Set the shutdown mode.
+        /// </summary>
+        /// <param name="shutdown">Set the showdown mode. `true` sets device into shutdown mode. `false` sets device into normal operation.</param>
+        public void Shutdown(bool shutdown = true)
+        {
+            // mode values
+            // 0 = shutdown mode
+            // 1 = normal operation
+            int mode = shutdown ? 0 : 1;
+            Write(FUNCTION_REGISTER, SHUTDOWN, (byte)mode);
+        }
+
+        private void Write(byte register, byte address, byte[] value)
+        {
+            _i2cDevice.Write(new byte[] { COMMAND_REGISTER, register });
+            byte[] data = new byte[value.Length + 1];
+            data[0] = address;
+            value.CopyTo(data.AsSpan(1));
+            _i2cDevice.Write(data);
+        }
+
+        private byte Read(int address)
+        {
+            byte[] buffer = new byte[1];
+            _i2cDevice.Write(new byte[] { (byte)address });
+            return _i2cDevice.ReadByte();
+        }
+
+        private void Write(byte register, byte address, byte value)
+        {
+            _i2cDevice.Write(new byte[] { COMMAND_REGISTER, register });
+            _i2cDevice.Write(new byte[] { address, value });
+        }
+
+        private int GetLedAddress(int x, int y) => x + y * Width;
+
+        private byte ReadLedPwm(int x, int y) => Read(GetLedAddress(x, y) + PWM_REGISTER);
+
+        private void WriteLedPwm(int x, int y, byte brightness)
+        {
+            int address = GetLedAddress(x, y) + PWM_REGISTER;
+            Write(0, (byte)address, brightness);
+        }
+
+        private void WriteLed(int x, int y, bool enable)
+        {
+            int longAddress = GetLedAddress(x, y);
+            int address = longAddress / 8;
+            int ledBlock = Read(address);
+            int ledRegisterBit = longAddress % 8;
+            int mask = 1 >> ledRegisterBit;
+
+            if (enable)
+            {
+                ledBlock |= mask;
+            }
+            else
+            {
+                ledBlock &= ~mask;
+            }
+
+            Write(0, (byte)address, (byte)ledBlock);
+        }
+    }
+}
---
+++ src/devices/Is31Fl3731/Program.cs
@@ -0,0 +1,186 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Device.I2c;
+using System.Linq;
+using System.Threading;
+using Iot.Device.Display;
+
+Is31Fl3731 matrix = new(I2cDevice.Create(new I2cConnectionSettings(busId: 1, Is31Fl3731.DefaultI2cAddress)));
+
+matrix.Initialize();
+matrix.Fill(0);
+
+for (int i = 0; i < 2; i++)
+{
+    byte brightness = (byte)((i + 1) * (2 + i));
+    FillSlow(brightness);
+}
+
+for (int i = 0; i < 16; i++)
+{
+matrix.WritePixel(i, 0, 0, true, false);
+Thread.Sleep(50);
+// matrix.WritePixel(i, 0, (byte)i, true, false);
+// Thread.Sleep(50);
+}
+
+void FillSlow(byte brightness)
+{
+    for (int h = 0; h < matrix.Height; h++)
+    {
+        for (int w = 0; w < matrix.Width; w++)
+        {
+            matrix[w, h] = brightness;
+            Thread.Sleep(2);
+        }
+    }
+}
+
+// Dimensions
+int width = matrix.Width - 1;
+int halfWidth = matrix.Width / 2;
+
+// Clear matrix
+matrix.Fill(0);
+
+// Set pixel in the origin 0, 0 position.
+matrix[0, 0] = 1;
+// Set pixels in the middle
+matrix[halfWidth - 1, 3] = 1;
+matrix[halfWidth - 1, 4] = 1;
+matrix[halfWidth, 3] = 1;
+matrix[halfWidth, 4] = 1;
+// Set pixel on the opposite edge
+matrix[width - 1, 6] = 1;
+matrix[width - 1, 7] = 1;
+matrix[width, 6] = 1;
+matrix[width, 7] = 1;
+
+Thread.Sleep(500);
+matrix.Fill(0);
+
+// Draw line in first row
+for (int i = 0; i < matrix.Width; i++)
+{
+    if (i % 2 is 1)
+    {
+        continue;
+    }
+
+    matrix[i, 0] = 1;
+    Thread.Sleep(50);
+}
+
+// Draw line in last row
+for (int i = 0; i < matrix.Width; i++)
+{
+    if (i % 2 is 0)
+    {
+        continue;
+    }
+
+    matrix[i, 9] = 1;
+    Thread.Sleep(50);
+}
+
+Thread.Sleep(500);
+matrix.Fill(0);
+
+// Draw diagonal lines
+for (int i = 0; i < 8; i++)
+{
+    matrix[i,  i] = 1;
+    matrix[8 - i, i] = 1;
+    // uncomment if matrix.Width == 16
+    // matrix[i + 8, i] = 1;
+    // matrix[15 - i, i] = 1;
+    Thread.Sleep(50);
+}
+
+for (int i = 0; i < 8; i++)
+{
+    matrix[i,  i] = 0;
+    matrix[8 - i, i] = 0;
+    // uncomment if matrix.Width == 16
+    // matrix[i + 8, i] = 0;
+    // matrix[15 - i, i] = 0;
+    Thread.Sleep(50);
+}
+
+// Draw a bounding box
+for (int i = 0; i < matrix.Width; i++)
+{
+    matrix[i, 0] = 1;
+    Thread.Sleep(10);
+}
+
+for (int i = 0; i < 8; i++)
+{
+    matrix[width, i] = 1;
+    Thread.Sleep(10);
+}
+
+for (int i = matrix.Width; i >= 0; i--)
+{
+    matrix[i, 7] = 1;
+    Thread.Sleep(10);
+}
+
+for (int i = 7; i >= 0; i--)
+{
+    matrix[0, i] = 1;
+    Thread.Sleep(50);
+}
+
+Thread.Sleep(500);
+matrix.Fill(0);
+
+void WriteRowPixels(int row, IEnumerable<int> pixels, int value)
+{
+    foreach (int pixel in pixels)
+    {
+        matrix[pixel, row] = (byte)value;
+        Thread.Sleep(15);
+    }
+}
+
+void WriteColumnPixels(int column, IEnumerable<int> pixels, int value)
+{
+    foreach (int pixel in pixels)
+    {
+        matrix[column, pixel] = (byte)value;
+        Thread.Sleep(15);
+    }
+}
+
+// Draw a spiral bounding box
+for (int j = 0; j < 4; j++)
+{
+    int rangeW = matrix.Width - j * 2;
+    int rangeH = 8 - j * 2;
+    // top
+    WriteRowPixels(j, Enumerable.Range(j, rangeW), 1);
+
+    // right
+    WriteColumnPixels(width - j, Enumerable.Range(j + 1, rangeH - 2), 1);
+
+    // bottom
+    WriteRowPixels(7 - j, Enumerable.Range(j, rangeW).Reverse(), 1);
+
+    // left
+    WriteColumnPixels(j, Enumerable.Range(j + 1, rangeH - 2).Reverse(), 1);
+}
+
+Thread.Sleep(500);
+matrix.Fill(0);
+
+matrix[0, 0] = 1;
+matrix[0, 7] = 1;
+matrix[width, 0] = 1;
+matrix[width, 7] = 1;
+
+Thread.Sleep(500);
+matrix.Fill(0);
