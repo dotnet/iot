524220dac42ff08cb447f307c34f2efe161e4cab
Rich Lander <rich@lander.ca>
2022-09-16 22:08:42 -0700

    Add Phat 17x7 binding

---
+++ src/devices/Is31Fl3731/ScrollPhatHD17x7.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.I2c;
+using System.Threading;
+
+namespace Iot.Device.Display
+{
+    /// <summary>
+    /// Represents 16x8 matrix, driven by an IS31FL3731 LED chip.
+    /// </summary>
+    // Product: https://shop.pimoroni.com/products/scroll-phat-hd
+    // Product: https://shop.pimoroni.com/products/scroll-hat-mini
+    public class ScrollPhat17x7 : Is31Fl3731
+    {
+         /// <summary>
+        /// Initialize LED driver.
+        /// </summary>
+        /// <param name="i2cDevice">The <see cref="System.Device.I2c.I2cDevice"/> to create with.</param>
+        public ScrollPhat17x7(I2cDevice i2cDevice)
+        : base(i2cDevice, 17, 7)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override int GetLedAddress(int x, int y)
+        {
+            if (x <= 8)
+            {
+                x = 8 - x;
+                y = 6 - y;
+            }
+            else
+            {
+                x = x - 8;
+                y = y - 8;
+            }
+
+            return x * 16 + y;
+        }
+    }
+}
