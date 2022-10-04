7dc26413920ce295d89af46a24b9641282c5dee8
sichelz1 <50455065+sichelz1@users.noreply.github.com>
2022-10-04 10:59:23 +0200

    Implementation of VL53L1X sensor including sample and documentation (#1923)
    * Implementation of VL53L1X sensor including sample and documentation
    
    * Added missing links to VL53l1X device
    
    * Removed links from md files
    Removed gpiocontroller from vl53l1x class
    Passing I2cdevice to constructor
    Switched from functions to properties
    
    * Now using UnitsNet for all distances.
    Small code cosmetics
    
    * Added additional comments and range checks

---
+++ src/devices/Vl53L1X/TimingBudget.cs
@@ -0,0 +1,51 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Vl53L1X
+{
+    /// <summary>
+    /// The timing budget for the device
+    /// </summary>
+    public enum TimingBudget : ushort
+    {
+        /// <summary>
+        /// Unknown budget or the budget is not configured
+        /// </summary>
+        BudgetUnknown = 0,
+
+        /// <summary>
+        /// Budget of 15 ms
+        /// </summary>
+        Budget15 = 15,
+
+        /// <summary>
+        /// Budget of 20 ms
+        /// </summary>
+        Budget20 = 20,
+
+        /// <summary>
+        /// Budget of 33 ms
+        /// </summary>
+        Budget33 = 33,
+
+        /// <summary>
+        /// Budget of 50 ms
+        /// </summary>
+        Budget50 = 50,
+
+        /// <summary>
+        /// Budget of 100 ms
+        /// </summary>
+        Budget100 = 100,
+
+        /// <summary>
+        /// Budget of 200 ms
+        /// </summary>
+        Budget200 = 200,
+
+        /// <summary>
+        /// Budget of 500 ms
+        /// </summary>
+        Budget500 = 500,
+    }
+}
