56b83726461cd5da858166756b6d242bad4eb60f
Jose Perez Rodriguez <joperezr@microsoft.com>
2022-10-04 09:21:27 -0700

    Fix build issue when trying to run pack (#1942)

--- Build.cmd
+++ Build.cmd
@@ -1,3 +1,9 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build %*"
+setlocal
+
+for %%x in (%*) do (
+    if "%%x"=="-pack" set _packArg=/p:BuildPackages=true
+)
+
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build %* %_packArg%"
 exit /b %ErrorLevel%
