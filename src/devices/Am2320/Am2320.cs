0b050cba3f4fd27450f32ff6bd7fdbfe7b7ec121
Laurent Ellerbach <laurelle@microsoft.com>
2022-12-24 14:31:33 +0100

    implementation of AM2320

---
+++ src/devices/Am2320/Am2320.cs
@@ -0,0 +1,214 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Buffers.Binary;
+using System.Device.I2c;
+using System.Device.Model;
+using System.Threading;
+using UnitsNet;
+
+namespace Iot.Device.Am2320
+{
+    /// <summary>
+    /// AM2320 - Temperature and Humidity sensor.
+    /// </summary>
+    [Interface("AM2320 - Temperature and Humidity sensor")]
+    public class Am2320 : IDisposable
+    {
+        private readonly I2cDevice _i2c;
+        private readonly byte[] _readBuff = new byte[8];
+
+        /// <summary>
+        /// AM3220 default I2C address.
+        /// </summary>
+        public const int DefaultI2cAddress = 0x5C;
+
+        /// <summary>
+        /// The minimum read period is 1.5 seconds. Do not read the sensor more often.
+        /// </summary>
+        public static readonly TimeSpan MinimumReadPeriod = new TimeSpan(0, 0, 0, 1, 500);
+
+        private DateTime _lastMeasurement = DateTime.UtcNow.Subtract(MinimumReadPeriod);
+
+        /// <summary>
+        /// Gets a value indicating whether last read went, <c>true</c> for success, <c>false</c> for failure.
+        /// </summary>
+        public bool IsLastReadSuccessful { get; internal set; }
+
+        /// <summary>
+        /// Gets the last read temperature.
+        /// </summary>
+        /// <remarks>
+        /// If last read was not successful, it returns <code>default(Temperature).</code>
+        /// </remarks>
+        [Telemetry]
+        public Temperature Temperature
+        {
+            get
+            {
+                if (IsOutDated())
+                {
+                    ReadData();
+                }
+
+                return IsLastReadSuccessful ? GetTemperature() : default(Temperature);
+            }
+        }
+
+        /// <summary>
+        /// Gets the last read of relative humidity in percentage.
+        /// </summary>
+        /// <remarks>
+        /// If last read was not successful, it returns <code>default(RelativeHumidity).</code>
+        /// </remarks>
+        [Telemetry]
+        public RelativeHumidity Humidity
+        {
+            get
+            {
+                if (IsOutDated())
+                {
+                    ReadData();
+                }
+
+                return IsLastReadSuccessful ? GetHumidity() : default(RelativeHumidity);
+            }
+        }
+
+        /// <summary>
+        /// Gets the device information.
+        /// </summary>
+        [Property]
+        public DeviceInformation DeviceInformation
+        {
+            get
+            {
+                Span<byte> buff = stackalloc byte[11];
+
+                // 32 bit device ID
+                // Forces the sensor to wake up and wait 10 ms according to documentation
+                _i2c.WriteByte(0x00);
+                Thread.Sleep(10);
+
+                // Sending functin code 0x03, start regster 0x00 and 4 registers
+                _i2c.Write(stackalloc byte[] { 0x03, 0x08, 0x07 });
+
+                // Wait at least 30 micro seconds
+                Thread.Sleep(1);
+                _i2c.Read(buff);
+
+                // Check if it is valid
+                if (!IsValidReadBuffer(buff, 0x07))
+                {
+                    return null!;
+                }
+
+                if (!IsCrcValid(buff))
+                {
+                    return null!;
+                }
+
+                return new DeviceInformation()
+                {
+                    Model = BinaryPrimitives.ReadUInt16BigEndian(buff.Slice(2)),
+                    Version = buff[4],
+                    DeviceId = BinaryPrimitives.ReadUInt32BigEndian(buff.Slice(5)),
+                };
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Am2320" /> class.
+        /// </summary>
+        /// <param name="i2c">The <see cref="I2cDevice"/>.</param>
+        /// <remarks>This sensor only works on Standard Mode speed.</remarks>
+        public Am2320(I2cDevice i2c)
+        {
+            _i2c = i2c ?? throw new ArgumentNullException(nameof(i2c));
+        }
+
+        private bool IsOutDated() => !(_lastMeasurement.Add(MinimumReadPeriod) > DateTime.UtcNow);
+
+        private void ReadData()
+        {
+            // Forces the sensor to wake up and wait 10 ms according to documentation
+            _i2c.WriteByte(0x00);
+            Thread.Sleep(10);
+
+            // Sending functin code 0x03, start regster 0x00 and 4 registers
+            _i2c.Write(stackalloc byte[] { 0x03, 0x00, 0x04 });
+
+            // Wait at least 30 micro seconds
+            Thread.Sleep(1);
+            _i2c.Read(_readBuff);
+            _lastMeasurement = DateTime.UtcNow;
+
+            // Check if sucessfull read
+            if (!IsValidReadBuffer(_readBuff, 0x04))
+            {
+                IsLastReadSuccessful = false;
+                return;
+            }
+
+            if (!IsCrcValid(_readBuff))
+            {
+                IsLastReadSuccessful = false;
+                return;
+            }
+
+            IsLastReadSuccessful = true;
+        }
+
+        private bool IsValidReadBuffer(Span<byte> buff, byte expected) => (buff[0] == 0x03) && (buff[1] == expected);
+
+        private bool IsCrcValid(Span<byte> buff)
+        {
+            var crc = Crc16(buff.Slice(0, buff.Length - 2));
+            return (crc >> 8 == buff[buff.Length - 1]) && ((crc & 0xFF) == buff[buff.Length - 2]);
+        }
+
+        private Temperature GetTemperature()
+        {
+            short temp = BinaryPrimitives.ReadInt16BigEndian((new Span<byte>(_readBuff)).Slice(4));
+            return Temperature.FromDegreesCelsius(temp / 10.0);
+        }
+
+        private RelativeHumidity GetHumidity()
+        {
+            short hum = BinaryPrimitives.ReadInt16BigEndian((new Span<byte>(_readBuff)).Slice(2));
+            return RelativeHumidity.FromPercent(hum / 10.0);
+        }
+
+        private ushort Crc16(Span<byte> ptr)
+        {
+            ushort crc = 0xFFFF;
+            byte i;
+            byte inc = 0;
+            while (inc < ptr.Length)
+            {
+                crc ^= ptr[inc++];
+                for (i = 0; i < 8; i++)
+                {
+                    if ((crc & 0x01) == 0x01)
+                    {
+                        crc >>= 1;
+                        crc ^= 0xA001;
+                    }
+                    else
+                    {
+                        crc >>= 1;
+                    }
+                }
+            }
+
+            return crc;
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            _i2c?.Dispose();
+        }
+    }
+}
---
+++ src/devices/Am2320/samples/Program.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Device.I2c;
+using System.Diagnostics;
+using System.Threading;
+using Iot.Device.Am2320;
+
+Debug.WriteLine("Hello from AM2320!");
+
+// Important: make sure the bus speed is in standar mode and not in fast more.
+using Am2320 am2330 = new(I2cDevice.Create(new I2cConnectionSettings(1, Am2320.DefaultI2cAddress)));
+
+// On some copies, the device information contains only 0
+var deviceInfo = am2330.DeviceInformation;
+if (deviceInfo != null)
+{
+    Debug.WriteLine($"Model: {deviceInfo.Model}");
+    Debug.WriteLine($"Version: {deviceInfo.Version}");
+    Debug.WriteLine($"Device ID: {deviceInfo.DeviceId}");
+}
+
+while (true)
+{
+    var temp = am2330.Temperature;
+    var hum = am2330.Humidity;
+    if (am2330.IsLastReadSuccessful)
+    {
+        Debug.WriteLine($"Temp = {temp.DegreesCelsius} C, Hum = {hum.Percent} %");
+    }
+    else
+    {
+        Debug.WriteLine("Not sucessful reading.");
+    }
+
+    Thread.Sleep(Am2320.MinimumReadPeriod);
+}
