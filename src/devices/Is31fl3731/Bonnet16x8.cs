2580229760fd983b7ab6f0280ee2746cd4edca9c
Rich Lander <rlander@microsoft.com>
2023-01-16 12:40:54 -0800

    Add LED matrix bindings for IS31FL3731 driver (#1926)
    * Add IS31FL3731 LED matrix binding
    
    * Update sample
    
    * Make program more generic
    
    * Add specific bindings
    
    * Add Phat 17x7 binding
    
    * Add Phat 17x7 binding
    
    * Add LED Shim 28x1 driver
    
    * Update switch expression
    
    * Update TPN
    
    * Add README
    
    * Add Led Matrix Breakout 11x7 binding
    
    * Update README for led matrix breakout
    
    * Add Led RGB Matrix 5x5 binding
    
    * Add 5x5 RGB Matrix sample
    
    * Switch integers to decimals
    
    * Fix syntax
    
    * Rename type
    
    * Update per feedback
    
    * Update per feedback
    
    * Switch to enums for const data
    
    * Switch from enum to static class
    
    * Switch to ROS
    
    * Fix flipped field
    
    * Update brightness
    
    * Update TFMs
    
    * Adding GpioPin (#1895)
    
    * Adding GpioPin
    
    * adjusting based on PR feedback
    
    * Adjusting to have natively drivers.
    
    * make methods virtual
    
    * adding Toggle to System.Device drivers
    
    * Board and Mcp23xxx adjustments
    
    * adding FT4222, FT232H and Pcx857x
    
    * adding Arduino and Seasaw
    
    * adjsting file encoding after conflict merge
    
    * Adding missing Toggle implementation in Arduino native
    
    * Adjusting test for new pattern with GpioPin
    
    * adjusting PR feedback
    
    * typo in test
    
    * fixing nit
    
    * fixing test
    
    * fixing test
    
    * Bump Microsoft.CodeAnalysis.CSharp.Scripting in /tools/DevicesApiTester (#1981)
    
    Bumps [Microsoft.CodeAnalysis.CSharp.Scripting](https://github.com/dotnet/roslyn) from 4.3.1 to 4.4.0.
    - [Release notes](https://github.com/dotnet/roslyn/releases)
    - [Changelog](https://github.com/dotnet/roslyn/blob/main/docs/Breaking%20API%20Changes.md)
    - [Commits](https://github.com/dotnet/roslyn/commits/Visual-Studio-2019-Version-16.0-Preview-4.4)
    
    ---
    updated-dependencies:
    - dependency-name: Microsoft.CodeAnalysis.CSharp.Scripting
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump Microsoft.Net.Compilers.Toolset in /tools/DevicesApiTester (#1980)
    
    Bumps [Microsoft.Net.Compilers.Toolset](https://github.com/dotnet/roslyn) from 4.3.1 to 4.4.0.
    - [Release notes](https://github.com/dotnet/roslyn/releases)
    - [Changelog](https://github.com/dotnet/roslyn/blob/main/docs/Breaking%20API%20Changes.md)
    - [Commits](https://github.com/dotnet/roslyn/commits/Visual-Studio-2019-Version-16.0-Preview-4.4)
    
    ---
    updated-dependencies:
    - dependency-name: Microsoft.Net.Compilers.Toolset
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Upgrade to .NET 7 GA (#1988)
    
    * Upgrade to .NET 7 GA
    
    * Update global.json
    
    Co-authored-by: Jose Perez Rodriguez <joperezr@microsoft.com>
    
    * Update blinking
    
    * Add 'Port of' comments
    
    * Update sample
    
    * Update sample
    
    * Update comment
    
    * Update per feedback
    
    * Add 'Port of' comment
    
    * Move samples to directories
    
    * Update README
    
    * Change exception type
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Laurent Ellerbach <laurelle@microsoft.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Jose Perez Rodriguez <joperezr@microsoft.com>

---
+++ src/devices/Is31fl3731/Bonnet16x8.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.I2c;
+using System.Threading;
+
+namespace Iot.Device.Display
+{
+    /// <summary>
+    /// Represents 16x8 matrix, driven by an IS31FL3731 LED chip.
+    /// </summary>
+    // Product: https://www.adafruit.com/product/4122
+    // Port of: https://github.com/adafruit/Adafruit_CircuitPython_IS31FL3731/blob/main/adafruit_is31fl3731/charlie_bonnet.py
+    public class Bonnet16x8 : Is31fl3731
+    {
+        /// <summary>
+        /// Initialize LED driver.
+        /// </summary>
+        /// <param name="i2cDevice">The <see cref="System.Device.I2c.I2cDevice"/> to create with.</param>
+        public Bonnet16x8(I2cDevice i2cDevice)
+        : base(i2cDevice, 16, 8)
+        {
+        }
+
+        /// <summary>
+        /// Default I2C address for device.
+        /// </summary>
+        public static new readonly int DefaultI2cAddress = 0x74;
+
+        /// <inheritdoc/>
+        public override int GetLedAddress(int x, int y) => x switch
+        {
+            < 8 => (x + 1) * 16 + (7 - y),
+            < 16 => (x - 6) * 16 - (y + 1),
+            _ => throw new ArgumentException($"{nameof(x)} value must be < 16.")
+        };
+    }
+}
