2c3e1a2da02813a21779eaa0803410b326caf628
Patrick Grawehr <pgrawehr@hotmail.com>
2023-02-10 21:13:54 +0100

    Include attribution for tests

--- src/devices/Nmea0183/tests/Ais/ParserTests.cs
+++ src/devices/Nmea0183/tests/Ais/ParserTests.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+// Original code taken from https://github.com/yellowfeather/AisParser, under MIT License
 using Iot.Device.Nmea0183.Ais;
 using Shouldly;
 using Xunit;
