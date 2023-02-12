ad7bc86f8068cdd2b2e2fce8ec798ce285d947e4
Scott W Harden <swharden@gmail.com>
2023-02-12 12:06:35 -0500

    mark Ft232HI2c obsolete
    #2032

---
+++ src/devices/Ft232H/Ft232HI2c.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Iot.Device.Ft232H
+{
+    /// <summary>
+    /// This class has been replaced by <see cref="Ft232HI2cDevice"/>
+    /// </summary>
+    [Obsolete("This class has been replaced by Ft232HI2cDevice", true)]
+    public class Ft232HI2c
+    {
+    }
+}
