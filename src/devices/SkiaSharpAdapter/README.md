e0a6a7d24ae2e1c2e3f1c6d75f683e1ff62d1170
Patrick Grawehr <pgrawehr@hotmail.com>
2023-02-19 15:48:05 +0100

    Create new SkiaSharpAdapter project
    This is an adapter that uses SkiaSharp as Bitmap handling backend

---
+++ src/devices/SkiaSharpAdapter/README.md
@@ -0,0 +1,12 @@
+﻿# SkiaSharp graphics library adapter
+
+## Summary
+
+This folder contains the SkiaSharp adapter for Iot.Device.Bindings. It provides an implementation for BitmapImage that uses the
+[SkiaSharp](https://github.com/mono/SkiaSharp) multi-platform graphics library. This folder is compiled into its own .nuget file, so that no external dependencies
+are included when they're not needed.
+
+See [Ili934x](../Ili934x/Readme.md) for an usage example.
+
+To use SkiaSharp as image library with Iot.Device.Bindings, reference (in addition to `Iot.Device.Bindings.nuget`) the `Iot.Device.Bindings.SkiaSharpAdapter.nuget`
+and put a line with `SkiaSharpAdapter.Register()` somewhere at the beginning of your application.
---
+++ src/devices/SkiaSharpAdapter/SkiaSharpAdapter.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.Gpio;
+using System.Device.I2c;
+using System.Device.Spi;
+
+namespace Iot.Device.Graphics.SkiaSharpAdapter
+{
+    /// <summary>
+    /// Image factory registry helper.
+    /// </summary>
+    public static class SkiaSharpAdapter
+    {
+        /// <summary>
+        /// Registers this factory as the default image factory.
+        /// Call this method once at startup to use SkiaSharp as image backend.
+        /// </summary>
+        public static void Register()
+        {
+            BitmapImage.RegisterImageFactory(new SkiaSharpImageFactory());
+        }
+    }
+}
