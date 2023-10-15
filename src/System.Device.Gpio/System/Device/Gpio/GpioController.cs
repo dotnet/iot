6d8906209989f1d3bbb3fc7c45e944aebc8f17e9
HumJ <tlos142857@live.cn>
2023-10-15 23:15:36 +0800

    batch write and read

--- src/System.Device.Gpio/System/Device/Gpio/GpioController.cs
+++ src/System.Device.Gpio/System/Device/Gpio/GpioController.cs
@@ -427,10 +427,7 @@ public class GpioController : IDisposable
     /// <param name="pinValuePairs">The pin/value pairs to write.</param>
     public void Write(ReadOnlySpan<PinValuePair> pinValuePairs)
     {
-        for (int i = 0; i < pinValuePairs.Length; i++)
-        {
-            Write(pinValuePairs[i].PinNumber, pinValuePairs[i].PinValue);
-        }
+        _driver.Write(pinValuePairs);
     }
 
     /// <summary>
@@ -439,11 +436,7 @@ public class GpioController : IDisposable
     /// <param name="pinValuePairs">The pin/value pairs to read.</param>
     public void Read(Span<PinValuePair> pinValuePairs)
     {
-        for (int i = 0; i < pinValuePairs.Length; i++)
-        {
-            int pin = pinValuePairs[i].PinNumber;
-            pinValuePairs[i] = new PinValuePair(pin, Read(pin));
-        }
+        _driver.Read(pinValuePairs);
     }
 
     /// <summary>
