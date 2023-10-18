59861028f356c48563945c6cf36eeb9986a505e0
Patrick Grawehr <pgrawehr@hotmail.com>
2023-10-18 21:10:36 +0200

    Allow creation of duplicate device addresses for the Tca9548A. (#2146)
    * Allow creation of duplicate device addresses for the Tca9548A.
    
    After all, that's what it's for.
    
    * Add some testing.
    
    We absolutely need some default simulation implementation for the
    main hardware layes (I2C, SPI, GPIO), otherwise testing becomes
    a nightmare, because the mocks have always to be created manually.

--- src/System.Device.Gpio/System/Device/I2c/I2cConnectionSettings.cs
+++ src/System.Device.Gpio/System/Device/I2c/I2cConnectionSettings.cs
@@ -6,7 +6,7 @@ namespace System.Device.I2c;
 /// <summary>
 /// The connection settings of a device on an I2C bus.
 /// </summary>
-public sealed class I2cConnectionSettings
+public sealed class I2cConnectionSettings : IEquatable<I2cConnectionSettings>
 {
     private I2cConnectionSettings()
     {
@@ -38,4 +38,46 @@ public sealed class I2cConnectionSettings
     /// The bus address of the I2C device.
     /// </summary>
     public int DeviceAddress { get; }
+
+    /// <summary>
+    /// Equality comparator
+    /// </summary>
+    /// <param name="other">The other instance</param>
+    /// <returns>True or false</returns>
+    public bool Equals(I2cConnectionSettings? other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return BusId == other.BusId && DeviceAddress == other.DeviceAddress;
+    }
+
+    /// <summary>
+    /// Basic equality overload
+    /// </summary>
+    /// <param name="obj">The other instance</param>
+    /// <returns>True on value equality, false otherwise</returns>
+    public override bool Equals(object? obj)
+    {
+        return ReferenceEquals(this, obj) || (obj is I2cConnectionSettings other && Equals(other));
+    }
+
+    /// <summary>
+    /// Standard hash function
+    /// </summary>
+    /// <returns>A hash code</returns>
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            return (BusId * 397) ^ DeviceAddress;
+        }
+    }
 }
