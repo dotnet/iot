a5181f52222270a5cb8b333e0a2ceabac479e47f
Raf (Raffaele Rialdi) <raffaeler@vevy.com>
2023-11-09 17:28:29 +0100

    Camera binding (#2150)
    * Holding button test change to measure effective time elapsed
    
    * Raising the timeout for holding tests
    
    * Fix the other holding test and shorten the time of all the other tests
    
    * Making the test itself async
    
    * changed strategy for signalling the holding event
    
    * adding logging to the button tests
    
    * New Camera binding
    
    * Provides support to catpure still images or video streaming using the external utilities that are part of the OS
    
    * Added the command enumeration to retrieve the command line string easily
    
    * Added fluent api to build the command options
    
    * README revised
    
    * Creating the string array for the arguments
    
    * Comand line builder test
    
    * Testing the uniqueness of the options
    
    * Diagnostics
    
    * doc fix
    
    * Created the samples to capture still, video, timelapse either with the legacy or libcamera stack.
    Various fixes
    
    * Minor fixes and cleanup
    
    * Added the last missing commands. Added the configuration for stderr. Tested with all the configurations on both the legacy and libcamera stacks
    
    * cleanup
    
    * Doc update
    
    * doc fixes
    
    * doc fix
    
    * Fixed the tfms in the projects
    
    * 'Moved the projects
    
    * Added System.IO.Pipelines to Iot.Device.Binding
    
    * Adjusted csproj to avoid implicit usings
    
    * undo the button stuff
    
    * remove the button commits from this pr
    
    * Reviewed files according to the feedback
    
    * Referenced the FakeVideocapture from tests to get the exe copied
    
    * Moved the process runner and settings into the Common assembly.
    Made the required review changes on the comments.
    
    * Fixed the piplines version on MjpegStream project
    Fixed the SkiaSharpAdapter in Media.sln
    
    * Added a more verbose comment
    
    * Comments and docs changes based on the review
    
    * markdown fixes
    
    * comment fix as for review

---
+++ src/devices/Camera/Camera/Settings/CommandInputType.cs
@@ -0,0 +1,177 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iot.Device.Camera.Settings;
+
+/// <summary>
+/// Represents the type of value expected on the command line
+/// </summary>
+public enum CommandInputType
+{
+    /// <summary>
+    /// No value is expected
+    /// </summary>
+    Void,
+
+    /// <summary>
+    /// The list of cameras prefixed by the index
+    /// </summary>
+    IndexOfCamera,
+
+    /// <summary>
+    /// A filename
+    /// </summary>
+    Filename,
+
+    /// <summary>
+    /// An integer specifying milliseconds
+    /// </summary>
+    Milliseconds,
+
+    /// <summary>
+    /// An integer specifying microseconds
+    /// </summary>
+    Microseconds,
+
+    /// <summary>
+    /// Four integers specifying x,y,w,h
+    /// </summary>
+    Xywh,
+
+    /// <summary>
+    /// Three integers specifying w,h,q (width, height, quality)
+    /// </summary>
+    Whq,
+
+    /// <summary>
+    /// A single string
+    /// </summary>
+    String,
+
+    /// <summary>
+    /// A single formattable string accepting the following directives.<para/>
+    /// Directive   Substitution<para/>
+    /// %frame      The sequence number of the frame<para/>
+    /// %fps        The instantaneous frame rate<para/>
+    /// %exp        The shutter speed used to capture the image, in microseconds<para/>
+    /// %ag         The analogue gain applied to the image in the sensor<para/>
+    /// %dg         The digital gain applied to the image by the ISP<para/>
+    /// %rg         The gain applied to the red component of each pixel<para/>
+    /// %bg         The gain applied to the blue component of each pixel<para/>
+    /// %focus      The focus metric for the image, where a larger value implies a sharper image<para/>
+    /// %lp         The current lens position in dioptres (1 / distance in metres).<para/>
+    /// %afstate    The autofocus algorithm state (one of idle, scanning, focused or failed).<para/>
+    /// </summary>
+    FormattableString,
+
+    /// <summary>
+    /// An integer
+    /// </summary>
+    Int,
+
+    /// <summary>
+    /// An decimal number (dot is the separator)
+    /// </summary>
+    Decimal,
+
+    /// <summary>
+    /// A pair of decimal numbers separated by a comma
+    /// </summary>
+    DecimalPair,
+
+    /// <summary>
+    /// An decimal number or the 'default' string
+    /// </summary>
+    DecimalOrString,
+
+    /// <summary>
+    /// Sensor Mode string
+    /// </summary>
+    SensorMode,
+
+    /// <summary>
+    /// Rotation 0 or 180
+    /// </summary>
+    Rotate0Or180,
+
+    /// <summary>
+    /// One of the following strings: centre, spot, average, custom
+    /// </summary>
+    Metering,
+
+    /// <summary>
+    /// One of the following strings: normal, sport or long
+    /// </summary>
+    Exposure,
+
+    /// <summary>
+    /// One of the following strings: auto, incandescent, tungsten, fluorescent, indoor, daylight, cloudy, custom
+    /// </summary>
+    WhiteBalance,
+
+    /// <summary>
+    /// One of the following strings: auto, off, cdn_off, cdn_fast, cdn_hq
+    /// </summary>
+    Denoise,
+
+    /// <summary>
+    /// One of the following strings: default, manual, auto, continuous
+    /// </summary>
+    AutofocusMode,
+
+    /// <summary>
+    /// One of the following strings: normal, macro, full
+    /// </summary>
+    AutofocusRange,
+
+    /// <summary>
+    /// One of the following strings: normal, fast
+    /// </summary>
+    AutofocusSpeed,
+
+    /// <summary>
+    /// One of the following strings: '-' (stdout), 'udp://', 'tcp://' or filename (including %d == progressive number)
+    /// </summary>
+    Output,
+
+    /// <summary>
+    /// One of the following strings: jpg, png, bmp, rgb, yuv420
+    /// </summary>
+    Encoding,
+
+    /// <summary>
+    /// One of the following strings: baseline, main or high
+    /// </summary>
+    H264Profile,
+
+    /// <summary>
+    /// One of the following strings: 4, 4.1 or 4.2
+    /// </summary>
+    H264Level,
+
+    /// <summary>
+    /// One of the following strings: h264, mjpeg, yuv420, libav
+    /// </summary>
+    Codec,
+
+    /// <summary>
+    /// One of the following strings: record or pause
+    /// </summary>
+    Initial,
+
+    /// <summary>
+    /// A number expressing megabytes
+    /// </summary>
+    Megabytes,
+
+    /// <summary>
+    /// A listening Uri in the form tcp://0.0.0.0:8123
+    /// </summary>
+    ListeningTcpUri,
+}
---
+++ src/devices/Camera/tests/TestCamera/CameraInfoTests.cs
@@ -0,0 +1,44 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Iot.Device.Camera;
+
+namespace TestCamera;
+
+/// <summary>
+/// Tests for the CameraInfo class.
+/// </summary>
+public class CameraInfoTests
+{
+    private const string Text1 = "test.txt";
+
+    /// <summary>
+    /// Test parsing --list-cameras
+    /// </summary>
+    /// <returns>The test task operation.</returns>
+    [Fact]
+    public async Task TestCameraList()
+    {
+        var source = await File.ReadAllTextAsync(Text1, Encoding.UTF8);
+        var cameras = await CameraInfo.From(source);
+
+        Assert.Equal(2, cameras.Count());
+        var cam0 = cameras.First();
+        Assert.Equal(0, cam0.Index);
+        Assert.Equal("imx219", cam0.Name);
+        Assert.Equal("3280x2464", cam0.MaxResolution);
+        Assert.Equal("/base/soc/i2c0mux/i2c@1/imx219@10", cam0.DevicePath);
+
+        var cam1 = cameras.Skip(1).First();
+        Assert.Equal(1, cam1.Index);
+        Assert.Equal("imx477", cam1.Name);
+        Assert.Equal("4056x3040", cam1.MaxResolution);
+        Assert.Equal("/base/soc/i2c0mux/i2c@1/imx477@1a", cam1.DevicePath);
+    }
+}
