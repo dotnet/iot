4ccabf4752ac21f1733da08ff9cd56f745171adb
famblard <48769175+famblard86@users.noreply.github.com>
2024-06-06 18:05:28 +0200

    Add LPS22HB device (#2309)
    * Add LPS22HB device
    
    * remove emty link in readme
    
    * fix markdownlint
    
    * Enable low-pass filter
    
    2.1 Digital low-pass filter
    The LPS22HB embeds an additional low-pass filter that can be applied on the pressure readout path when the device is in continuous mode.
    https://www.st.com/resource/en/application_note/an4833-measuring-pressure-data-from-sts-lps22hb-digital-pressure-sensor-stmicroelectronics.pdf
    
    * apply review comment
    
    * fix MD009
    
    * Allow more configuration
    
    * Change Exception to IOException
    
    * Use TryReadPressure and TryReadTemperature
    
    * Add missing Header
    
    * Remove old read property & Add and use reset device in constructor
    
    * fix other markdownlint
    
    * Add timeout loop
    
    ---------
    
    Co-authored-by: Frédéric Amblard <famblard@rayonnance.fr>

---
+++ src/devices/Lps22hb/BduMode.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Lps22hb
+{
+    /// <summary>
+    /// The BDU (Block Data Update) bit is located is used to inhibit the
+    /// update of the output registers between the reading of upper, middle and lower register
+    /// parts
+    /// </summary>
+    public enum BduMode
+    {
+        /// <summary>
+        /// BDU disable: the lower, middle and upper register parts are updated continuously
+        /// </summary>
+        ContinuousUpdate = 0b0,
+
+        /// <summary>
+        /// BDU activated the content of the output registers is not updated
+        /// until the PRESS_OUT_H register has been read in order to avoid output data corruption
+        /// </summary>
+        BlockDataUpdate = 0b1
+    }
+}
---
+++ src/devices/Lps22hb/Register.cs
@@ -0,0 +1,93 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Lps22hb
+{
+    internal enum Register : byte
+    {
+        /// <summary>
+        /// Interrupt register
+        /// </summary>
+        INTERRUPT_CFG = 0x0B,
+
+        /// <summary>
+        /// 16-bit Pressure threshold registers
+        /// THS_P_L (0x0C), THS_P_H (0x0D)
+        /// </summary>
+        THS_P_L = 0x0C,
+
+        /// <summary>
+        /// Who am I
+        /// </summary>
+        WHO_AM_I = 0x0F,
+
+        /// <summary>
+        /// Control1
+        /// </summary>
+        CTRL_REG1 = 0x10,
+
+        /// <summary>
+        /// Control2
+        /// </summary>
+        CTRL_REG2 = 0x11,
+
+        /// <summary>
+        /// Control3
+        /// </summary>
+        CTRL_REG3 = 0x12,
+
+        /// <summary>
+        /// FIFO configuration register
+        /// </summary>
+        FIFO_CTRL = 0x14,
+
+        /// <summary>
+        /// Int24 Reference pressure registers
+        /// REF_P_XL (0x15), REF_P_L (0x16), REF_P_H (0x17)
+        /// </summary>
+        REF_P_XL = 0x15,
+
+        /// <summary>
+        /// 16-bit Pressure offset registers
+        /// RDPS_L (0x18), RDPS_H (0x19)
+        /// </summary>
+        RPDS_L = 0x18,
+
+        /// <summary>
+        /// Resolution register
+        /// </summary>
+        RES_CONF = 0x1A,
+
+        /// <summary>
+        /// Interrupt register
+        /// </summary>
+        INT_SOURCE = 0x25,
+
+        /// <summary>
+        /// FIFO status register
+        /// </summary>
+        FIFO_STATUS = 0x26,
+
+        /// <summary>
+        /// Status register
+        /// </summary>
+        STATUS = 0x27,
+
+        /// <summary>
+        /// 24-bit Pressure output registers
+        /// PRESS_OUT_XL (0x28), PRESS_OUT_L (0x29), PRESS_OUT_H (0x2A)
+        /// </summary>
+        PRESS_OUT_XL = 0x28,
+
+        /// <summary>
+        /// 16-bit Temperature output registers
+        /// TEMP_OUT_L (0x2B), TEMP_OUT_H (0x2C)
+        /// </summary>
+        TEMP_OUT_L = 0x2B,
+
+        /// <summary>
+        /// Filter reset register
+        /// </summary>
+        LPFP_RES = 0x33,
+    }
+}
