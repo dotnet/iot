11f4d23bd417108f6c830a909617a4647b6dd192
JimBobBennett <jimbobbennett@mac.com>
2025-01-09 08:50:36 -0800

    Removing trailing whitespace

--- src/devices/Blinkt/Blinkt.cs
+++ src/devices/Blinkt/Blinkt.cs
@@ -34,7 +34,7 @@ namespace Iot.Device.Blinkt
 
         /// <summary>
         /// The sleep time in milliseconds between each bit written to the Blinkt.
-        /// 
+        ///
         /// The default value of 0 should be enough for most cases and works on the RPi 4 and 5,
         /// but you can increase this if you experience issues with the Blinkt on faster CPUs.
         /// </summary>
