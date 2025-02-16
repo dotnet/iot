319c83dea74fdcc4a737d18861f52ec2e0a4ba2a
Patrick Grawehr <pgrawehr@hotmail.com>
2025-02-16 12:05:10 +0100

    Add test for SysFs

--- src/System.Device.Gpio.Tests/SysFsDriverTests.cs
+++ src/System.Device.Gpio.Tests/SysFsDriverTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Device.Gpio.Drivers;
+using System.IO;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -19,4 +20,44 @@ public class SysFsDriverTests : GpioControllerTestBase
     }
 
     protected override GpioDriver GetTestDriver() => new SysFsDriver();
+
+    [Fact]
+    public void CheckAllChipsCanBeConstructed()
+    {
+        var chips = SysFsDriver.GetAvailableChips();
+        Assert.NotEmpty(chips);
+        if (IsRaspi4())
+        {
+            // 2 real ones and the default 0 entry
+            Assert.Equal(3, chips.Count);
+        }
+
+        foreach (var chip in chips)
+        {
+            var driver = new SysFsDriver(chip.Id);
+            var ctrl = new GpioController(driver);
+            Assert.NotNull(ctrl);
+            var driverInfo = driver.GetChipInfo();
+            Assert.NotNull(driverInfo);
+            Assert.Equal(chip.Id, driverInfo.Id);
+            Assert.Equal(chip.Label, driverInfo.Label);
+            Assert.Equal(chip.Name, driverInfo.Name);
+            Assert.Equal(chip.NumLines, driverInfo.NumLines);
+            ctrl.Dispose();
+        }
+    }
+
+    private bool IsRaspi4()
+    {
+        if (File.Exists("/proc/device-tree/model"))
+        {
+            string model = File.ReadAllText("/proc/device-tree/model", Text.Encoding.ASCII);
+            if (model.Contains("Raspberry Pi 4") || model.Contains("Raspberry Pi Compute Module 4"))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
