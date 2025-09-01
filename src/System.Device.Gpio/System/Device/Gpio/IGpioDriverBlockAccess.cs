bde0b37dba94f49ca0126e26ffc26b53b12e57c1
Felix Collins <felix@intranel.com>
2025-09-02 11:29:25 +1200

    Added a new interface to make GpioDriver implementations that support read/write of blocks of gpio pins.

---
+++ src/System.Device.Gpio/System/Device/Gpio/IGpioDriverBlockAccess.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Device.Gpio
+{
+    /// <summary>
+    /// Interface to mark drivers that support block access to GPIO pins.
+    /// Block access in this case meaning the ability to read/write multiple pins in one call.
+    /// </summary>
+    public interface IGpioDriverBlockAccess
+    {
+        /// <summary>
+        /// Write the given pins with the given values.
+        /// </summary>
+        /// <param name="pinValuePairs">The pin/value pairs to write.</param>
+        public void Write(ReadOnlySpan<PinValuePair> pinValuePairs);
+
+        /// <summary>
+        /// Read the given pins with the given pin numbers.
+        /// </summary>
+        /// <param name="pinValuePairs">The pin/value pairs to read.</param>
+        public void Read(Span<PinValuePair> pinValuePairs);
+    }
+}
