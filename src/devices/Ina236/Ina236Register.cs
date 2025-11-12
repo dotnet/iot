7d39c928ab96719d02e4ed9fb841e3fc223a789c
Patrick Grawehr <pgrawehr@hotmail.com>
2025-11-12 20:51:08 +0100

    A first draft

---
+++ src/devices/Ina236/Ina236Register.cs
@@ -0,0 +1,20 @@
+﻿namespace Iot.Device.Adc;
+
+#pragma warning disable CS1591
+
+/// <summary>
+/// The Ina236 Register map. Note that all registers are 16 bit wide.
+/// </summary>
+public enum Ina236Register
+{
+    Configuration = 0,
+    ShuntVoltage = 1,
+    BusVoltage = 2,
+    Power = 3,
+    Current = 4,
+    Calibration = 5,
+    MaskEnable = 6,
+    AlertLimit = 7,
+    ManufacturerId = 0x3E,
+    DeviceId = 0x3F
+}
