db01de247fd891ade0f03227cc6fba46f8eec443
Patrick Grawehr <pgrawehr@hotmail.com>
2026-03-08 10:38:45 +0100

    Include RPI setup in example

--- src/devices/Ina236/samples/Program.cs
+++ src/devices/Ina236/samples/Program.cs
@@ -7,11 +7,17 @@ using System.Device.I2c;
 using Iot.Device;
 using Iot.Device.Adc;
 using Iot.Device.Arduino;
+using Iot.Device.Board;
 using UnitsNet;
 
+// To work with an arduino:
 using ArduinoBoard board = new ArduinoBoard("COM5", 115200);
 using Ina236 device = new(board.CreateI2cDevice(new I2cConnectionSettings(0, 0x40)), ElectricResistance.FromMilliohms(8),
     ElectricCurrent.FromAmperes(10.0));
+
+// Use this on a Raspberry Pi:
+////using Ina236 device2 = new Ina236(I2cDevice.Create(new I2cConnectionSettings(1, 0x40)), ElectricResistance.FromMilliohms(8),
+////    ElectricCurrent.FromAmperes(10.0));
 
 Console.WriteLine("Device initialized. Default settings used:");
 Console.WriteLine($"Operating Mode: {device.OperatingMode}");
