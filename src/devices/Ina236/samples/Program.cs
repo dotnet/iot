3982d38b24cd53bebb05540ff48bdc018a2d913a
Patrick Grawehr <pgrawehr@hotmail.com>
2026-03-08 15:55:54 +0100

    New Binding: Ina236 (#2459)
    * New Binding for Ina236 with many bells and whistles
    
    * Add generic helper classes to simulate I2C devices
    
    Generic implementation is difficult, one problem is
    the different argument sizes and the fact that the
    endianess on the bus may change.

---
+++ src/devices/Ina236/samples/Program.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Threading;
+using System.Device.I2c;
+using Iot.Device;
+using Iot.Device.Adc;
+using Iot.Device.Arduino;
+using Iot.Device.Board;
+using UnitsNet;
+
+// To work with an arduino:
+using ArduinoBoard board = new ArduinoBoard("COM5", 115200);
+using Ina236 device = new(board.CreateI2cDevice(new I2cConnectionSettings(0, 0x40)), ElectricResistance.FromMilliohms(8),
+    ElectricCurrent.FromAmperes(10.0));
+
+// Use this on a Raspberry Pi:
+////using Ina236 device2 = new Ina236(I2cDevice.Create(new I2cConnectionSettings(1, 0x40)), ElectricResistance.FromMilliohms(8),
+////    ElectricCurrent.FromAmperes(10.0));
+
+Console.WriteLine("Device initialized. Default settings used:");
+Console.WriteLine($"Operating Mode: {device.OperatingMode}");
+Console.WriteLine($"Number of Samples to average: {device.AverageOverNoSamples}");
+Console.WriteLine($"Bus conversion time: {device.BusConversionTime}us");
+Console.WriteLine($"Shunt conversion time: {device.ShuntConversionTime}us");
+
+while (!Console.KeyAvailable)
+{
+    // write out the current values from the INA219 device.
+    Console.WriteLine($"Bus Voltage {device.ReadBusVoltage()} Shunt Voltage {device.ReadShuntVoltage().Millivolts}mV Current {device.ReadCurrent()} Power {device.ReadPower()}");
+    Thread.Sleep(1000);
+}
